a28d51445e4a5941f723e4172ab722ab2873c640
ArmaanT <13340433+ArmaanT@users.noreply.github.com>
2019-02-02 17:27:49 -0500

    Set up multiple environment settings

---
+++ pennclubs/settings/production.py
@@ -0,0 +1,32 @@
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+from pennclubs.settings.base import *
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+DEBUG = False
+
+# Disable Django's own staticfiles handling in favour of WhiteNoise, for
+# greater consistency between gunicorn and `./manage.py runserver`. See:
+# http://whitenoise.evans.io/en/stable/django.html#using-whitenoise-in-development
+MIDDLEWARE.remove('django.middleware.security.SecurityMiddleware')
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+] + MIDDLEWARE
+
+# Fix MySQL Emoji support
+DATABASES['default']['OPTIONS'] = {'charset': 'utf8mb4'}
+
+# Honour the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow production host headers
+ALLOWED_HOSTS = ['clubs-backend.pennlabs.org', 'clubs-backend.apps.pennlabs.org']
+
+SENTRY_URL = os.environ.get('SENTRY_URL', '')
+
+sentry_sdk.init(
+    dsn=SENTRY_URL,
+    integrations=[DjangoIntegration()]
+)
