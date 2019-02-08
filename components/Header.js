08b5004ee44898c63a145ea807170243292d79d2
Arun Kirubarajan <arun.kirubarajan@gmail.com>
2019-02-08 16:37:29 -0500

    Add viewport tag

--- components/Header.js
+++ components/Header.js
@@ -3,6 +3,7 @@ const Header = () => (
     <head>
       <title>Penn Clubs</title>
       <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" /> 
       <link rel="stylesheet" href="static/style.css" />
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous"></link>
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossOrigin="anonymous" />
