a91f518e079132982307888a7ba4e4be15a0b00e
Leonardo Felipe Nerone <leonardofelipenerone@gmail.com>
2019-04-26 16:20:15 -0400

    server: improved server/index.js code

--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
+const ClientOAuth2 = require('client-oauth2')
 const express = require('express')
 const next = require('next')
 const bodyParser = require('body-parser')
+const router = require('./router')
+
 const PORT = process.env.PORT || 5000
-const dev = process.env.NODE_DEV !== 'production' //true false
+const dev = process.env.NODE_DEV !== 'production'
+
 const nextApp = next({ dev })
 const handle = nextApp.getRequestHandler()
 
-
-const ClientOAuth2 = require('client-oauth2')
 const platformAuth = new ClientOAuth2({
   clientId: process.env.clientId,
   clientSecret: process.env.clientSecret,
@@ -15,17 +17,17 @@ const platformAuth = new ClientOAuth2({
   authorizationUri: 'https://platform.pennlabs.org/accounts/authorize/',
   redirectUri: 'http://localhost:5000/auth',
   scopes: ['read', 'write'],
-  state: "eva",
+  state: 'eva',
 })
 
 nextApp.prepare().then(() => {
-  // express code here
   const app = express()
+
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: true }))
   app.listen(PORT, (err) => {
-  if (err) throw err
-  console.log(`ready at http://localhost:${PORT}`)
-  require('./router.js')(app, platformAuth, handle)
-});
+    if (err) throw err
+    console.log(`ready at http://localhost:${PORT}`)
+    router(app, platformAuth, handle)
+  })
 })
