6f1cd1d541d21a98ffbe616c2232a6555de997fa
Leonardo Felipe Nerone <leonardofelipenerone@gmail.com>
2019-04-26 16:22:32 -0400

    server: improved server/router.js code

--- server/router.js
+++ server/router.js
@@ -1,36 +1,38 @@
-module.exports = function getRoutes(app, platformAuth, handle) {
+module.exports = function getRoutes (app, platformAuth, handle) {
   app.get('/login', (req, res) => {
-    console.log("HERE");
+    console.log('HERE')
     const uri = platformAuth.code.getUri()
     res.redirect(uri)
   })
 
   app.get('/auth', (req, res) => {
-    console.log("auth!", req.originalUrl)
+    console.log('auth!', req.originalUrl)
     platformAuth.code.getToken(req.originalUrl, platformAuth)
       .then((user) => {
-        console.log("user", user) //=> { accessToken: '...', tokenType: 'bearer', ... }
+        console.log('user', user) // => { accessToken: '...', tokenType: 'bearer', ... }
 
-        // // Refresh the current users access token.
-        // user.refresh().then((updatedUser) => {
-        //   console.log(updatedUser !== user) //=> true
-        //   console.log(updatedUser.accessToken)
-        // })
-        //
-        // // Sign API requests on behalf of the current user.
-        // user.sign({
-        //   method: 'get',
-        //   url: 'http://example.com',
-        // })
+        /*
+          Refresh the current users access token.
+          user.refresh().then((updatedUser) => {
+            console.log(updatedUser !== user) //=> true
+            console.log(updatedUser.accessToken)
+          })
+
+          Sign API requests on behalf of the current user.
+          user.sign({
+            method: 'get',
+            url: 'http://example.com',
+          })
+        */
 
         // We should store the token into a database.
         return res.send(user.accessToken)
       })
       .catch((err) => {
-        console.log("err", err)
+        console.log('err', err)
       })
   })
 
-  //for all react stuff
+  // for all react stuff
   app.get('*', (req, res) => handle(req, res))
 }
