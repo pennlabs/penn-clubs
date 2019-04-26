0ccf19c99384780a48b312d967f1f3a19fc1f5e1
Leonardo Felipe Nerone <leonardofelipenerone@gmail.com>
2019-04-26 17:18:51 -0400

    chore: improve pages/favorites.js code

--- pages/favorites.js
+++ pages/favorites.js
@@ -1,42 +1,52 @@
 import React from 'react'
-import Select from 'react-select'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
-import ClubList from '../components/ClubList.js'
-import ClubModal from '../components/ClubModal.js'
-import renderPage from '../renderPage.js'
+import PropTypes from 'prop-types'
+import ClubList from '../components/ClubList'
+import renderPage from '../renderPage'
 import { CLUBS_GREY } from '../colors'
 
+const Favorites = ({
+  tags,
+  favorites,
+  updateFavorites,
+  openModal,
+  favoriteClubs,
+}) => (
+  <div
+    style={{
+      padding: '0 2rem', display: 'flex', alignItems: 'center', minHeight: '72vh', flexDirection: 'column',
+    }}
+  >
+    <div style={{ padding: '30px 0' }}>
+      <h1 className="title" style={{ color: CLUBS_GREY }}>Favorites</h1>
+    </div>
 
-class Favorites extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      modal: false,
-      modalClub: {},
+    {favoriteClubs.map(club => (
+      <ClubList
+        club={club}
+        tags={tags}
+        updateFavorites={updateFavorites}
+        openModal={openModal}
+        favorite={favorites.includes(club.id)}
+      />
+    ))}
+
+    {
+      (favorites.length === 0) ? (
+        <p className="has-text-light-grey" style={{ paddingTop: 200 }}>
+          No favorites yet! Browse clubs
+          <a href="/">here.</a>
+        </p>
+      ) : <div />
     }
-  }
+  </div>
+)
 
-  render() {
-    var { tags, favorites, updateFavorites, openModal, closeModal, favoriteClubs } = this.props
-    return (
-      <div style={{padding: '0 2rem', display: "flex", alignItems: "center", minHeight: "72vh", flexDirection: "column"}}>
-        <div style={{padding: "30px 0"}}>
-          <h1 className="title" style={{color: CLUBS_GREY}}>Favorites</h1>
-        </div>
-        {favoriteClubs.map((club) => (
-          <ClubList
-            club={club}
-            tags={tags}
-            updateFavorites={updateFavorites}
-            openModal={openModal}
-            favorite={favorites.includes(club.id)}/>
-        ))}
-        {!favorites.length ? <p className="has-text-light-grey" style={{paddingTop: 200}}>No favorites yet! Browse clubs <a href="/">here.</a></p> : <div></div>}
-      </div>
-    );
-  }
+Favorites.propTypes = {
+  tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+  favorites: PropTypes.arrayOf(PropTypes.string).isRequired,
+  favoriteClubs: PropTypes.arrayOf(PropTypes.string).isRequired,
+  updateFavorites: PropTypes.func.isRequired,
+  openModal: PropTypes.func.isRequired,
 }
 
-
-export default renderPage(Favorites);
+export default renderPage(Favorites)
