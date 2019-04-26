12b69f4aa26815e4da89850007b5f76012c5508e
Leonardo Felipe Nerone <leonardofelipenerone@gmail.com>
2019-04-26 17:59:13 -0400

    chore: improve components/Footer.js code

--- components/Footer.js
+++ components/Footer.js
@@ -1,19 +1,31 @@
 const Footer = () => (
-  <footer className="footer" style={{height: "8rem", backgroundColor: "#f2f2f2"}}>
-      <div className="container">
-        <div className="content has-text-centered">
-          <p style={{fontSize: "0.85rem"}}>
-            Made with <span className="icon is-small" style={{color:"#F56F71"}}><i className="fa fa-heart"></i></span> by Penn Labs in Philadelphia.
-          </p>
-          <a href="https://github.com/pennlabs/" style={{marginRight:"1rem"}}>
-          <span><i className="fab fa-github"></i></span>
+  <footer
+    className="footer"
+    style={{ height: '8rem', backgroundColor: '#f2f2f2' }}
+  >
+    <div className="container">
+      <div className="content has-text-centered">
+        <p style={{ fontSize: '0.85rem' }}>
+            Made with
+          {' '}
+          <span
+            className="icon is-small"
+            style={{ color: '#F56F71' }}
+          >
+            <i className="fa fa-heart" />
+          </span>
+          {' '}
+by Penn Labs in Philadelphia.
+        </p>
+        <a href="https://github.com/pennlabs/" style={{ marginRight: '1rem' }}>
+          <span><i className="fab fa-github" /></span>
         </a>
         <a href="https://www.facebook.com/labsatpenn/">
-        <span><i className="fab fa-facebook-square"></i></span>
-      </a>
+          <span><i className="fab fa-facebook-square" /></span>
+        </a>
+      </div>
     </div>
-  </div>
-</footer>
+  </footer>
 )
 
 export default Footer
