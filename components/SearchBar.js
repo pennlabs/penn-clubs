9d411e2cfd7b243cd09fb9ca2474923af68c5e29
Leonardo Felipe Nerone <leonardofelipenerone@gmail.com>
2019-04-26 17:59:31 -0400

    chore: improve components/SearchBar.js code

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import Select from 'react-select'
-import DropdownFilter from './DropdownFilter'
 import posed from 'react-pose'
+import DropdownFilter from './DropdownFilter'
 import { CLUBS_GREY, CLUBS_GREY_LIGHT } from '../colors'
 
 const Pop = posed.div({
@@ -10,109 +9,120 @@ const Pop = posed.div({
 })
 
 class SearchBar extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.state = {
-      nameInput: "",
+      nameInput: '',
       sizeOptions: [
         { value: 1, label: 'less than 20 members' },
         { value: 2, label: '20 to 50 members' },
         { value: 3, label: '50 to 100 members' },
-        { value: 4, label: 'more than 100'}
+        { value: 4, label: 'more than 100' },
       ],
-      tagOptions: props.tags.map((tag) => ({value: tag.id, label: tag.name})),
+      tagOptions: props.tags.map(tag => ({ value: tag.id, label: tag.name })),
       applicationOptions: [
-        {value: 1, label: "Requires application"},
-        {value: 2, label: "Does not require application"},
-        {value: 3, label: "Currently accepting applications"}],
+        { value: 1, label: 'Requires application' },
+        { value: 2, label: 'Does not require application' },
+        { value: 3, label: 'Currently accepting applications' }],
       hoverList: false,
       hoverCard: false,
       hoverDown: false,
-      selectedTags: props.selectedTags
+      selectedTags: props.selectedTags,
     }
   }
 
-  componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate (prevProps, prevState) {
     if (prevState.nameInput !== this.state.nameInput) {
       this.props.resetDisplay(this.state.nameInput, this.state.selectedTags)
     }
   }
 
-  render() {
-    const { tagOptions, sizeOptions, applicationOptions, selectedTags } = this.state
+  render () {
+    const {
+      tagOptions, sizeOptions, applicationOptions, selectedTags,
+    } = this.state
     const { switchDisplay, resetDisplay, updateTag } = this.props
     return (
-      <div style={{height: "100vh", width: "100%", overflow: "hidden", position: "sticky", top: -20}}>
+      <div style={{
+        height: '100vh', width: '100%', overflow: 'hidden', position: 'sticky', top: -20,
+      }}
+      >
         <div
           style={{
-            position: "absolute",
-            height: "90vh",
-            width: "100%",
+            position: 'absolute',
+            height: '90vh',
+            width: '100%',
             right: -17,
-            padding: "50px 0",
-            overflowY: "scroll",
-            overflowX: "hidden",
-            marginBottom: "8rem"
-          }}>
-          <div className="is-flex" style={{justifyContent: "space-between", padding: "0 3px"}}>
-            <b style={{color: CLUBS_GREY}}>View: </b>
+            padding: '50px 0',
+            overflowY: 'scroll',
+            overflowX: 'hidden',
+            marginBottom: '8rem',
+          }}
+        >
+          <div className="is-flex" style={{ justifyContent: 'space-between', padding: '0 3px' }}>
+            <b style={{ color: CLUBS_GREY }}>View: </b>
             <div className="is-flex">
               <Pop
-                pose={this.state.hoverCard ? "hovered" : "idle"}
+                pose={this.state.hoverCard ? 'hovered' : 'idle'}
                 onMouseEnter={() => this.setState({ hoverCard: true })}
-                onMouseLeave={() => this.setState({ hoverCard: false })}>
-                <span className="icon" style={{cursor: "pointer", color: CLUBS_GREY}} onClick={(e)=>switchDisplay("cards")}>
-                  <i class="fas fa-th-large"></i>
+                onMouseLeave={() => this.setState({ hoverCard: false })}
+              >
+                <span className="icon" style={{ cursor: 'pointer', color: CLUBS_GREY }} onClick={e => switchDisplay('cards')}>
+                  <i className="fas fa-th-large" />
                 </span>
               </Pop>
               <Pop
-                pose={this.state.hoverList ? "hovered" : "idle"}
+                pose={this.state.hoverList ? 'hovered' : 'idle'}
                 onMouseEnter={() => this.setState({ hoverList: true })}
-                onMouseLeave={() => this.setState({ hoverList: false })}>
-                <span className="icon" >
-                  <i class="fas fa-list" style={{cursor: "pointer", color: CLUBS_GREY}} onClick={(e)=>switchDisplay("list")}></i>
+                onMouseLeave={() => this.setState({ hoverList: false })}
+              >
+                <span className="icon">
+                  <i className="fas fa-list" style={{ cursor: 'pointer', color: CLUBS_GREY }} onClick={e => switchDisplay('list')} />
                 </span>
               </Pop>
             </div>
           </div>
-          <div style={{margin: "30px 0"}}>
-            <hr style={{backgroundColor: CLUBS_GREY, height:"2px", margin: 0, padding: 0}}/>
-            <div style={{display: "flex", justifyContent: "space-between", padding: "7px 3px"}}>
+          <div style={{ margin: '30px 0' }}>
+            <hr style={{
+              backgroundColor: CLUBS_GREY, height: '2px', margin: 0, padding: 0,
+            }}
+            />
+            <div style={{ display: 'flex', justifyContent: 'space-between', padding: '7px 3px' }}>
               <input
                 type="text"
                 name="search"
                 placeholder="Search"
                 style={{
                   borderWidth: 0,
-                  outline: "none",
+                  outline: 'none',
                   color: CLUBS_GREY,
-                  width: "100%",
-                  fontSize: "1em",
+                  width: '100%',
+                  fontSize: '1em',
                 }}
                 value={this.state.nameInput}
-                onInput={(e) => this.setState({nameInput: e.target.value})}
-                />
-              <span className="icon" style={{cursor: "pointer", color: CLUBS_GREY}}>
-                {this.state.nameInput ? <i onClick={(e)=>this.setState({nameInput: ""})} class="fas fa-times"></i> : <i class="fas fa-search"></i>}
+                onInput={e => this.setState({ nameInput: e.target.value })}
+              />
+              <span className="icon" style={{ cursor: 'pointer', color: CLUBS_GREY }}>
+                {this.state.nameInput ? <i onClick={e => this.setState({ nameInput: '' })} className="fas fa-times" /> : <i className="fas fa-search" />}
               </span>
             </div>
           </div>
           <DropdownFilter
             name="Type"
             options={tagOptions}
-            selected={selectedTags.filter(tag => tag.name === "Type")}
+            selected={selectedTags.filter(tag => tag.name === 'Type')}
             updateTag={updateTag}
           />
           <DropdownFilter
             name="Size"
             options={sizeOptions}
-            selected={selectedTags.filter(tag => tag.name === "Size")}
+            selected={selectedTags.filter(tag => tag.name === 'Size')}
             updateTag={updateTag}
           />
           <DropdownFilter
             name="Application"
             options={applicationOptions}
-            selected={selectedTags.filter(tag => tag.name === "Application")}
+            selected={selectedTags.filter(tag => tag.name === 'Application')}
             updateTag={updateTag}
           />
         </div>
@@ -121,4 +131,4 @@ class SearchBar extends React.Component {
   }
 }
 
-export default SearchBar;
+export default SearchBar
