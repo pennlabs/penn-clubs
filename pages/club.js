61317c2c0c5e05aa1ce41700ffebb02ae7fe4870
Leonardo Felipe Nerone <leonardofelipenerone@gmail.com>
2019-04-26 17:11:30 -0400

    chore: improve pages/club.js code

--- pages/club.js
+++ pages/club.js
@@ -1,39 +1,25 @@
-import fetch from 'isomorphic-unfetch'
-import renderPage from '../renderPage.js'
-import { CLUBS_GREY } from '../colors'
 import React from 'react'
+import fetch from 'isomorphic-unfetch'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
-class Club extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
+const Club = ({ club }) => console.log('club', club) || (
+  <div>
+    <Header />
+    <Footer />
+  </div>
+)
 
-    }
-  }
-
-  render() {
-    const { club, tags } = this.props
-    console.log("club", club)
-    return (
-      <div>
-       <Header />
-       <Footer />
-      </div>
-    )
-  }
-}
-
-Club.getInitialProps = async (props) => {
+Club.getInitialProps = async ({ query }) => {
   const tagsRequest = await fetch('https://clubs.pennlabs.org/tags/?format=json')
   const tagsResponse = await tagsRequest.json()
-  var { query } = props
   const clubRequest = await fetch(`https://clubs.pennlabs.org/clubs/${query.club}/?format=json`)
   const clubResponse = await clubRequest.json()
-  return { club: clubResponse, tags: tagsResponse }
+
+  return {
+    club: clubResponse,
+    tags: tagsResponse,
+  }
 }
-
-
 
 export default Club
