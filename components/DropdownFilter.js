19a8a2d748664419fd46af59c3e7ef643da3f6d1
Leonardo Felipe Nerone <leonardofelipenerone@gmail.com>
2019-04-26 17:59:04 -0400

    chore: improve components/DropdownFilter.js code

--- components/DropdownFilter.js
+++ components/DropdownFilter.js
@@ -8,7 +8,7 @@ const Pop = posed.div({
 })
 
 class DropdownFilter extends React.Component {
-  constructor(props){
+  constructor (props) {
     super(props)
     this.state = {
       hoverArrow: false,
@@ -17,41 +17,50 @@ class DropdownFilter extends React.Component {
     }
   }
 
-  toggleDrop() {
-    this.setState({drop: !this.state.drop})
+  toggleDrop () {
+    this.setState({ drop: !this.state.drop })
   }
 
-  isSelected(tag) {
-    var { label, value, name } = tag
-    var { selected } = this.props
+  isSelected (tag) {
+    const { label, value, name } = tag
+    const { selected } = this.props
     return selected.find(tag => tag.value === value)
   }
 
-  render() {
-    var { name, options, selected, updateTag } = this.props
-    var { drop, hoverDown } = this.state
-    return(
+  render () {
+    const {
+      name, options, selected, updateTag,
+    } = this.props
+    const { drop, hoverDown } = this.state
+    return (
       <div>
-        <hr style={{backgroundColor: CLUBS_GREY, height:"2px", margin: 0, marginTop: 30, padding: 0}}/>
-        <div style={{display: "flex", justifyContent: "space-between", padding: "7px 3px"}}>
-          <p style={{color: CLUBS_GREY}}>{name} </p>
+        <hr style={{
+          backgroundColor: CLUBS_GREY, height: '2px', margin: 0, marginTop: 30, padding: 0,
+        }}
+        />
+        <div style={{ display: 'flex', justifyContent: 'space-between', padding: '7px 3px' }}>
+          <p style={{ color: CLUBS_GREY }}>
+            {name}
+            {' '}
+          </p>
           <Pop
-            pose={hoverDown ? "hovered" : "idle"}
+            pose={hoverDown ? 'hovered' : 'idle'}
             onMouseEnter={() => this.setState({ hoverDown: true })}
-            onMouseLeave={() => this.setState({ hoverDown: false })}>
-            <span className="icon" style={{cursor: "pointer", color: CLUBS_GREY}} onClick={(e)=>this.toggleDrop()}>
-              <i class="fas fa-chevron-down"></i>
+            onMouseLeave={() => this.setState({ hoverDown: false })}
+          >
+            <span className="icon" style={{ cursor: 'pointer', color: CLUBS_GREY }} onClick={e => this.toggleDrop()}>
+              <i className="fas fa-chevron-down" />
             </span>
           </Pop>
         </div>
         {drop ? (options.map(tag => (
-          <div style={{display: "flex", paddingTop: 3}}>
-            <span className="icon" style={{cursor: "pointer", color: CLUBS_GREY_LIGHT}} onClick={(e)=>updateTag(tag, name)}>
-              <i className={this.isSelected(tag) ? "fas fa-check-square" : "far fa-square"}></i>
+          <div style={{ display: 'flex', paddingTop: 3 }}>
+            <span className="icon" style={{ cursor: 'pointer', color: CLUBS_GREY_LIGHT }} onClick={e => updateTag(tag, name)}>
+              <i className={this.isSelected(tag) ? 'fas fa-check-square' : 'far fa-square'} />
             </span>
-            <p style={{color: CLUBS_GREY_LIGHT}}>{tag.label}</p>
+            <p style={{ color: CLUBS_GREY_LIGHT }}>{tag.label}</p>
           </div>
-        ))) : ""}
+        ))) : ''}
       </div>
     )
   }
