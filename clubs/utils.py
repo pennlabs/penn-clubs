c7586007c78b2d8c78da07088cbf61a0f2b7d9d5
Eric Wang <ezwang1@gmail.com>
2019-08-17 08:10:27 -0400

    add error flag on error

--- clubs/utils.py
+++ clubs/utils.py
@@ -3,7 +3,5 @@ from rest_framework.views import exception_handler
 
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
-
-    
-
+    response.data['error'] = True
     return response
