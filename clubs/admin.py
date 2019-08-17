5ca28e1030339ac73d6937cd93670a424a4d8939
Eric Wang <ezwang1@gmail.com>
2019-08-16 23:33:09 -0400

    improve django admin

--- clubs/admin.py
+++ clubs/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
-from clubs.models import Club, Tag
+from clubs.models import Club, Tag, Membership
+from django.contrib.auth.models import Group
 
-admin.site.register(Club)
-admin.site.register(Tag)
+
+admin.site.unregister(Group)
+
+
+class ClubAdmin(admin.ModelAdmin):
+    search_fields = ('name', 'subtitle', 'email')
+    list_display = ('name', 'email')
+    list_filter = ('size', 'application_required', 'application_available', 'listserv_available')
+
+
+class TagAdmin(admin.ModelAdmin):
+    def club_count(self, obj):
+        return obj.club_set.count()
+
+    search_fields = ('name',)
+    list_display = ('name', 'club_count')
+
+
+admin.site.register(Club, ClubAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Membership)
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from users.models import Person
 
-# Register your models here.
+
+admin.site.register(Person)
