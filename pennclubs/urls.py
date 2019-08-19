4bfe1cd6ef6af674bcacb8d7ea66de262212f8fc
Armaan Tobaccowalla <13340433+ArmaanT@users.noreply.github.com>
2019-08-18 21:06:02 -0400

    Move non-clubs urls to base urls.py

--- pennclubs/urls.py
+++ pennclubs/urls.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
+
+from clubs.views import splash
 
 admin.site.site_header = "Clubs Backend Admin"
 
 urlpatterns = [
     path('', include('clubs.urls')),
+    path('', splash, name='homepage'),
+    path('openapi/', get_schema_view(
+        title='Clubs Backend Documentation'
+    ), name='openapi-schema'),
+    path('documentation/', TemplateView.as_view(
+        template_name='redoc.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='documentation'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls', namespace='accounts')),
 ]
