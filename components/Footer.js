eab2897102c581740dd176990c05a283e6648173
Eric Wang <ezwang1@gmail.com>
2019-08-21 11:13:53 -0400

    standardize footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ const Footer = () => (
     <div className="container">
       <div className="content has-text-centered">
         <p style={{ fontSize: '0.85rem' }}>
-            Made with <span className="icon is-small" style={{ color: '#F56F71' }}><i className="fa fa-heart"></i></span> by <a style={{ color: 'inherit' }} href="https://pennlabs.org/">Penn Labs</a> in Philadelphia
+            Made with <span className="icon is-small" style={{ color: '#F56F71' }}><i className="fa fa-heart"></i></span> by <a style={{ color: 'inherit' }} href="https://pennlabs.org/">Penn Labs</a>
         </p>
         <a href="https://github.com/pennlabs/" style={{ marginRight: '1rem' }}>
           <span><i className="fab fa-github"></i></span>
