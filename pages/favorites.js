0e4345a2a09bee4453ee8e1c62a2b9d745246149
ccabo1 <ccabo@seas.upenn.edu>
2019-08-28 22:57:34 -0400

    refactor favs

--- pages/favorites.js
+++ pages/favorites.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import s from 'styled-components'
+
 import ClubList from '../components/ClubList'
 import { renderListPage } from '../renderPage'
 import { CLUBS_GREY } from '../constants/colors'
+
+const Wrapper = s.div`
+  padding: 0 2rem;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`
 
 class Favorites extends React.Component {
   constructor(props) {
@@ -13,12 +22,16 @@ class Favorites extends React.Component {
   }
 
   render() {
-    var { tags, favorites, updateFavorites, openModal, closeModal, favoriteClubs } = this.props
+    const {
+      tags, favorites, updateFavorites, openModal, favoriteClubs
+    } = this.props
+
     return (
-      <div style={{ padding: '0 2rem', display: 'flex', alignItems: 'center', minHeight: 'calc(100vh - 180px)', flexDirection: 'column' }}>
+      <Wrapper>
         <div style={{ padding: '30px 0' }}>
           <h1 className="title" style={{ color: CLUBS_GREY }}>Favorites</h1>
         </div>
+
         {favoriteClubs.map((club) => (
           <ClubList
             key={club.id}
@@ -26,10 +39,16 @@ class Favorites extends React.Component {
             tags={tags}
             updateFavorites={updateFavorites}
             openModal={openModal}
-            favorite={favorites.includes(club.id)}/>
+            favorite={favorites.includes(club.id)}
+          />
         ))}
-        {!favorites.length ? <p className="has-text-light-grey" style={{ paddingTop: 200 }}>No favorites yet! Browse clubs <a href="/">here.</a></p> : <div></div>}
-      </div>
+
+        {(!favorites.length) ? (
+          <p className="has-text-light-grey" style={{ paddingTop: 200 }}>
+            No favorites yet! Browse clubs <a href="/">here.</a>
+          </p>
+        ) : (<div />)}
+      </Wrapper>
     )
   }
 }
