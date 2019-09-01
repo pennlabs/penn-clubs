08f093a86fb16334454ece4e93bf34ade1dc3222
Eric Wang <ezwang1@gmail.com>
2019-09-01 16:03:22 -0400

    improve clubs admin

--- clubs/admin.py
+++ clubs/admin.py
@@ -5,18 +5,35 @@ from django.db.models import Exists, OuterRef
 from clubs.models import Asset, Club, Event, Favorite, Membership, MembershipInvite, Tag
 
 
+class HasOwnerListFilter(admin.SimpleListFilter):
+    title = 'has owner'
+    parameter_name = 'has_owner'
+
+    def lookups(self, request, model_admin):
+        return [('true', 'True'), ('false', 'False')]
+
+    def queryset(self, request, queryset):
+        return queryset.filter(has_owner=self.value() == 'true')
+
+
 class ClubAdmin(admin.ModelAdmin):
     search_fields = ('name', 'subtitle', 'email')
-    list_display = ('name', 'email', 'has_owner')
-    list_filter = ('size', 'application_required', 'accepting_members', 'listserv')
+    list_display = ('name', 'email', 'has_owner', 'has_invite')
+    list_filter = ('size', 'application_required', 'accepting_members', HasOwnerListFilter)
 
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(
-            has_owner=Exists(Membership.objects.filter(club=OuterRef('pk'), role__lte=0))
+            has_owner=Exists(Membership.objects.filter(club=OuterRef('pk'), role__lte=0)),
+            has_invite=Exists(MembershipInvite.objects.filter(club=OuterRef('pk')))
         )
+
+    def has_invite(self, obj):
+        return obj.has_invite
+    has_invite.boolean = True
 
     def has_owner(self, obj):
         return obj.has_owner
+    has_owner.boolean = True
 
 
 class EventAdmin(admin.ModelAdmin):
