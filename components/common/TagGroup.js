b7eaa7289cc7af16ce193b25d4b1c7d713df320a
Daniel Tao <danieltaox@gmail.com>
2019-09-02 19:10:12 -0400

    clicking on club modal tags filters clubs

--- components/common/TagGroup.js
+++ components/common/TagGroup.js
@@ -1,14 +1,41 @@
 import React from 'react'
-import { BlueTag } from './Tags'
+import { BlueTag, SelectedTag } from './Tags'
 
 export default (props) => {
-  const { tags } = props
+  let { tags, selectedTags, updateTag } = props
   if (!tags || !tags.length) return null
+  selectedTags = selectedTags || []
+  // TODO: Use same tag format between DropdownFilter and TagGroup
   return (
-    tags.map(({ id, name }) => (
-      <BlueTag key={id} className="tag is-rounded has-text-white">
-        {name}
+    tags.map(tag => {
+      const matchedTag = selectedTags.find(({ value }) => value === tag.id)
+      if (matchedTag) {
+        return <SelectedTag
+          key={tag.value}
+          className="tag is-rounded has-text-white"
+          onClick={(e) => {
+            e.stopPropagation()
+            updateTag(matchedTag, matchedTag.name)
+          }}
+        >
+          {tag.name}
+          <button className="delete is-small" />
+        </SelectedTag>
+      }
+      return <BlueTag
+        key={tag.id}
+        className="tag is-rounded has-text-white"
+        onClick={(e) => {
+          e.stopPropagation()
+          updateTag({
+            value: tag.id,
+            label: tag.name,
+            name: "Type"
+          }, "Type")
+        }}
+      >
+        {tag.name}
       </BlueTag>
-    ))
+    })
   )
 }
