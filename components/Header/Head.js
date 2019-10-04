5953f325e412e65826bed27dd192519c15f86a23
ccabo1 <ccabo@seas.upenn.edu>
2019-10-04 16:25:59 -0400

    refactor otw

--- components/Header/Head.js
+++ components/Header/Head.js
@@ -6,14 +6,29 @@ import { initGA, logPageView } from '../../utils/analytics'
 // Data to populate site metadata
 const title = 'Penn Clubs'
 const keywords = [
-  'clubs', 'penn', 'upenn', 'labs', 'org', 'organization', 'student',
-  'undergrad', 'peer', 'leadership', 'community', 'university', 'college',
-  'wharton', 'seas', 'school'
+  'clubs',
+  'penn',
+  'upenn',
+  'labs',
+  'org',
+  'organization',
+  'student',
+  'undergrad',
+  'peer',
+  'leadership',
+  'community',
+  'university',
+  'college',
+  'wharton',
+  'seas',
+  'school',
 ].join(', ')
 const author = 'Penn Labs <contact@pennlabs.org>'
-const description = 'Penn Clubs is your central source of information about student organizations at the University of Pennsylvania. Keep discovering new clubs throughout the year.'
+const description =
+  'Penn Clubs is your central source of information about student organizations at the University of Pennsylvania. Keep discovering new clubs throughout the year.'
 const url = 'https://pennclubs.com'
-const image = 'https://pennlabs-assets.s3.amazonaws.com/metadata-images/penn-clubs.png'
+const image =
+  'https://pennlabs-assets.s3.amazonaws.com/metadata-images/penn-clubs.png'
 const imageAlt = 'Penn Clubs logo'
 const type = 'website'
 const twitterUsername = '@pennlabs'
@@ -59,12 +74,16 @@ export default class Heading extends React.Component {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css"
         />
-        <link rel="stylesheet"
+        <link
+          rel="stylesheet"
           href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
           integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
           crossOrigin="anonymous"
         />
-        <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Roboto&display=swap"
+          rel="stylesheet"
+        />
         <script
           src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
           integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
