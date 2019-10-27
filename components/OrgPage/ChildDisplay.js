88b638255a38431d5129a73f4d97a1416db865e0
haoyxc <48307028+haoyxc@users.noreply.github.com>
2019-10-27 14:12:12 -0400

    org children displayed

--- components/OrgPage/ChildDisplay.js
+++ components/OrgPage/ChildDisplay.js
@@ -1,102 +1,62 @@
-import React, { useState, Component } from 'react'
-import OrgChildren from './OrgChildren'
-import { Link } from '../../routes'
-import Icon from '../common/Icon'
-import {
-  LIGHT_GRAY,
-  BLACK,
-  CLUBS_GREY_LIGHT,
-  WHITE,
-  BABY_BLUE,
-} from '../../constants/colors'
-import s from 'styled-components'
+import React, { useState, Component } from "react";
+import OrgChildren from "./OrgChildren";
+import { LIGHT_GRAY, HOVER_GRAY } from "../../constants/colors";
+import s from "styled-components";
 
 const Child = s.div`
   display: flex;
   flex-direction: row; 
   justify-content: space-between;
   margin: 5px 20px;
-  margin-left: 30px;
-  border-radius: 20px;
-  background: ${CLUBS_GREY_LIGHT};
-  padding: 20px; 
+  border-radius: 25px;
+  background: ${LIGHT_GRAY};
+  padding: 15px; 
 
   &:hover{
-    background: ${LIGHT_GRAY}
-    color: ${BLACK}
+    background: ${HOVER_GRAY}
   }
-`
-const Text = s.p`
-  color: ${WHITE};
-  font-size: 16px;
-
-  &:hover{
-    font-weight: bold;
-    color: ${BABY_BLUE}
-  }
-`
-const TextLink = s.a`
-  color: ${WHITE};
-  font-size: 16px;
-
-  &:hover{
-    font-weight: bold;
-    
-  }
-`
+`;
 
 export default class ChildDisplay extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      show: false,
-    }
-    this.handleClick = this.handleClick.bind(this)
-    this.handleDetails = this.handleDetails.bind(this)
+      show: false
+    };
+    this.handleClick = this.handleClick.bind(this);
   }
   handleClick() {
     this.setState(state => ({
-      show: !state.show,
-    }))
-  }
-  handleDetails() {
-    console.log(this.state.child)
+      show: !state.show
+    }));
   }
   render() {
-    const { child } = this.props
+    const { child } = this.props;
     return (
-      <div style={{ margin: '10px', borderRadius: '20px' }}>
+      <div style={{ margin: "10px", borderRadius: "20px" }}>
         <div
           style={{
             marginTop: 20,
-            cursor: 'pointer',
+            cursor: "pointer"
           }}
         >
           <Child>
-            <Text
+            <p
               style={{
-                fontSize: '15px',
-                color: 'white',
+                fontSize: "15px",
+                color: "white"
               }}
               onClick={this.handleClick}
             >
               {child.name}
-            </Text>
-            {/* <Link>Click for info</Link> */}
-            <TextLink
-              href={`/club/${child.code}/admin`}
-              style={{ color: 'white' }}
-              onClick={this.handleDetails}
-            >
-              View{` `}
-              <Icon name="external-link-white" alt="View Club Page"></Icon>
-            </TextLink>
+            </p>
+            <p style={{ color: "white" }}>Click for info</p>
           </Child>
           {this.state.show && child.children.length ? (
             <OrgChildren children={this.props.child.children} />
           ) : null}
         </div>
       </div>
-    )
+    );
   }
 }
--- pages/club.js
+++ pages/club.js
@@ -1,37 +1,37 @@
-import renderPage from '../renderPage.js'
-import { getDefaultClubImageURL, doApiRequest } from '../utils'
-import React from 'react'
-import s from 'styled-components'
-import Tabs from '../components/ClubPage/Tabs.js'
-import Header from '../components/ClubPage/Header.js'
-import InfoBox from '../components/ClubPage/InfoBox.js'
-import SocialIcons from '../components/ClubPage/SocialIcons.js'
+import renderPage from "../renderPage.js";
+import { getDefaultClubImageURL, doApiRequest } from "../utils";
+import React from "react";
+import s from "styled-components";
+import Tabs from "../components/ClubPage/Tabs.js";
+import Header from "../components/ClubPage/Header.js";
+import InfoBox from "../components/ClubPage/InfoBox.js";
+import SocialIcons from "../components/ClubPage/SocialIcons.js";
 
 const Image = s.img`
   max-height: 300px;
   max-width: 100%;
   object-fit: contain;
-`
+`;
 
 class Club extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      club: null,
-    }
+      club: null
+    };
   }
 
   componentDidMount() {
     doApiRequest(`/clubs/${this.props.query.club}/?format=json`)
       .then(resp => resp.json())
-      .then(data => this.setState({ club: data }))
+      .then(data => this.setState({ club: data }));
   }
 
   render() {
-    const { club } = this.state
+    const { club } = this.state;
 
     if (!club) {
-      return <div />
+      return <div />;
     }
 
     if (!club.code) {
@@ -40,11 +40,11 @@ class Club extends React.Component {
           <h1 className="title is-h1">404 Not Found</h1>
           <p>The club you are looking for does not exist.</p>
         </div>
-      )
+      );
     }
 
     return (
-      <div style={{ padding: '30px 50px' }}>
+      <div style={{ padding: "30px 50px" }}>
         <Header
           club={club}
           userInfo={this.props.userInfo}
@@ -62,13 +62,13 @@ class Club extends React.Component {
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
 Club.getInitialProps = async props => {
-  var { query } = props
-  return { query: query }
-}
+  var { query } = props;
+  return { query: query };
+};
 
-export default renderPage(Club)
+export default renderPage(Club);
