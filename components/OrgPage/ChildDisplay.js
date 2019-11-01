1cc8a69523d0df492308912d948863e40391372b
haoyxc <48307028+haoyxc@users.noreply.github.com>
2019-11-01 17:39:29 -0400

    added icon

--- components/OrgPage/ChildDisplay.js
+++ components/OrgPage/ChildDisplay.js
@@ -1,14 +1,15 @@
-import React, { useState, Component } from "react";
-import OrgChildren from "./OrgChildren";
-import { Link } from "../../routes";
+import React, { useState, Component } from 'react'
+import OrgChildren from './OrgChildren'
+import { Link } from '../../routes'
+import Icon from '../common/Icon'
 import {
   LIGHT_GRAY,
   BLACK,
   CLUBS_GREY_LIGHT,
   WHITE,
-  BABY_BLUE
-} from "../../constants/colors";
-import s from "styled-components";
+  BABY_BLUE,
+} from '../../constants/colors'
+import s from 'styled-components'
 
 const Child = s.div`
   display: flex;
@@ -24,7 +25,7 @@ const Child = s.div`
     background: ${LIGHT_GRAY}
     color: ${BLACK}
   }
-`;
+`
 const Text = s.p`
   color: ${WHITE};
   font-size: 16px;
@@ -33,7 +34,7 @@ const Text = s.p`
     font-weight: bold;
     color: ${BABY_BLUE}
   }
-`;
+`
 const TextLink = s.a`
   color: ${WHITE};
   font-size: 16px;
@@ -42,40 +43,40 @@ const TextLink = s.a`
     font-weight: bold;
     
   }
-`;
+`
 
 export default class ChildDisplay extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      show: false
-    };
-    this.handleClick = this.handleClick.bind(this);
-    this.handleDetails = this.handleDetails.bind(this);
+      show: false,
+    }
+    this.handleClick = this.handleClick.bind(this)
+    this.handleDetails = this.handleDetails.bind(this)
   }
   handleClick() {
     this.setState(state => ({
-      show: !state.show
-    }));
+      show: !state.show,
+    }))
   }
   handleDetails() {
-    console.log(this.state.child);
+    console.log(this.state.child)
   }
   render() {
-    const { child } = this.props;
+    const { child } = this.props
     return (
-      <div style={{ margin: "10px", borderRadius: "20px" }}>
+      <div style={{ margin: '10px', borderRadius: '20px' }}>
         <div
           style={{
             marginTop: 20,
-            cursor: "pointer"
+            cursor: 'pointer',
           }}
         >
           <Child>
             <Text
               style={{
-                fontSize: "15px",
-                color: "white"
+                fontSize: '15px',
+                color: 'white',
               }}
               onClick={this.handleClick}
             >
@@ -84,10 +85,11 @@ export default class ChildDisplay extends Component {
             {/* <Link>Click for info</Link> */}
             <TextLink
               href={`/club/${child.code}/admin`}
-              style={{ color: "white" }}
+              style={{ color: 'white' }}
               onClick={this.handleDetails}
             >
-              Click for info
+              View{` `}
+              <Icon name="external-link-white" alt="View Club Page"></Icon>
             </TextLink>
           </Child>
           {this.state.show && child.children.length ? (
@@ -95,6 +97,6 @@ export default class ChildDisplay extends Component {
           ) : null}
         </div>
       </div>
-    );
+    )
   }
 }
