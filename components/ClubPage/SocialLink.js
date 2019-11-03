85ec908dbf569ff91965552e56c1d76ae38e17f9
Cameron Cabo <ccabo@seas.upenn.edu>
2019-11-03 13:17:17 -0500

    Update SocialLink.js

--- components/ClubPage/SocialLink.js
+++ components/ClubPage/SocialLink.js
@@ -2,7 +2,7 @@ const SocialLink = ({ club, item, type }) => {
   let url
 
   if (type === 'email') {
-    url = `mailto:${club[item.name]`
+    url = `mailto:${club[item.name]}`
   } else {
     url = club[item.name]
   }
