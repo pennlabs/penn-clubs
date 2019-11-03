5d65448e4e3d19a4dc0f6ef34c173442572d3b7d
Peyton Walters <pwalters18@bloomberg.net>
2019-11-03 13:14:01 -0500

    i am dum

--- backend/pennclubs/settings/development.py
+++ backend/pennclubs/settings/development.py
@@ -16,7 +16,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 PLATFORM_ACCOUNTS.update(
     {
-        'REDIRECT_URI': f'https:///{BACKEND_DOMAIN}/accounts/callback/'),
+        'REDIRECT_URI': os.environ.get('LABS_REDIRECT_URI', 'http://localhost:8000/accounts/callback/'),
         'CLIENT_ID': 'clientid',
         'CLIENT_SECRET': 'supersecretclientsecret',
         'PLATFORM_URL': 'https://platform-dev.pennlabs.org',
