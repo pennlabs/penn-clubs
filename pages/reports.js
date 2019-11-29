f60df03bebe008d9fa83fa39cf94ee9d21ca6465
Davis Haupt <davis.haupt@gmail.com>
2019-11-29 14:56:49 -0500

    add reports page

---
+++ pages/reports.js
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react'
+import s from 'styled-components'
+
+import renderPage from '../renderPage.js'
+
+const Reports = ({query, userInfo, favorites, updateFavorites}) => {
+  return (
+    <Container>
+      Hello, world!
+    </Container>
+  )
+}
+
+export default renderPage(Reports);
