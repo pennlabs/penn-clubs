d616662f896261557f3b2b4f230d8a00ce156f74
Davis Haupt <davis.haupt@gmail.com>
2019-11-29 15:25:39 -0500

    added Sidebar component and started on reports layout

--- pages/reports.js
+++ pages/reports.js
@@ -2,13 +2,26 @@ import { useState, useEffect } from 'react'
 import s from 'styled-components'
 
 import renderPage from '../renderPage.js'
+import { Sidebar } from '../components/common/Sidebar'
+import { Container, WideContainer } from '../components/common/Container'
+import { CLUBS_GREY } from '../constants/colors'
+
 
 const Reports = ({query, userInfo, favorites, updateFavorites}) => {
   return (
-    <Container>
-      Hello, world!
-    </Container>
+    <div>
+      <Sidebar>
+        Sidebar! 
+      </Sidebar>        
+      <Container>
+        <WideContainer>
+          <p className="title" style={{ color: CLUBS_GREY }}>
+            Create a new report 
+          </p>
+        </WideContainer>
+      </Container>
+    </div>
   )
 }
 
-export default renderPage(Reports);
+export default renderPage(Reports)
