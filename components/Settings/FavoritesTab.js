31a864a0643353760041a0bb26e233d65a42c05d
Eric Wang <ezwang1@gmail.com>
2020-01-02 12:26:32 -0500

    implement subscription settings page

--- components/Settings/FavoritesTab.js
+++ components/Settings/FavoritesTab.js
@@ -1,14 +1,14 @@
 import ClubTableRow from '../ClubTableRow'
 
 export default (props) => {
-  const { updateFavorites, favorites, clubs } = props
+  const { keyword, updateFavorites, favorites, clubs } = props
 
   const findClub = clubs ? code => clubs.find(club => club.code === code) : () => {}
 
   if (!favorites || !favorites.length) {
     return (
       <p className="has-text-light-grey" style={{ paddingTop: 200, textAlign: 'center' }}>
-        No bookmarks yet! Browse clubs <a href="/">here.</a>
+        No {keyword}s yet! Browse clubs <a href="/">here.</a>
       </p>
     )
   }
