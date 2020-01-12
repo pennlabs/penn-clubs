9a1aa7bdca601b7e2dd53554f60a155e491190ff
Eric Wang <ezwang1@gmail.com>
2020-01-11 21:13:54 -0500

    randomize testimonials and hide expand if only one

--- components/ClubPage/Testimonials.js
+++ components/ClubPage/Testimonials.js
@@ -1,21 +1,24 @@
 import s from 'styled-components'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { StrongText, Icon } from '../common'
 import { DARK_GRAY } from '../../constants/colors'
 
 const Wrapper = s.span`
   display: inline-block;
+  position: relative;
+  width: 100%;
 
   svg {
-    width: 10%;
+    width: 25px;
     fill: #E1E2FF;
     stroke: #E1E2FF;
     stroke-width: none;
     stroke-linecap: square;
     stroke-linejoin: square;
     transform: rotate(90deg);
-    margin-top: -3.5rem;
-    float: right;
+    position: absolute;
+    bottom: 2.5rem;
+    right: -0.5rem;
   }
 `
 
@@ -27,7 +30,7 @@ const Quote = s.div`
   border-radius: 10px;
   margin-bottom: 1em;
   margin-right: -1rem;
-  width: 95%;
+  width: 98%;
 `
 
 const Toggle = s.div`
@@ -35,10 +38,32 @@ const Toggle = s.div`
   cursor: pointer;
 `
 
-const Testimonials = props => {
-  const { data } = props
+const FeatherTriangle = () =>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    class="feather feather-triangle">
+    <path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"></path>
+  </svg>
 
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]
+  }
+}
+
+const Testimonials = props => {
+  const { data: originalData } = props
+
+  const [data, setData] = useState(null)
   const [expanded, setExpanded] = useState(false)
+
+  useEffect(() => {
+    const newData = [...originalData]
+    shuffleArray(newData)
+    setData(newData)
+  }, [originalData])
 
   if (!data || !data.length) {
     return <></>
@@ -50,29 +75,19 @@ const Testimonials = props => {
       {expanded ? data.map(({ text }, i) => (
         <Wrapper>
           <Quote key={i}>{text}</Quote>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            class="feather feather-triangle">
-            <path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"></path>
-          </svg>
+          <FeatherTriangle />
         </Wrapper>
       )) : <Wrapper>
         <Quote>{data[0].text}</Quote>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          class="feather feather-triangle">
-          <path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"></path>
-        </svg>
+        <FeatherTriangle />
       </Wrapper>}
-      <Toggle className="is-pulled-right" onClick={() => setExpanded(!expanded)}>
+      {data.length >= 2 && <Toggle className="is-pulled-right" onClick={() => setExpanded(!expanded)}>
         See {expanded ? 'less' : 'more'}{' '}
         <Icon
           alt={expanded ? 'less' : 'more'}
           name={expanded ? 'chevron-up' : 'chevron-down'}
         />
-      </Toggle>
+      </Toggle>}
     </>
   )
 }
