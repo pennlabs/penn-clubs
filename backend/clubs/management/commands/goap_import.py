2baa4b8037cf80a7143ac5ce0ce751e52c1afcc2
Eric Wang <ezwang1@gmail.com>
2020-01-13 11:17:50 -0500

    document import script better

--- backend/clubs/management/commands/goap_import.py
+++ backend/clubs/management/commands/goap_import.py
@@ -30,6 +30,8 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         self.count = 1
         self.club_count = 0
+        self.create_count = 0
+        self.update_count = 0
         self.dry_run = kwargs["dry_run"]
         self.skip_tags = kwargs["skip_tags"]
         self.session = requests.Session()
@@ -43,7 +45,11 @@ class Command(BaseCommand):
         if self.skip_tags:
             self.stdout.write("Skipping tag imports...")
         self.process_url(self.START_URL)
-        self.stdout.write("Imported {} clubs!".format(self.club_count))
+        self.stdout.write(
+            "Imported {} clubs! {} created, {} updated".format(
+                self.club_count, self.create_count, self.update_count
+            )
+        )
 
     def process_url(self, url):
         self.stdout.write("Processing Page {}".format(self.count))
@@ -54,30 +60,42 @@ class Command(BaseCommand):
         soup = BeautifulSoup(resp.content, "html.parser")
         grps = soup.select(".grpl .grpl-grp")
         for grp in grps:
+            # parse name
             name = grp.select_one("h3 a").text.strip()
+
+            # parse image url
             image_url = urljoin(url, grp.select_one("img")["src"]).strip()
             if image_url.endswith("/group_img.png"):
                 image_url = None
+
+            # parse tag
             group_tag = grp.select_one(".grpl-type")
             if group_tag is not None:
                 group_type = group_tag.text.strip()
             else:
                 group_type = None
+
+            # parse description
             description = grp.select_one(".grpl-purpose").text.replace("\r\n", "\n").strip()
             if description == "This group has not written a purpose":
                 description = ""
             else:
                 description = clean(description)
+
+            # parse email contact
             contact_tag = grp.select_one(".grpl-contact")
             if contact_tag is not None:
                 contact_email = contact_tag.text.strip()
             else:
                 contact_email = None
 
+            # create or update tag
             if group_type is not None and not self.dry_run and not self.skip_tags:
                 tag = Tag.objects.get_or_create(name=group_type)[0]
             else:
                 tag = None
+
+            # create or update club
             clubs = Club.objects.filter(name__iexact=name)
             if clubs.exists():
                 if clubs.count() > 1:
@@ -100,32 +118,47 @@ class Command(BaseCommand):
                 club.name = name
             if not club.description:
                 club.description = description
+
+            # only update image if existing image is nonexistent/broken link
+            # if image is local and set, assume that it exists
             use_image = False
             if image_url:
                 if not self.dry_run:
                     if club.image:
-                        resp = requests.head(image_url, allow_redirects=True)
-                        use_image = not resp.ok
+                        if club.image.url.startswith("http"):
+                            resp = requests.head(club.image.url, allow_redirects=True)
+                            use_image = not resp.ok
+                        else:
+                            use_image = False
                     else:
                         use_image = True
+
                     if use_image:
                         resp = requests.get(image_url, allow_redirects=True)
                         resp.raise_for_status()
                         club.image.save(os.path.basename(image_url), ContentFile(resp.content))
+
+            # update email if there is no email
             if not club.email:
                 club.email = contact_email
 
             # mark newly created clubs as inactive (has no owner)
             if flag:
                 club.active = False
+
             if not self.dry_run:
                 club.save()
                 if tag is not None and not club.tags.count():
                     club.tags.set([tag])
+
             self.club_count += 1
             self.stdout.write(
                 "{} '{}' (image: {})".format("Created" if flag else "Updated", name, use_image)
             )
+            if flag:
+                self.create_count += 1
+            else:
+                self.update_count += 1
 
         next_tag = soup.find(text="Next >")
         if next_tag is not None:
