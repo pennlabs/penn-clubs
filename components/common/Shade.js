8178062e624e11b3240139a12955f6e44512cb57
Daniel Tao <dtao@seas.upenn.edu>
2020-01-12 22:55:51 -0600

    extract ModalBackground into Shade component

---
+++ components/common/Shade.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+import { ALLBIRDS_GRAY } from '../../constants/colors'
+import { LONG_ANIMATION_DURATION } from '../../constants/measurements'
+import { fadeIn, fadeOut } from './Animations'
+
+export const Shade = styled.div`
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  overflow: hidden;
+  background: ${ALLBIRDS_GRAY};
+  z-index: 1007;
+  text-align: center;
+  animation-name: ${({ isNewlyMounted, show }) => {
+    if (isNewlyMounted) {
+      return ''
+    }
+    return show ? fadeIn : fadeOut
+  }};
+  animation-duration: ${LONG_ANIMATION_DURATION};
+  opacity: ${({ show }) => (show ? '.75' : '0')};
+`
