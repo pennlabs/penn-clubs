8864337b32d7416b28fab5d1b3c957ee6c1d66c0
Daniel Tao <dtao@seas.upenn.edu>
2020-01-14 00:24:23 -0600

    remove selectedTags from TagGroup

--- frontend/components/common/TagGroup.js
+++ frontend/components/common/TagGroup.js
@@ -1,62 +1,22 @@
 import PropTypes from 'prop-types'
-import { BlueTag, SelectedTag } from './Tags'
+import { BlueTag } from './Tags'
 
-export const TagGroup = ({ tags = [], selectedTags = [], updateTag }) => {
+export const TagGroup = ({ tags = [] }) => {
   if (!tags || !tags.length) return null
-
-  // TODO: Use same tag format between DropdownFilter and TagGroup
-  return tags.map(tag => {
-    const matchedTag = selectedTags.find(
-      ({ value, name: filterType }) => filterType === 'Type' && value === tag.id
-    )
-    const key = `${tag.id}-${tag.value}`
-    if (matchedTag) {
-      return (
-        <SelectedTag
-          key={key}
-          className="tag is-rounded has-text-white"
-          onClick={e => {
-            // Prevent click event from propagating so clicking on the tag doesn't
-            // fire the generic club handle click
-            e.preventDefault()
-            e.stopPropagation()
-            updateTag && updateTag(matchedTag, matchedTag.name)
-          }}
-        >
-          {tag.name}
-          <button className="delete is-small" />
-        </SelectedTag>
-      )
-    }
-    return (
+  return <>
+    {tags.map(tag => (
       <BlueTag
-        key={key}
+        key={`${tag.id}-${tag.value}`}
         className="tag is-rounded has-text-white"
-        onClick={e => {
-          // Stop propagation of click event for same reasons as above
-          e.preventDefault()
-          e.stopPropagation()
-          updateTag &&
-            updateTag(
-              {
-                value: tag.id,
-                label: tag.name,
-                name: 'Type',
-              },
-              'Type'
-            )
-        }}
       >
         {tag.name}
       </BlueTag>
-    )
-  })
+    ))}
+  </>
 }
 
 TagGroup.defaultProps = {
-  selectedTags: [],
   tags: [],
-  updateTag: undefined,
 }
 
 TagGroup.propTypes = {
@@ -66,10 +26,4 @@ TagGroup.propTypes = {
       id: PropTypes.number,
     })
   ),
-  selectedTags: PropTypes.arrayOf(
-    PropTypes.shape({
-      value: PropTypes.string,
-    })
-  ),
-  updateTag: PropTypes.func,
 }
