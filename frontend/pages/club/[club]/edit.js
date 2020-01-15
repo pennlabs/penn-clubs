9c59095248c1de44d122bbb708dd6c7d95bc28d8
Campbell Phalen <cphalen@seas.upenn.edu>
2020-01-15 18:26:59 -0500

    Added the ability to delete events

--- frontend/pages/club/[club]/edit.js
+++ frontend/pages/club/[club]/edit.js
@@ -96,6 +96,7 @@ class ClubForm extends Component {
     }
     this.submit = this.submit.bind(this)
     this.submitEvent = this.submitEvent.bind(this)
+    this.deleteEvent = this.deleteEvent.bind(this)
     this.notify = this.notify.bind(this)
     this.sendInvites = this.sendInvites.bind(this)
   }
