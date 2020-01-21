216f201bb9b05f2eaf65ec53c729deeacf758da5
Daniel Tao <dtao@seas.upenn.edu>
2020-01-21 13:08:25 -0500

    add EmptyState to club members tab

--- frontend/components/ClubPage/Tabs.js
+++ frontend/components/ClubPage/Tabs.js
@@ -1,8 +1,9 @@
 import { EMPTY_DESCRIPTION } from '../../utils'
+import { EmptyState } from '../common'
 import TabView from '../TabView'
 import MemberCard from './MemberCard'
 
-export default props => (
+export default ({ club }) => (
   <TabView
     tabs={[
       {
@@ -12,16 +13,16 @@ export default props => (
             <div
               style={{ whiteSpace: 'pre-wrap' }}
               dangerouslySetInnerHTML={{
-                __html: props.club.description || EMPTY_DESCRIPTION,
+                __html: club.description || EMPTY_DESCRIPTION,
               }}
             />
-            {props.club.how_to_get_involved && (
+            {club.how_to_get_involved && (
               <div>
                 <div style={{ marginTop: 20 }}>
                   <b>Getting Involved</b>
                 </div>
                 <div style={{ whiteSpace: 'pre-wrap' }}>
-                  {props.club.how_to_get_involved}
+                  {club.how_to_get_involved}
                 </div>
               </div>
             )}
@@ -32,13 +33,36 @@ export default props => (
         name: 'members',
         content: (
           <div>
-            {props.club.members.length ? (
-              props.club.members.map((a, i) => <MemberCard a={a} i={i} />)
+            {club.members.length ? (
+              club.members.map((a, i) => (
+                <div className="media" key={i}>
+                  <div className="media-left">
+                    <figure className="has-background-light image is-48x48"></figure>
+                  </div>
+                  <div className="media-content">
+                    <p className="title is-4">{a.name || 'No Name'}</p>
+                    <p className="subtitle is-6">
+                      {a.email ? (
+                        <span>
+                          <a href={'mailto:' + a.email}>{a.email}</a> ({a.title}
+                          )
+                        </span>
+                      ) : (
+                          a.title
+                        )}
+                    </p>
+                  </div>
+                </div>
+              ))
             ) : (
-              <p>
-                No club members have linked their accounts on Penn Clubs yet.
-                Check back later for a list of club members!
-              </p>
+                <>
+                  <EmptyState name="hiring" size="25%" style={{ marginTop: 0, marginBottom: 0 }} />
+                  <p style={{ textAlign: 'center' }}>
+                    No club members have linked their accounts on Penn Clubs yet.
+                    <br />
+                    Check back later for a list of club members!
+                  </p>
+                </>
             )}
           </div>
         ),
