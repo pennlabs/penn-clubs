4ff764ec4961b228b4fa8a2c40234cffcfc2981b
Eric Wang <ezwang1@gmail.com>
2020-01-24 18:10:41 -0500

    try fix

--- backend/clubs/management/commands/merge_duplicates.py
+++ backend/clubs/management/commands/merge_duplicates.py
@@ -160,10 +160,10 @@ def merge_clubs(one, two):
     primary.tags.add(*secondary.tags.all())
 
     # Take all members
-    Membership.objects.filter(
-        club=secondary,
-        person__in=Membership.objects.filter(club=primary).values_list("person__id", flat=True),
-    ).delete()
+    duplicate_memberships = Membership.objects.filter(club=primary).values_list(
+        "person__id", flat=True
+    )
+    Membership.objects.filter(club=secondary, person__in=duplicate_memberships,).delete()
     Membership.objects.filter(club=secondary).update(club=primary)
 
     # Take all membership invites
