7e6d1962905fc9e5f489fff68e94f5f29cf3cec0
Eric Wang <ezwang1@gmail.com>
2020-01-24 18:14:35 -0500

    force evaluation

--- backend/clubs/management/commands/merge_duplicates.py
+++ backend/clubs/management/commands/merge_duplicates.py
@@ -160,8 +160,8 @@ def merge_clubs(one, two):
     primary.tags.add(*secondary.tags.all())
 
     # Take all members
-    duplicate_memberships = Membership.objects.filter(club=primary).values_list(
-        "person__id", flat=True
+    duplicate_memberships = list(
+        Membership.objects.filter(club=primary).values_list("person__id", flat=True)
     )
     Membership.objects.filter(club=secondary, person__in=duplicate_memberships,).delete()
     Membership.objects.filter(club=secondary).update(club=primary)
