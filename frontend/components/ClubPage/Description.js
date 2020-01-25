4af5cbaa4fc67070e4e1b42d81037509ebb65e50
Daniel Tao <dtao@seas.upenn.edu>
2020-01-25 13:30:51 -0500

    destructure club out of props in Description

--- frontend/components/ClubPage/Description.js
+++ frontend/components/ClubPage/Description.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-import { EMPTY_DESCRIPTION } from '../../utils'
-import { EmptyState, Center } from '../common'
-import TabView from '../TabView'
-import MemberCard from './MemberCard'
-=======
->>>>>>> code cleanup
 import s from 'styled-components'
 import { StrongText } from '../common'
 import { EMPTY_DESCRIPTION } from '../../utils'
@@ -17,87 +10,26 @@ const Wrapper = s.div`
   flex: 1;
 `
 
-<<<<<<< HEAD
-export default ({
-  club: {
-    description,
-    how_to_get_involved: involvedText,
-    members,
-  },
-}) => (
-  <TabView
-    tabs={[
-      {
-        name: 'description',
-        content: (
-          <div>
-            <div
-              style={{ whiteSpace: 'pre-wrap' }}
-              dangerouslySetInnerHTML={{
-                __html: description || EMPTY_DESCRIPTION,
-              }}
-            />
-            {involvedText && (
-              <div>
-                <div style={{ marginTop: 20 }}>
-                  <b>Getting Involved</b>
-                </div>
-                <div style={{ whiteSpace: 'pre-wrap' }}>
-                  {involvedText}
-                </div>
-              </div>
-            )}
-=======
-export default props => (
+export default ({ club }) => (
   <Wrapper>
     <div style={{ padding: '10px' }}>
       <StrongText>Description</StrongText>
       <div
         style={{ whiteSpace: 'pre-wrap' }}
         dangerouslySetInnerHTML={{
-          __html: props.club.description || EMPTY_DESCRIPTION,
+          __html: club.description || EMPTY_DESCRIPTION,
         }}
       />
-      {props.club.how_to_get_involved && (
+      {club.how_to_get_involved && (
         <div>
           <div style={{ marginTop: 20 }}>
             <b>Getting Involved</b>
           </div>
           <div style={{ whiteSpace: 'pre-wrap' }}>
-            {props.club.how_to_get_involved}
->>>>>>> header fix and kill tabs temporary
+            {club.how_to_get_involved}
           </div>
         </div>
       )}
-    </div>{' '}
-<<<<<<< HEAD
-  </Wrapper> /*
-        ),
-      },
-      {
-        name: 'members',
-        content: (
-          <div>
-            {props.club.members.length ? (
-              props.club.members.map((a, i) => <MemberCard a={a} key={i} />)
-            ) : (
-                <>
-                  <Center>
-                    <EmptyState name="hiring" size="25%" style={{ marginTop: 0, marginBottom: 0 }} />
-                    <p>
-                      No club members have linked their accounts on Penn Clubs yet.
-                      <br />
-                      Check back later for a list of club members!
-                    </p>
-                  </Center>
-                </>
-            )}
-          </div>
-        ),
-      },
-    ]}
-  />*/
-=======
+    </div>
   </Wrapper>
->>>>>>> code cleanup
 )
