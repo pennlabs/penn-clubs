9c721792c42fbc37909ffd2008c55fafa9d4d309
Eric Wang <ezwang1@gmail.com>
2020-01-30 15:58:09 -0500

    fix prebuild

--- frontend/components/common/Icon.js
+++ frontend/components/common/Icon.js
@@ -3,14 +3,19 @@ import s from 'styled-components'
 
 const reqSvgs = require.context('../../public/static/img/icons', true, /\.svg$/)
 
-export const Icon = ({ name, size = '1rem', ...props }) =>
-  reqSvgs(`./${name}.svg`).default({
+export const Icon = ({ name, size = '1rem', ...props }) => {
+  const svg = reqSvgs(`./${name}.svg`)
+  if (!svg || !svg.default) {
+    return null
+  }
+  return svg.default({
     preserveAspectRatio: true,
     width: size,
     height: size,
     viewBox: '0 0 24 24',
     ...props,
   })
+}
 
 Icon.defaultProps = {
   style: {},
