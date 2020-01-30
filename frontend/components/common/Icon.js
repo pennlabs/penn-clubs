16d839d681e3cfac23aa503a0936a6452fd568ec
Eric Wang <ezwang1@gmail.com>
2020-01-30 15:51:13 -0500

    embed svg inline

--- frontend/components/common/Icon.js
+++ frontend/components/common/Icon.js
@@ -1,49 +1,20 @@
 import PropTypes from 'prop-types'
 import s from 'styled-components'
 
-const IconTag = s.img`
-  ${({ size }) => `
-    height: ${size || '1rem'};
-    width: ${size || '1rem'};
-  `}
-  object-fit: contain;
-  margin-bottom: 0;
-  transform: translateY(2.5px);
+const reqSvgs = require.context('../../public/static/img/icons', true, /\.svg$/)
 
-  .button.is-primary &,
-  .button.is-link &,
-  .button.is-info &,
-  .button.is-success &,
-  .button.is-danger & {
-    filter: invert(100%);
-    margin-right: 3px;
-    margin-top: -5px;
-  }
-
-  .button.is-large & {
-    ${({ size }) => `
-      height: ${size || '1.5rem'};
-      width: ${size || '1.5rem'};
-      margin-right: 7px;
-      margin-top: -7px;
-    `}
-  }
-`
-
-export const Icon = ({ name, alt, size, style, className, ...props }) => (
-  <IconTag
-    src={`/static/img/icons/${name}.svg`}
-    alt={alt}
-    className={className}
-    style={style}
-    size={size}
-    {...props}
-  />
-)
+export const Icon = ({ name, size = '1rem', ...props }) =>
+  reqSvgs(`./${name}.svg`).default({
+    preserveAspectRatio: true,
+    width: size,
+    height: size,
+    viewBox: '0 0 24 24',
+    ...props,
+  })
 
 Icon.defaultProps = {
   style: {},
-  size: undefined,
+  size: '1rem',
   alt: '',
 }
 
