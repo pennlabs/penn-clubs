e6eed1cabc569448185c4032494f9e79817314d9
Valencia <valenciafu@gmail.com>
2020-01-29 21:14:13 -0500

    final wording changes

--- frontend/pages/rank.js
+++ frontend/pages/rank.js
@@ -27,40 +27,40 @@ const Rank = () => (
     </Title>
     <StrongText>How are clubs ordered?</StrongText>
     <Text>
-      The order that clubs appear on the home page is controlled by several
+      The order that clubs appear on the home page is determined by several
       criteria. A recommendation algorithm exists to ensure that students receive the
-      best experience when browsing for new clubs and that clubs can best
-      reach the members that they want to recruit.
+      best experience when browsing for new clubs and that clubs can effectively
+      reach their target demographic.
     </Text>
     <StrongText>How does the club recommendation algorithm work?</StrongText>
     <Text>
       The reccomendation algorithm uses the following criteria to determine how to
-      order the club list. The criteria are as follows:
+      order clubs on the home page. The criteria are:
       {[
         {
           name: 'Matches Target Schools',
           description:
-            'Adding target schools will increase the ranking of the club for students in those schools. Clubs that have specified less schools will be ranked higher than clubs that have specified more schools for relevant students. Specifying all of the schools is the same as specifying none of them.',
+            'Adding target schools will cause the club to appear higher on the home page for students in those schools. Clubs that have specified fewer schools are more likely to appear higher than clubs that have specified more schools, for relevant students. Specifying all of the schools is the same as specifying none of them.',
         },
         {
           name: 'Matches Target Majors',
           description:
-            'Adding target majors will increase the ranking of the club for students in those majors. Clubs that have specified less majors will be ranked higher than clubs that have specified more majors for relevant students. Specifying 10 or more majors is the same as specifying no majors.',
+            'Adding target majors will cause the club to appear higher on the home page for students in those majors. Clubs that have specified fewer majors are more likely to appear higher than clubs that have specified more majors, for relevant students. Specifying 10 or more majors is the same as specifying no majors.',
         },
         {
           name: 'Matches Target Years',
           description:
-            'Adding target years will increase the ranking of the club for students in those years. Clubs that have specified less years will be ranked higher than clubs that have specified more years for relevant students. Specifying all of the years is the same as specifying none of them.',
+            'Adding target years will cause the club to appear higher on the home page for students in those years. Clubs that have specified fewer years are more likely to appear higher than clubs that have specified more years, for relevant students. Specifying all of the years is the same as specifying none of them.',
         },
         {
           name: 'Has Description',
           description:
-            'Adding a description will help students learn more about whether or not a club is a good fit for them. Clubs without a description will therefore appear lower in the rankings.',
+            'Adding a description helps students learn more about whether or not a club is a good fit for them. Clubs without a description will therefore appear lower on the homepage.',
         },
         {
           name: 'Is Club Active',
           description:
-            'Clubs that are marked as inactive will be shifted to the bottom of the list. You can easily reactivate your club if it has been marked as inactive.',
+            'Clubs that are marked as inactive will be shifted to the bottom of the list. You can easily reactivate your club from the "Edit Club" page if it has been marked as inactive.',
         },
         {
           name: 'Random Factor',
