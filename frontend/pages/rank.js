413966b057242a524002b9a56c0b55b1d74a6cf8
Eric Wang <ezwang1@gmail.com>
2020-01-30 13:25:25 -0500

    fix console log errors

--- frontend/pages/rank.js
+++ frontend/pages/rank.js
@@ -36,45 +36,45 @@ const Rank = () => (
     <Text>
       The reccomendation algorithm uses the following criteria to determine how
       to order clubs on the home page. The criteria are:
-      {[
-        {
-          name: 'Matches Target Schools',
-          description:
-            'Adding target schools will cause the club to appear higher on the home page for students in those schools. Clubs that have specified fewer schools are more likely to appear higher than clubs that have specified more schools, for relevant students. Specifying all of the schools is the same as specifying none of them.',
-        },
-        {
-          name: 'Matches Target Majors',
-          description:
-            'Adding target majors will cause the club to appear higher on the home page for students in those majors. Clubs that have specified fewer majors are more likely to appear higher than clubs that have specified more majors, for relevant students. Specifying 10 or more majors is the same as specifying no majors.',
-        },
-        {
-          name: 'Matches Target Years',
-          description:
-            'Adding target years will cause the club to appear higher on the home page for students in those years. Clubs that have specified fewer years are more likely to appear higher than clubs that have specified more years, for relevant students. Specifying all of the years is the same as specifying none of them.',
-        },
-        {
-          name: 'Has Description',
-          description:
-            'Adding a description helps students learn more about whether or not a club is a good fit for them. Clubs without a description will therefore appear lower on the homepage.',
-        },
-        {
-          name: 'Is Club Active',
-          description:
-            'Clubs that are marked as inactive will be shifted to the bottom of the list. You can easily reactivate your club from the "Edit Club" page if it has been marked as inactive.',
-        },
-        {
-          name: 'Random Factor',
-          description:
-            'A random factor is applied each time the home page loads in order to ensure that students see new clubs every time they visit the site.',
-        },
-      ].map(({ name, description }) => (
-        <RankItem>
-          <LargeIcon name="check-circle-green" alt="check" />
-          <b>{name}</b>
-          <Text>{description}</Text>
-        </RankItem>
-      ))}
     </Text>
+    {[
+      {
+        name: 'Matches Target Schools',
+        description:
+          'Adding target schools will cause the club to appear higher on the home page for students in those schools. Clubs that have specified fewer schools are more likely to appear higher than clubs that have specified more schools, for relevant students. Specifying all of the schools is the same as specifying none of them.',
+      },
+      {
+        name: 'Matches Target Majors',
+        description:
+          'Adding target majors will cause the club to appear higher on the home page for students in those majors. Clubs that have specified fewer majors are more likely to appear higher than clubs that have specified more majors, for relevant students. Specifying 10 or more majors is the same as specifying no majors.',
+      },
+      {
+        name: 'Matches Target Years',
+        description:
+          'Adding target years will cause the club to appear higher on the home page for students in those years. Clubs that have specified fewer years are more likely to appear higher than clubs that have specified more years, for relevant students. Specifying all of the years is the same as specifying none of them.',
+      },
+      {
+        name: 'Has Description',
+        description:
+          'Adding a description helps students learn more about whether or not a club is a good fit for them. Clubs without a description will therefore appear lower on the homepage.',
+      },
+      {
+        name: 'Is Club Active',
+        description:
+          'Clubs that are marked as inactive will be shifted to the bottom of the list. You can easily reactivate your club from the "Edit Club" page if it has been marked as inactive.',
+      },
+      {
+        name: 'Random Factor',
+        description:
+          'A random factor is applied each time the home page loads in order to ensure that students see new clubs every time they visit the site.',
+      },
+    ].map(({ name, description }) => (
+      <RankItem key={name}>
+        <LargeIcon name="check-circle-green" alt="check" />
+        <b>{name}</b>
+        <Text>{description}</Text>
+      </RankItem>
+    ))}
   </Container>
 )
 
