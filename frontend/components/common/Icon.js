20988885837be4c6941b765fe7908c9e360daabe
Eric Wang <ezwang1@gmail.com>
2020-01-30 19:33:47 -0500

    fix icons

--- frontend/components/common/Icon.js
+++ frontend/components/common/Icon.js
@@ -3,19 +3,36 @@ import s from 'styled-components'
 
 const reqSvgs = require.context('../../public/static/img/icons', true, /\.svg$/)
 
+const IconWrapper = s.span`
+  display: inline-block;
+
+  .button & {
+    margin-right: 0.25rem;
+  }
+
+  & svg {
+    display: block;
+    margin: auto;
+  }
+`
+
 export const Icon = ({ name, size = '1rem', ...props }) => {
   const svg = reqSvgs(`./${name}.svg`)
   if (!svg || !svg.default) {
     return null
   }
   const iconInfo = svg.default().props
-  return svg.default({
-    preserveAspectRatio: 'true',
-    width: size,
-    height: size,
-    viewBox: `0 0 ${iconInfo.width} ${iconInfo.height}`,
-    ...props,
-  })
+  return (
+    <IconWrapper>
+      {svg.default({
+        preserveAspectRatio: 'true',
+        width: size,
+        height: size,
+        viewBox: `0 0 ${iconInfo.width} ${iconInfo.height}`,
+        ...props,
+      })}
+    </IconWrapper>
+  )
 }
 
 Icon.defaultProps = {
