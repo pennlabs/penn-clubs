da7c22fd2395215265e6bf9090c57652ff28a079
Chirality <danieltaox@gmail.com>
2020-01-30 20:40:26 -0500

    convert Head to functional component

--- frontend/components/Header/Head.js
+++ frontend/components/Header/Head.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Head from 'next/head'
 
 import { initGA, logPageView } from '../../utils/analytics'
@@ -34,56 +34,56 @@ const type = 'website'
 const twitterUsername = '@pennlabs'
 const twitterCardType = 'summary'
 
-export default class Heading extends React.Component {
-  componentDidMount() {
+const Heading = () => {
+  useEffect(() => {
     if (!window.GA_INITIALIZED) {
       initGA()
       window.GA_INITIALIZED = true
     }
     logPageView()
-  }
+  }, [])
 
-  render() {
-    return (
-      <Head>
-        <title>{title}</title>
+  return (
+    <Head>
+      <title>{title}</title>
 
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0" />
-        <meta name="theme-color" content="#ffffff" />
-        <meta name="description" content={description} />
-        <meta name="keywords" content={keywords} />
-        <meta name="author" content={author} />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0" />
+      <meta name="theme-color" content="#ffffff" />
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+      <meta name="author" content={author} />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-        <meta property="og:title" content={title} />
-        <meta property="og:type" content={type} />
-        <meta property="og:url" content={url} />
-        <meta property="og:image" content={image} />
-        <meta property="og:image:alt" content={imageAlt} />
+      <meta property="og:title" content={title} />
+      <meta property="og:type" content={type} />
+      <meta property="og:url" content={url} />
+      <meta property="og:image" content={image} />
+      <meta property="og:image:alt" content={imageAlt} />
 
-        <meta property="twitter:site" content={twitterUsername} />
-        <meta property="twitter:description" content={description} />
-        <meta property="twitter:title" content={title} />
-        <meta property="twitter:image" content={image} />
-        <meta property="twitter:image:alt" content={imageAlt} />
-        <meta property="twitter:card" content={twitterCardType} />
+      <meta property="twitter:site" content={twitterUsername} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:image" content={image} />
+      <meta property="twitter:image:alt" content={imageAlt} />
+      <meta property="twitter:card" content={twitterCardType} />
 
-        <link rel="shortcut icon" href="/static/favicon.ico" />
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css"
-        />
-        <link
-          href="https://fonts.googleapis.com/css?family=Roboto&display=swap"
-          rel="stylesheet"
-        />
-        <script
-          src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-          integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-          crossOrigin="anonymous"
-        />
-      </Head>
-    )
-  }
+      <link rel="shortcut icon" href="/static/favicon.ico" />
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css"
+      />
+      <link
+        href="https://fonts.googleapis.com/css?family=Roboto&display=swap"
+        rel="stylesheet"
+      />
+      <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossOrigin="anonymous"
+      />
+    </Head>
+  )
 }
+
+export default Heading
