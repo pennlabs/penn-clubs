db7c262329f1546bd85ea52c69cac5fa20f64240
Eric Wang <ezwang1@gmail.com>
2020-02-01 13:02:28 -0500

    move login to commands

--- frontend/cypress/integration/pages_spec.js
+++ frontend/cypress/integration/pages_spec.js
@@ -22,36 +22,7 @@ describe('Page load tests', () => {
 
 describe('Authenticated user tests', () => {
   before(() => {
-    cy.visit('/')
-    cy.contains('Login')
-      .invoke('attr', 'href')
-      .then(href => {
-        cy.request({
-          method: 'GET',
-          url: href,
-        }).then(data => {
-          cy.window().then(win => {
-            const ele = win.document.createElement('div')
-            ele.innerHTML = data.body
-            const form = new win.FormData(ele.querySelector('form'))
-            const formattedData = {}
-            Array.from(form.entries()).forEach(a => {
-              formattedData[a[0]] = a[1]
-            })
-            formattedData.allow = 'Authorize'
-            const url = data.redirects[data.redirects.length - 1].substring(5)
-            cy.request({
-              method: 'POST',
-              url: url,
-              form: true,
-              headers: {
-                Referer: url,
-              },
-              body: formattedData,
-            })
-          })
-        })
-      })
+    cy.login()
   })
 
   beforeEach(() => {
--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -10,7 +10,38 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
+Cypress.Commands.add('login', () => {
+  cy.visit('/')
+  cy.contains('Login')
+    .invoke('attr', 'href')
+    .then(href => {
+      cy.request({
+        method: 'GET',
+        url: href,
+      }).then(data => {
+        cy.window().then(win => {
+          const ele = win.document.createElement('div')
+          ele.innerHTML = data.body
+          const form = new win.FormData(ele.querySelector('form'))
+          const formattedData = {}
+          Array.from(form.entries()).forEach(a => {
+            formattedData[a[0]] = a[1]
+          })
+          formattedData.allow = 'Authorize'
+          const url = data.redirects[data.redirects.length - 1].substring(5)
+          cy.request({
+            method: 'POST',
+            url: url,
+            form: true,
+            headers: {
+              Referer: url,
+            },
+            body: formattedData,
+          })
+        })
+      })
+    })
+})
 //
 //
 // -- This is a child command --
