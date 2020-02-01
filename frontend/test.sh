f3a6e10d2543f66d41a1721a90bdd18ba3ce4fb1
Eric Wang <ezwang1@gmail.com>
2020-02-01 12:07:00 -0500

    timeouts in ms not seconds

--- frontend/test.sh
+++ frontend/test.sh
@@ -4,11 +4,11 @@
 pushd ../backend
 pipenv install --dev
 pipenv run ./manage.py migrate
-pipenv run ./manage.py runserver & npx wait-on -s 3 -d 1 -t 30 http://localhost:8000/api
+pipenv run ./manage.py runserver & npx wait-on -s 3 -d 500 -t 30000 http://localhost:8000/api
 popd
 
 # Setup frontend server
-node server.js & npx wait-on -d 1 -t 30 http://localhost:3000
+node server.js & npx wait-on -s 3 -d 500 -t 30000 http://localhost:3000
 
 # Run tests
 npx cypress run
