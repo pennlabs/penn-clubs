f7b95028175c1cd20d03ece82680e05da137b33e
Daniel Tao <dtao@seas.upenn.edu>
2020-02-07 04:10:33 -0500

    mock up recommended search options

--- frontend/components/FilterSearch.js
+++ frontend/components/FilterSearch.js
@@ -1,33 +1,33 @@
 import { useState, useRef, useEffect } from 'react'
 import s from 'styled-components'
-import Select from 'react-select'
+import Select from 'react-select/async'
 
 import { Icon } from './common'
 import { FilterHeader } from './DropdownFilter'
 import {
-    BORDER_RADIUS,
-    mediaMaxWidth,
-    MD,
-    NAV_HEIGHT,
-    mediaMinWidth,
-    SEARCH_BAR_MOBILE_HEIGHT,
-    ANIMATION_DURATION,
+  BORDER_RADIUS,
+  mediaMaxWidth,
+  MD,
+  NAV_HEIGHT,
+  mediaMinWidth,
+  SEARCH_BAR_MOBILE_HEIGHT,
+  ANIMATION_DURATION,
 } from '../constants/measurements'
 import {
-    ALLBIRDS_GRAY,
-    MEDIUM_GRAY,
-    FOCUS_GRAY,
-    CLUBS_GREY,
-    BORDER,
-    WHITE,
-    CLUBS_RED,
+  ALLBIRDS_GRAY,
+  MEDIUM_GRAY,
+  FOCUS_GRAY,
+  CLUBS_GREY,
+  BORDER,
+  WHITE,
+  CLUBS_RED,
 } from '../constants/colors'
 
 const SearchWrapper = s.div`
   margin-bottom: 30px;
   max-height: 0;
   opacity: 0;
-  overflow: hidden;
+  overflow: visible;
   transition: all ${ANIMATION_DURATION}ms ease;
 
   ${({ active }) => active && 'max-height: 150vh; opacity: 1;'}
@@ -48,50 +48,45 @@ const SearchIcon = s.span`
   }
 `
 
-{/* <SearchIcon>
-                {isTextInSearchBar ? (
-                  <Icon
-                    name="x"
-                    alt="cancel search"
-                    onClick={() => {
-                      setNameInput('')
-                      focus()
-                    }}
-                  />
-                ) : (
-                  <Icon name="tag" alt="" onClick={focus} />
-                )}
-              </SearchIcon> */}
-
-const Filter = ({ active, toggleActive, tags }) => {
-    const [nameInput, setNameInput] = useState('')
-    const isTextInSearchBar = Boolean(nameInput)
-    console.log(tags)
-    const options = [
-        { value: 'chocolate', label: 'Chocolate' },
-        { value: 'strawberry', label: 'Strawberry' },
-        { value: 'vanilla', label: 'Vanilla' }
-      ]
-      
-    return (
+const Filter = ({ active, toggleActive, tags, updateTag }) => {
+    const [defaultTags] = useState(() => {
+        const shuffled = tags.sort(() => 0.5 - Math.random());
+        return [{
+            label: "Suggested for you",
+            options: shuffled.slice(0, Math.min(shuffled.length, 3))
+        }]
+    })
+    const searchTags = async query => {
+        // TODO: actually search tags
+        return tags;
+    }
+  return (
         <>
             <FilterHeader
-                active={active}
-                color={CLUBS_RED}
-                name="Tags"
-                toggleActive={toggleActive}
+              active={active}
+              color={CLUBS_RED}
+              name="Tags"
+              toggleActive={toggleActive}
             />
             <SearchWrapper active={active}>
-                <Select
-                    components={{
-                        IndicatorSeparator: () => null,
-                        DropdownIndicator: () => <SearchIcon><Icon name="tag" alt=""/></SearchIcon>
-                    }}
-                    options={[options]}
-                />
+              <Select
+                isMulti
+                cacheOptions
+                loadOptions={searchTags}
+                components={{
+                  IndicatorSeparator: () => null,
+                  DropdownIndicator: () => <SearchIcon><Icon name="tag" alt=""/></SearchIcon>,
+                }}
+                defaultOptions={defaultTags}
+                onChange={(_, action) => {
+                    action.option && updateTag(action.option, "Tags")
+                }}
+                value={[]}
+                placeholder="Search for tags"
+              />
             </SearchWrapper>
         </>
-    )
+  )
 }
 
 export default Filter
