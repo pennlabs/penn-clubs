5041f8ac0937a06b83e57458e28e252380cac9ad
Eric Wang <ezwang1@gmail.com>
2020-02-08 11:21:51 -0500

    shorter check

--- backend/clubs/serializers.py
+++ backend/clubs/serializers.py
@@ -336,7 +336,7 @@ class ClubListSerializer(serializers.ModelSerializer):
     def get_fields(self):
         all_fields = super().get_fields()
         fields_param = getattr(self.context.get("request", dict()), "GET", {}).get("fields", "")
-        if len(fields_param) > 0:
+        if fields_param:
             fields_param = fields_param.split(",")
         else:
             return all_fields
