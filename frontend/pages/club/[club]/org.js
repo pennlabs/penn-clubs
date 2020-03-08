32dc3490cceb7e41a3f9b16b79310da2aa94e30b
Eric Wang <ezwang1@gmail.com>
2020-03-08 12:23:00 -0400

    implement orgs

--- frontend/pages/club/[club]/org.js
+++ frontend/pages/club/[club]/org.js
@@ -1,46 +1,99 @@
 import renderPage from '../../../renderPage'
 import { doApiRequest } from '../../../utils'
-import React, { Component } from 'react'
-import { Loading } from '../../../components/common'
-import OrgChildren from '../../../components/OrgPage/OrgChildren'
+import { useState, useEffect } from 'react'
+import Link from 'next/link'
+import { Title, Loading, WideContainer, Icon } from '../../../components/common'
+import s from 'styled-components'
 
-class Org extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      club: null,
-      children: null,
-    }
+const Subtitle = s(Title)`
+  font-size: 1.5em;
+  font-weight: normal;
+  margin-top: 1em;
+  margin-bottom: 0.5em;
+`
+
+const OrgChildWrapper = s.div`
+  margin-left: 1em;
+
+  & .entry {
+    display: block;
+    border-radius: 3px;
+    border: 1px solid #ccc;
+    margin-bottom: 0.5em;
+    padding: 5px 8px;
   }
 
-  componentDidMount() {
-    doApiRequest(`/clubs/${this.props.query.club}/?format=json`)
-      .then(resp => resp.json())
-      .then(data => {
-        this.setState({ club: data })
-      })
-    doApiRequest(`/clubs/${this.props.query.club}/children/?format=json`)
-      .then(resp => {
-        console.log(resp)
-        return resp.json()
-      })
-      .then(data => {
-        this.setState({ children: data.children })
-      })
+  & .entry a {
+    vertical-align: middle;
   }
 
-  render() {
-    const { club, children } = this.state
-    if (!club || !children) {
-      return <Loading />
-    }
-    return <OrgChildren children={children} />
+  & .entry:hover {
+    background-color: #eee;  
   }
+`
+
+const OrgChild = ({ name, code, children, isParent }) => {
+  return (
+    <OrgChildWrapper>
+      <div className="entry">
+        {(!children || !children.length) && (
+          <>
+            <Icon name={children ? 'leaf' : 'refresh'} alt="leaf" />{' '}
+          </>
+        )}
+        {!isParent ? (
+          <Link href="/club/[club]/org" as={`/club/${code}/org`}>
+            <a>{name}</a>
+          </Link>
+        ) : (
+          name
+        )}
+        <Link href="/club/[club]" as={`/club/${code}`}>
+          <a target="_blank" className="is-pulled-right">
+            <Icon name="external-link" alt="view" />
+          </a>
+        </Link>
+      </div>
+      {children && children.map((a, i) => <OrgChild key={i} {...a} />)}
+    </OrgChildWrapper>
+  )
 }
 
-Org.getInitialProps = async props => {
-  const { query } = props
-  return { query: query }
+const Organization = ({ query, club }) => {
+  const [children, setChildren] = useState(null)
+  const [parents, setParents] = useState(null)
+
+  useEffect(() => {
+    doApiRequest(`/clubs/${query.club}/children/?format=json`)
+      .then(res => res.json())
+      .then(res => {
+        setChildren(res)
+      })
+
+    doApiRequest(`/clubs/${query.club}/parents/?format=json`)
+      .then(res => res.json())
+      .then(res => {
+        setParents(res)
+      })
+  }, [query])
+
+  return (
+    <WideContainer>
+      <Title>{club.name}</Title>
+      <hr />
+      <Subtitle>Children of {club.name}</Subtitle>
+      {children ? <OrgChild isParent={true} {...children} /> : <Loading />}
+      <Subtitle>Parents of {club.name}</Subtitle>
+      {parents ? <OrgChild isParent={true} {...parents} /> : <Loading />}
+    </WideContainer>
+  )
 }
 
-export default renderPage(Org)
+Organization.getInitialProps = async ({ query }) => {
+  const clubReq = await doApiRequest(`/clubs/${query.club}/?format=json`)
+  const clubRes = await clubReq.json()
+
+  return { query, club: clubRes }
+}
+
+export default renderPage(Organization)
