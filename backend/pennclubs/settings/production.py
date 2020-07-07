ffded6fff739f684518d55239267bd9fd265d454
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2020-07-06 20:28:33 -0400

    Pin isort & remove mysql

--- backend/pennclubs/settings/production.py
+++ backend/pennclubs/settings/production.py
@@ -4,13 +4,10 @@ import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
 from pennclubs.settings.base import *  # noqa: F401, F403
-from pennclubs.settings.base import DATABASES, DOMAIN
+from pennclubs.settings.base import DOMAIN
 
 
 DEBUG = False
-
-# Fix MySQL Emoji support
-DATABASES["default"]["OPTIONS"] = {"charset": "utf8mb4"}
 
 # Honour the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
