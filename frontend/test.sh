4b27c0312e273a945eb48075868087c9440091f3
Eric Wang <ezwang1@gmail.com>
2020-07-11 19:09:17 -0400

    upload artifacts

--- frontend/test.sh
+++ frontend/test.sh
@@ -9,17 +9,26 @@ export LANG=C.UTF-8
 # Kill background servers
 trap 'kill $(jobs -p)' EXIT
 
+# ake test-results folder
+mkdir -p test-results
+if ! [ "$(expr substr $(uname -s) 1 4)" == "MING" ]; then
+  mkdir -p ./cypress/screenshots
+  mkdir -p ./cypress/videos
+  ln -s ./cypress/screenshots ./test-results/screenshots
+  ln -s ./cypress/videos ./test-results/videos
+fi
+
 # Setup Python backend server
 pushd ../backend
 time pipenv install --dev || echo "Failed to install all dependencies, continuing anyways..."
 pipenv run python manage.py migrate
 pipenv run python manage.py populate
-pipenv run python manage.py runserver &
+pipenv run python manage.py runserver 2>&1 > ../frontend/test-results/django-server.log &
 popd
 yarn run wait-on -s 3 -d 500 -t 30000 http://localhost:8000/api
 
 # Setup frontend server
-node server.js &
+node server.js 2>&1 > ./test-results/node-server.log &
 yarn run wait-on -s 3 -d 500 -t 30000 http://localhost:3000
 
 # Run tests
