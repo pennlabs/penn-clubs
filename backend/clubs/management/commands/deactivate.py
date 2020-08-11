30a0da83d7b4af4805c8397b0e7ca69a5f075379
Eric Wang <ezwang1@gmail.com>
2020-08-11 09:29:34 -0400

    add ability to deactivate specific clubs

--- backend/clubs/management/commands/deactivate.py
+++ backend/clubs/management/commands/deactivate.py
@@ -14,30 +14,44 @@ class Command(BaseCommand):
             action="store_true",
             help="Do not prompt for confirmation, just start the club renewal process.",
         )
+        parser.add_argument(
+            "--clubs",
+            dest="club",
+            type=str,
+            help="If this parameter is specified, then only trigger renewal for the comma \
+                  separated list of club codes specified by this argument.",
+        )
 
     def handle(self, *args, **kwargs):
-        if not kwargs["force"]:
+        # warn if we're resetting all clubs and force flag is not specified
+        if not kwargs["force"] and not kwargs["club"]:
             self.stdout.write(
                 self.style.WARNING(
                     "You are about to set all club status to inactive and will have "
                     + "to begin the renewal process. This should only happen at the beginning "
                     + "of the school year. Are you postive this is what you want "
-                    + "to do? Type 'deactivate all clubs' to continue."
+                    + "to do? Type 'deactivate all clubs' to continue or press Ctrl-C to abort."
                 )
             )
 
             correct = "deactivate all clubs"
             self.stdout.write("Input:", ending=" ")
-            while input().strip() != correct:
+            while not input().lower().strip() == correct:
                 self.stdout.write("Input:", ending=" ")
 
-        # deactivate all clubs
-        Club.objects.all().update(active=False, approved=None, approved_by=None)
+        # either select all clubs or just some
+        if not kwargs["club"]:
+            clubs = Club.objects.all()
+        else:
+            clubs = Club.objects.filter(code__in=kwargs["club"].strip().split(","))
 
-        self.stdout.write("All clubs deactivated!")
+        # deactivate all clubs
+        clubs.update(active=False, approved=None, approved_by=None)
+
+        self.stdout.write(f"{clubs.count()} clubs deactivated!")
 
         # send out renewal emails to all clubs
-        for club in Club.objects.all():
+        for club in clubs:
             club.send_renewal_email()
 
         self.stdout.write("All emails sent out!")
