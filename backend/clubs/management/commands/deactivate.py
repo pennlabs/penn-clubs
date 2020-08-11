e5d18a90d8a3278560614f1019b23a86aafd5c4d
Eric Wang <ezwang1@gmail.com>
2020-08-10 21:35:23 -0400

    also unapprove club and set approved by field to null

--- backend/clubs/management/commands/deactivate.py
+++ backend/clubs/management/commands/deactivate.py
@@ -32,6 +32,6 @@ class Command(BaseCommand):
                 self.stdout.write("Input:", ending=" ")
 
         # deactivate all clubs
-        Club.objects.all().update(active=False)
+        Club.objects.all().update(active=False, approved=False, approved_by=None)
 
         self.stdout.write("All clubs deactivated!")
