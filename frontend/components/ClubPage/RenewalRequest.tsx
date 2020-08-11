99b53db93b01131a50092ad6a3d2e28b1bf92cfb
Eric Wang <ezwang1@gmail.com>
2020-08-10 22:51:00 -0400

    add checks and renew button to dialog

--- frontend/components/ClubPage/RenewalRequest.tsx
+++ frontend/components/ClubPage/RenewalRequest.tsx
@@ -1,6 +1,8 @@
+import Link from 'next/link'
 import { ReactElement } from 'react'
 
-import { Club } from '../../types'
+import { CLUB_RENEW_ROUTE } from '../../constants/routes'
+import { Club, MembershipRank } from '../../types'
 import { AlertDesc, AlertText, Contact, Icon } from '../common'
 
 const iconStyles = {
@@ -18,13 +20,27 @@ const RenewalRequest = ({ club }: RenewalRequestProps): ReactElement => {
   return (
     <>
       <AlertText>
-        <Icon name={'alert-circle'} style={iconStyles} />
+        <Icon name="alert-circle" style={iconStyles} />
         <b>{club.name}</b> needs to be re-registered for the 2020-2021 academic
         year.
       </AlertText>
       <AlertDesc>
-        If you are an officer for this club and require access, please send an
-        email with your PennKey and club name to <Contact />.
+        {club.is_member !== false && club.is_member <= MembershipRank.Member ? (
+          <>
+            <p className="mb-2">
+              You are an officer of this club, so you can start the renewal
+              process by clicking the button below.
+            </p>
+            <Link href={CLUB_RENEW_ROUTE()} as={CLUB_RENEW_ROUTE(club.code)}>
+              <a className="button is-success">Renew Now</a>
+            </Link>
+          </>
+        ) : (
+          <>
+            If you are an officer for this club and require access, please send
+            an email with your PennKey and club name to <Contact />.
+          </>
+        )}
       </AlertDesc>
     </>
   )
