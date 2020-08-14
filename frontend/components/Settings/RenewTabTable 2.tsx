1adce26f81e13e822b3eca7a529c8f98e4c573b4
snie11 <selinanie11@gmail.com>
2020-08-14 18:55:10 -0500

    final email edits

---
+++ frontend/components/Settings/RenewTabTable 2.tsx
@@ -0,0 +1,72 @@
+import Link from 'next/link'
+import { ReactElement } from 'react'
+import ReactTooltip from 'react-tooltip'
+import s from 'styled-components'
+
+import { DARK_GRAY } from '../../constants/colors'
+import { CLUB_RENEWAL_ROUTE, CLUB_ROUTE } from '../../constants/routes'
+import { BODY_FONT } from '../../constants/styles'
+import { Club, MembershipRank } from '../../types'
+import { getRoleDisplay } from '../../utils'
+import { Icon } from '../common'
+import { UserMembership } from './ClubTab'
+import Toggle from './Toggle'
+
+const Table = s.table`
+  font-family: ${BODY_FONT};
+  font-size: 16px;
+  overflow: scroll;
+  color: ${DARK_GRAY} !important;
+`
+
+type ClubTabTableProps = {
+  className?: string
+  memberships: UserMembership[]
+  togglePublic: (club: Club) => void
+  toggleActive: (club: Club) => void
+  leaveClub: (club: Club) => void
+}
+
+export default ({
+  className,
+  memberships,
+  togglePublic,
+  toggleActive,
+  leaveClub,
+}: ClubTabTableProps): ReactElement => (
+  <Table className={`table is-fullwidth ${className}`}>
+    <thead>
+      <tr>
+        <th>Club</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      {memberships.map(({ club, active, public: isPublic, role }) => (
+        <tr key={club.code}>
+          <td>
+            {role <= MembershipRank.Officer ? (
+              <Link href={CLUB_ROUTE()} as={CLUB_ROUTE(club.code)}>
+                <a>{club.name}</a>
+              </Link>
+            ) : (
+              <></>
+            )}
+          </td>
+          <td>
+            {role <= MembershipRank.Officer ? (
+              <Link
+                href={CLUB_RENEWAL_ROUTE()}
+                as={CLUB_RENEWAL_ROUTE(club.code)}
+              >
+                <a className="button is-small">Register</a>
+              </Link>
+            ) : (
+              <></>
+            )}
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  </Table>
+)
---
+++ frontend/pages/renew 3.js
@@ -0,0 +1,99 @@
+import React from 'react'
+import s from 'styled-components'
+
+import { Container, Metadata, Title } from '../components/common'
+import AuthPrompt from '../components/common/AuthPrompt'
+import ClubTab from '../components/Settings/ClubTab'
+import FavoritesTab from '../components/Settings/FavoritesTab'
+import ProfileTab from '../components/Settings/ProfileTab'
+import RenewTab from '../components/Settings/RenewTab'
+import TabView from '../components/TabView'
+import { CLUBS_BLUE, WHITE } from '../constants/colors'
+import { BORDER_RADIUS } from '../constants/measurements'
+import renderPage from '../renderPage'
+
+const Notification = s.span`
+  border-radius: ${BORDER_RADIUS};
+  background-color: ${CLUBS_BLUE};
+  color: ${WHITE};
+  font-size: 16px;
+  padding: 5px 10px;
+  overflow-wrap: break-word;
+  position: absolute;
+  right: 2rem;
+  margin-top: 2rem;
+  padding-right: 35px;
+  max-width: 50%;
+`
+
+class UserRenewal extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+    this.notify = this.notify.bind(this)
+  }
+
+  /**
+   * @param {string} msg
+   */
+  notify(msg) {
+    // Display the message and scroll the user to the top of the page
+    this.setState(
+      {
+        message: msg,
+      },
+      () => window.scrollTo(0, 0),
+    )
+  }
+
+  render() {
+    const { userInfo, authenticated } = this.props
+    if (authenticated === null) {
+      return <div></div>
+    }
+
+    if (!userInfo) {
+      return <AuthPrompt />
+    }
+
+    const { message } = this.state
+
+    const tabs = [
+      {
+        name: 'Clubs',
+        icon: 'peoplelogo',
+        content: <RenewTab notify={this.notify} userInfo={userInfo} />,
+      },
+    ]
+
+    const { name } = this.props.userInfo
+
+    const gradient = 'linear-gradient(to right, #4954f4, #44469a)'
+
+    return (
+      <>
+        <Metadata title="Your Profile" />
+        <Container background={gradient}>
+          <Title style={{ marginTop: '2.5vw', color: WHITE, opacity: 0.95 }}>
+            Register Clubs
+          </Title>
+        </Container>
+        <TabView background={gradient} tabs={tabs} tabClassName="is-boxed" />
+
+        {message && (
+          <Container>
+            <Notification className="notification">
+              <button
+                className="delete"
+                onClick={() => this.setState({ message: null })}
+              />
+              {message}
+            </Notification>
+          </Container>
+        )}
+      </>
+    )
+  }
+}
+
+export default renderPage(UserRenewal)
