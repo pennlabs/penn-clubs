dd3a28e65315a12aa48651960d0232b9ab6126f1
Eric Wang <ezwang1@gmail.com>
2020-08-16 17:25:41 -0400

    fix style issues

--- backend/clubs/models.py
+++ backend/clubs/models.py
@@ -4,9 +4,9 @@ import uuid
 
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django.core.mail import EmailMultiAlternatives
 from django.core.validators import validate_email
-from django.core.exceptions import ValidationError
 from django.db import models
 from django.dispatch import receiver
 from django.template.loader import render_to_string
