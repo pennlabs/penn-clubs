a59e7545793565004c961e41f0a19e3a290c327c
Eric Wang <ezwang1@gmail.com>
2020-08-16 17:58:22 -0400

    add rejected clubs table

--- frontend/components/Settings/QueueTab.tsx
+++ frontend/components/Settings/QueueTab.tsx
@@ -6,9 +6,60 @@ import { Club } from '../../types'
 import { apiCheckPermission, doApiRequest } from '../../utils'
 import { Icon } from '../common'
 
+type QueueTableProps = {
+  clubs: Club[] | null
+}
+
+const QueueTable = ({ clubs }: QueueTableProps): ReactElement => {
+  if (clubs === null) {
+    return <div className="has-text-info">Loading table...</div>
+  }
+
+  if (!clubs.length) {
+    return (
+      <div className="has-text-info">There are no clubs in this table.</div>
+    )
+  }
+
+  return (
+    <table className="table is-fullwidth">
+      <thead>
+        <tr>
+          <th>Club</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {clubs.map((club) => (
+          <tr key={club.code}>
+            <td>
+              <Link href={CLUB_ROUTE()} as={CLUB_ROUTE(club.code)}>
+                <a target="_blank">{club.name}</a>
+              </Link>
+            </td>
+            <td>
+              {club.approved === null && (
+                <span className="has-text-info">
+                  <Icon name="clock" /> Pending Approval
+                </span>
+              )}
+              {club.approved === false && (
+                <span className="has-text-danger">
+                  <Icon name="x" /> Rejected
+                </span>
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
+
 const QueueTab = (): ReactElement => {
   const [canApprove, setCanApprove] = useState<boolean>(false)
-  const [clubs, setClubs] = useState<Club[]>([])
+  const [clubs, setClubs] = useState<Club[] | null>(null)
+  const [rejectedClubs, setRejectedClubs] = useState<Club[] | null>(null)
 
   useEffect(() => {
     apiCheckPermission('clubs.approve_club').then((approved) => {
@@ -16,9 +67,11 @@ const QueueTab = (): ReactElement => {
       if (approved) {
         doApiRequest('/clubs/?active=true&approved=none&format=json')
           .then((resp) => resp.json())
-          .then((clubs) => {
-            setClubs(clubs)
-          })
+          .then(setClubs)
+
+        doApiRequest('/clubs/?active=true&approved=false&format=json')
+          .then((resp) => resp.json())
+          .then(setRejectedClubs)
       }
     })
   }, [])
@@ -34,30 +87,11 @@ const QueueTab = (): ReactElement => {
         approval requests. The table below contains a list of clubs pending your
         approval. Click on the club name to view the club.
       </div>
-      <table className="table is-fullwidth">
-        <thead>
-          <tr>
-            <th>Club</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {clubs.map((club) => (
-            <tr key={club.code}>
-              <td>
-                <Link href={CLUB_ROUTE()} as={CLUB_ROUTE(club.code)}>
-                  <a target="_blank">{club.name}</a>
-                </Link>
-              </td>
-              <td>
-                <span className="has-text-info">
-                  <Icon name="clock" /> Pending Approval
-                </span>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <QueueTable clubs={clubs} />
+      <div className="mt-3 mb-3">
+        The table below shows a list of clubs that have been marked as rejected.
+      </div>
+      <QueueTable clubs={rejectedClubs} />
     </>
   )
 }
