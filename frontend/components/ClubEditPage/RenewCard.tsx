b8ae08e1e64f16d7cb346e03ae1d4add93ab6cf4
Eric Wang <ezwang1@gmail.com>
2020-08-17 22:30:14 -0400

    add sac fair check

--- frontend/components/ClubEditPage/RenewCard.tsx
+++ frontend/components/ClubEditPage/RenewCard.tsx
@@ -16,11 +16,44 @@ export default function RenewCard({ club }: RenewCardProps): ReactElement {
     <BaseCard title="Renew Club Approval">
       {club.active ? (
         <>
-          <b>{club.name}</b> has completed the club renewal process for the{' '}
-          {year}-{year + 1} school year.
-          {club.approved
-            ? ' The club has received approval from the Office of Student Affairs.'
-            : ' The club is currently pending approval from the Office of Student Affairs.'}
+          <div className="mb-3">
+            <b>{club.name}</b> has completed the club renewal process for the{' '}
+            {year}-{year + 1} school year.
+            {club.approved ? (
+              <>
+                {' '}
+                The club has <b>received approval</b> from the Office of Student
+                Affairs.
+              </>
+            ) : (
+              <>
+                {' '}
+                The club is currently <b>pending approval</b> from the Office of
+                Student Affairs.
+              </>
+            )}
+          </div>
+          <div>
+            {club.fair ? (
+              <span className="has-text-success">
+                <b>This club has indicated interest in the SAC fair.</b>{' '}
+                Priority for the fair will be given to SAC affiliated clubs, but
+                we will make every attempt to accomodate all clubs.
+              </span>
+            ) : (
+              <span className="has-text-danger">
+                <b>This club has not indicated interested in the SAC fair.</b>{' '}
+                If this is a mistake, you can fill out the{' '}
+                <Link
+                  href={CLUB_RENEW_ROUTE()}
+                  as={CLUB_RENEW_ROUTE(club.code)}
+                >
+                  <a>renewal form</a>
+                </Link>{' '}
+                again to change your status.
+              </span>
+            )}
+          </div>
         </>
       ) : (
         <>
