f003135e440c1753dddd06b3c025645e00d081cb
Eric Wang <ezwang1@gmail.com>
2020-08-19 08:12:43 -0400

    add directory link to home page

--- frontend/components/ClubPage/ListRenewalDialog.tsx
+++ frontend/components/ClubPage/ListRenewalDialog.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import { ReactElement } from 'react'
 
-import { USER_RENEWAL } from '../../constants'
+import { DIRECTORY_ROUTE, USER_RENEWAL } from '../../constants'
 
 const ListRenewalDialog = (): ReactElement => {
   const year = new Date().getFullYear()
@@ -20,7 +20,11 @@ const ListRenewalDialog = (): ReactElement => {
           may explain why this club list seems oddly empty. If you are an
           officer of a club, click the button below to start renewing your club!
           During this process, you will also have the option to register for the
-          SAC fair.
+          SAC fair. For a complete list of clubs, see{' '}
+          <Link href={DIRECTORY_ROUTE} as={DIRECTORY_ROUTE}>
+            <a>this page</a>
+          </Link>
+          .
         </p>
         <Link href={USER_RENEWAL} as={USER_RENEWAL}>
           <a className="button is-info is-light">Renew Clubs</a>
