fbaa1d4992d0a146ac2b411c0d99fd8469b7e59d
Chirality <danieltaox@gmail.com>
2020-08-22 19:39:22 -0500

    extract OrderInput component

---
+++ frontend/components/OrderInput.tsx
@@ -0,0 +1,170 @@
+import { ReactElement, useEffect, useMemo, useRef, useState } from 'react'
+import s from 'styled-components'
+
+import {
+  BORDER,
+  CLUBS_BLUE,
+  CLUBS_GREY_LIGHT,
+  FOCUS_GRAY,
+  MEDIUM_GRAY,
+} from '../constants/colors'
+import { BORDER_RADIUS, MD, mediaMaxWidth } from '../constants/measurements'
+import { BODY_FONT } from '../constants/styles'
+import { Icon } from './common'
+
+const ORDERINGS = [
+  {
+    key: 'featured',
+    name: 'Featured',
+    icon: 'star',
+  },
+  {
+    key: 'name',
+    name: 'Alphabetical',
+    icon: 'list',
+  },
+  {
+    key: '-favorite_count',
+    name: 'Bookmarks',
+    icon: 'bookmark',
+  },
+  {
+    key: 'random',
+    name: 'Random',
+    icon: 'shuffle',
+  },
+]
+
+const OrderingWrapper = s.div`
+  width: 100%;
+
+  & .dropdown-trigger,
+  & .dropdown-trigger button,
+  & .dropdown-menu {
+    width: 100%;
+    border-radius: ${BORDER_RADIUS};
+  }
+
+  & .dropdown-trigger button {
+    text-align: left;
+    justify-content: flex-start;
+    padding-right: 0;
+  }
+
+  & .dropdown-trigger .icon:last-child:not(:first-child) {
+    display: inline-block;
+    margin-left: auto;
+  }
+
+  & .dropdown-content {
+    padding: 0;
+  }
+
+  & .dropdown-item {
+    display: flex;
+    align-items: center;
+    border-radius: ${BORDER_RADIUS};
+  }
+
+  
+  & .dropdown-item.is-active {
+    background: ${CLUBS_BLUE};
+  }
+
+  & button, a {
+    padding: 8px 10px;
+    color: ${CLUBS_GREY_LIGHT};
+    font-family: ${BODY_FONT};
+  }
+
+  & button:focus, button:hover {
+    outline: none !important;
+    box-shadow: none !important;
+    border: 1px solid ${BORDER};
+    color: ${CLUBS_GREY_LIGHT};
+    background: ${FOCUS_GRAY};
+  }
+`
+
+const OrderingChevronWrapper = s.div`
+  cursor: pointer;
+  color: ${MEDIUM_GRAY};
+  opacity: 0.5;
+  margin-right: 6px !important;
+  margin-left: auto;
+  vertical-align: middle;
+
+  ${mediaMaxWidth(MD)} {
+    right: 24px;
+  }
+`
+
+const OrderInput = ({ onChange }): ReactElement => {
+  const [ordering, setOrdering] = useState<string>('featured')
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const selectedOrdering = ORDERINGS.find((order) => order.key === ordering)
+
+  const dropdownMenuRef = useRef<HTMLDivElement>(null)
+  // Memoize listener to ensure the listener is preserved between rerenders
+  const dropdownBlurListener = useMemo(
+    () => (e) => {
+      if (
+        dropdownMenuRef &&
+        dropdownMenuRef.current &&
+        !dropdownMenuRef.current.contains(e.target)
+      ) {
+        setIsOpen(false)
+      }
+    },
+    [dropdownMenuRef],
+  )
+  useEffect(() => {
+    if (!isOpen) document.removeEventListener('click', dropdownBlurListener)
+    else document.addEventListener('click', dropdownBlurListener)
+  }, [isOpen])
+  useEffect(
+    () => () => document.removeEventListener('click', dropdownBlurListener),
+    [],
+  )
+
+  return (
+    <OrderingWrapper className={`dropdown ${isOpen ? 'is-active' : ''}`}>
+      <div className="dropdown-trigger">
+        <button
+          className="button"
+          aria-haspopup="true"
+          aria-controls="dropdown-menu"
+          onClick={() => !isOpen && setIsOpen(true)}
+        >
+          <Icon name={selectedOrdering?.icon ?? 'x'} />{' '}
+          {selectedOrdering?.name ?? 'Unknown'}
+          <OrderingChevronWrapper>
+            <Icon name="chevron-down" noMargin />
+          </OrderingChevronWrapper>
+        </button>
+      </div>
+      <div ref={dropdownMenuRef} className="dropdown-menu" role="menu">
+        <div className="dropdown-content">
+          {ORDERINGS.map((order) => (
+            <a
+              key={order.key}
+              className={`dropdown-item ${
+                order.key === ordering ? 'is-active' : ''
+              }`}
+              onClick={(e) => {
+                e.preventDefault()
+                setOrdering(order.key)
+                onChange(order.key)
+                setIsOpen(false)
+              }}
+            >
+              <Icon name={order.icon} /> {order.name}
+            </a>
+          ))}
+        </div>
+      </div>
+    </OrderingWrapper>
+  )
+}
+
+export default OrderInput
--- frontend/components/SearchBar.tsx
+++ frontend/components/SearchBar.tsx
@@ -1,8 +1,7 @@
-import React, {
+import {
   ReactElement,
   SetStateAction,
   useEffect,
-  useMemo,
   useRef,
   useState,
 } from 'react'
@@ -11,9 +10,7 @@ import s from 'styled-components'
 import {
   ALLBIRDS_GRAY,
   BORDER,
-  CLUBS_BLUE,
   CLUBS_GREY,
-  CLUBS_GREY_LIGHT,
   FOCUS_GRAY,
   MEDIUM_GRAY,
   WHITE,
@@ -32,6 +29,7 @@ import { Tag } from '../types'
 import { Icon } from './common'
 import DropdownFilter, { FilterHeader, SelectableTag } from './DropdownFilter'
 import FilterSearch from './FilterSearch'
+import OrderInput from './OrderInput'
 
 const MobileSearchBarSpacer = s.div`
   display: block;
@@ -174,161 +172,6 @@ const Collapsible = ({
       />
       {isActive && children}
     </>
-  )
-}
-
-const OrderingWrapper = s.div`
-  width: 100%;
-
-  & .dropdown-trigger,
-  & .dropdown-trigger button,
-  & .dropdown-menu {
-    width: 100%;
-    border-radius: ${BORDER_RADIUS};
-  }
-
-  & .dropdown-trigger button {
-    text-align: left;
-    justify-content: flex-start;
-    padding-right: 0;
-  }
-
-  & .dropdown-trigger .icon:last-child:not(:first-child) {
-    display: inline-block;
-    margin-left: auto;
-  }
-
-  & .dropdown-content {
-    padding: 0;
-  }
-
-  & .dropdown-item {
-    display: flex;
-    align-items: center;
-    border-radius: ${BORDER_RADIUS};
-  }
-
-  
-  & .dropdown-item.is-active {
-    background: ${CLUBS_BLUE};
-  }
-
-  & button, a {
-    padding: 8px 10px;
-    color: ${CLUBS_GREY_LIGHT};
-    font-family: ${BODY_FONT};
-  }
-
-  & button:focus, button:hover {
-    outline: none !important;
-    box-shadow: none !important;
-    border: 1px solid ${BORDER};
-    color: ${CLUBS_GREY_LIGHT};
-    background: ${FOCUS_GRAY};
-  }
-`
-
-const ORDERINGS = [
-  {
-    key: 'featured',
-    name: 'Featured',
-    icon: 'star',
-  },
-  {
-    key: 'name',
-    name: 'Alphabetical',
-    icon: 'list',
-  },
-  {
-    key: '-favorite_count',
-    name: 'Bookmarks',
-    icon: 'bookmark',
-  },
-  {
-    key: 'random',
-    name: 'Random',
-    icon: 'shuffle',
-  },
-]
-
-const OrderingChevronWrapper = s.div`
-  cursor: pointer;
-  color: ${MEDIUM_GRAY};
-  opacity: 0.5;
-  margin-right: 6px !important;
-  margin-left: auto;
-  vertical-align: middle;
-
-  ${mediaMaxWidth(MD)} {
-    right: 24px;
-  }
-`
-
-const OrderInput = ({ onChange }): ReactElement => {
-  const [ordering, setOrdering] = useState<string>('featured')
-  const [isOpen, setIsOpen] = useState<boolean>(false)
-  const selectedOrdering = ORDERINGS.find((order) => order.key === ordering)
-
-  const dropdownMenuRef = useRef<HTMLDivElement>(null)
-  // Memoize listener to ensure the listener is preserved between rerenders
-  const dropdownBlurListener = useMemo(
-    () => (e) => {
-      if (
-        dropdownMenuRef &&
-        dropdownMenuRef.current &&
-        !dropdownMenuRef.current.contains(e.target)
-      ) {
-        setIsOpen(false)
-      }
-    },
-    [dropdownMenuRef],
-  )
-  useEffect(() => {
-    if (!isOpen) document.removeEventListener('click', dropdownBlurListener)
-    else document.addEventListener('click', dropdownBlurListener)
-  }, [isOpen])
-  useEffect(
-    () => () => document.removeEventListener('click', dropdownBlurListener),
-    [],
-  )
-
-  return (
-    <OrderingWrapper className={`dropdown ${isOpen ? 'is-active' : ''}`}>
-      <div className="dropdown-trigger">
-        <button
-          className="button"
-          aria-haspopup="true"
-          aria-controls="dropdown-menu"
-          onClick={() => !isOpen && setIsOpen(true)}
-        >
-          <Icon name={selectedOrdering?.icon ?? 'x'} />{' '}
-          {selectedOrdering?.name ?? 'Unknown'}
-          <OrderingChevronWrapper>
-            <Icon name="chevron-down" noMargin />
-          </OrderingChevronWrapper>
-        </button>
-      </div>
-      <div ref={dropdownMenuRef} className="dropdown-menu" role="menu">
-        <div className="dropdown-content">
-          {ORDERINGS.map((order) => (
-            <a
-              key={order.key}
-              className={`dropdown-item ${
-                order.key === ordering ? 'is-active' : ''
-              }`}
-              onClick={(e) => {
-                e.preventDefault()
-                setOrdering(order.key)
-                onChange(order.key)
-                setIsOpen(false)
-              }}
-            >
-              <Icon name={order.icon} /> {order.name}
-            </a>
-          ))}
-        </div>
-      </div>
-    </OrderingWrapper>
   )
 }
 
