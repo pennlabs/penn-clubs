da09cd043d69debad358d42d8fd46dcdc72d7efa
snie11 <selinanie11@gmail.com>
2020-08-26 00:17:15 -0400

    added all details

--- frontend/pages/sacfairguide.tsx
+++ frontend/pages/sacfairguide.tsx
@@ -1,17 +1,6 @@
-import Link from 'next/link'
-import { ReactElement } from 'react'
 import s from 'styled-components'
 
-import {
-  Contact,
-  Container,
-  Line,
-  Metadata,
-  StrongText,
-  Text,
-  Title,
-} from '../components/common'
-import { CREATE_ROUTE } from '../constants'
+import { Contact, Container, Metadata, Title } from '../components/common'
 import { SNOW } from '../constants/colors'
 import { M4 } from '../constants/measurements'
 import renderPage from '../renderPage'
@@ -19,17 +8,6 @@ import renderPage from '../renderPage'
 type QuestionProps = React.PropsWithChildren<{
   title: string
 }>
-
-const Question = ({ title, children }: QuestionProps): ReactElement => (
-  <>
-    <StrongText style={{ marginBottom: '0.5rem' }}>{title}</StrongText>
-    <Text>
-      {children}
-      <br />
-      <br />
-    </Text>
-  </>
-)
 
 const Subheading = s.div`
   margin-top: 1.5rem;
@@ -69,154 +47,237 @@ const FAQ = () => (
       page, or email <Contact /> with your Penn email and club name to be added
       as an officer.
     </p>
-
-    <br />
-    <br />
-    <br />
-    <br />
+    <img
+      style={{
+        maxHeight: 400,
+        verticalAlign: 'middle',
+        margin: 10,
+        padding: 10,
+      }}
+      src={'static/img/sacfair_screencaps/1.png'}
+      alt={'Manage Club Button'}
+      title={'Manage Club Button'}
+    />
+    <p>Then, click the "Events" tab:</p>
+    <img
+      style={{
+        maxHeight: 400,
+        verticalAlign: 'middle',
+        margin: 10,
+        padding: 10,
+      }}
+      src={'static/img/sacfair_screencaps/2.png'}
+      alt={'Events Tab'}
+      title={'Events Tab'}
+    />
 
     <p>
-      <Question title="What is Penn Clubs?">
-        Penn Clubs is meant to be your central source of information about
-        student organizations at the University of Pennsylvania. Keep
-        discovering new clubs throughout the year, not just at the SAC Fair.
-      </Question>
-      <Question title="How can I provide feedback?">
-        We’re so excited to let everyone at Penn contribute to the development
-        of Penn Clubs! Your feedback is incredibly important to us. Have any
-        questions or comments? Find any bugs?{' '}
-        <a href="https://airtable.com/shrCsYFWxCwfwE7cf">
-          Please let us know on our feedback form.
-        </a>
-      </Question>
-      <Line />
-      <Question title="Why do I have to log in?">
-        Logging in allows us to create an account for you on Penn Clubs. This
-        gives you access to many useful and upcoming features! When you bookmark
-        a club, it will be saved to your bookmarked list. You can use this to
-        keep track of clubs you’re interested in, or a part of. You can also be
-        invited to join club Member lists. Finally, you'll need to log in if you
-        want to use your administrator permissions to edit a club page.
-      </Question>
-      <Question title="How do I use this site?">
-        The #1 way to use this site is to browse clubs at Penn! You can:
-        <UnorderedList>
-          <li>
-            Search for clubs by name, and use filters like Tags (tags that
-            describe the club), Size (number of members), and Application (if
-            applications are required to join)
-          </li>
-          <li>Bookmark clubs to keep track of them</li>
-          <li>
-            Browse information that clubs post: description, how to join, member
-            testimonials
-          </li>
-        </UnorderedList>
-        <br />
-        If you run a club, make sure your club has a page on Penn Clubs! This
-        lets other students find out about your organization and how to get
-        involved.
-      </Question>
-      <Question title="How do I edit an organization’s profile?">
-        You’ll need to have administrator permission for that organization.
-        We’ve invited people as administrators based on information submitted by
-        clubs to SAC during Spring 2019.
-        <UnorderedList>
-          <li>
-            If you did not receive administrator permission and you believe you
-            should have, let us know at <Contact /> and we will work with you to
-            verify your request.
-          </li>
-          <li>
-            If your club did not submit this information to SAC, we’ve been
-            contacting clubs by their listed email to ask for the names of
-            people who need administrator permission. You can also email us at{' '}
-            <Contact /> and we will work with you to verify your request.
-          </li>
-        </UnorderedList>
-        <br />
-        Note that there are 2 levels of administrators: Officers and Owners.
-        Officers are able to edit the page, invite other members, and grant
-        administrator permissions. In addition to those abilities, Owners are
-        able to deactivate or delete the club page.
-      </Question>
-      <Question title="Why I can't find an organization on Penn Clubs?">
-        Sorry about that! We’re in the process of making Penn Clubs as
-        comprehensive as possible, creating the first complete directory of
-        student organizations at Penn.
-        <UnorderedList>
-          <li>
-            If you're an officer of a club and it <b>does not exist</b> on Penn
-            Clubs, you can add your club using the form found{' '}
-            <Link href={CREATE_ROUTE} as={CREATE_ROUTE}>
-              <a>here</a>
-            </Link>
-            .
-          </li>
-          <li>
-            If you're an officer of a club and it <b>already exists</b> on Penn
-            Clubs, email <Contact /> to gain edit access for your club.
-          </li>
-          <li>
-            Otherwise, if you're not the administrator of the club but would
-            still like for it to be added to Penn Clubs, please email{' '}
-            <Contact />.
-          </li>
-        </UnorderedList>
-      </Question>
-      <Question title="How are clubs ordered on Penn Clubs?">
-        Click{' '}
-        <Link href="/rank">
-          <a>here</a>
-        </Link>{' '}
-        for details about our club recommendation algorithm.
-      </Question>
-      <Question title="I have another question!">
-        <a href="https://airtable.com/shrCsYFWxCwfwE7cf">
-          Please let us know on our feedback form :)
-        </a>
-      </Question>
-      <Line />
-      <Question title="Special Thanks">
-        Thank you to the organizations below for their support in launching Penn
-        Clubs! We're excited to continue building this valuable resource
-        together.
-        <br />
-        <br />
-        <div>
-          {[
-            {
-              name: 'Student Activities Council',
-              image: '/static/img/collaborators/sac.png',
-              url: 'https://sacfunded.net/',
-            },
-            {
-              name: 'Undergraduate Assembly',
-              image: '/static/img/collaborators/ua.png',
-              url: 'https://pennua.org/',
-              height: 80,
-            },
-            {
-              name: 'Office of Student Affairs',
-              image: '/static/img/collaborators/osa.png',
-              url: 'https://www.vpul.upenn.edu/osa/',
-            },
-          ].map(({ name, url, image, height }) => (
-            <a href={url} target="_blank" key={name}>
-              <img
-                style={{
-                  maxHeight: height || 100,
-                  verticalAlign: 'middle',
-                  margin: 10,
-                }}
-                src={image}
-                alt={name}
-                title={name}
-              />
-            </a>
-          ))}
-        </div>
-      </Question>
+      There will be an existing event with your designated date & time titled
+      "SAC Fair Info Session". Click "Edit".
+    </p>
+    <img
+      style={{
+        maxHeight: 400,
+        verticalAlign: 'middle',
+        margin: 10,
+        padding: 10,
+      }}
+      src={'static/img/sacfair_screencaps/3.png'}
+      alt={'Edit SAC Fair Event'}
+      title={'Edit SAC Fair Event'}
+    />
+    <p>
+      Edit the club details: you <strong>MUST</strong> provide a Zoom link
+      (preferably from your school Zoom account) where prospective students can
+      navigate to. You can add a description, as well as upload a cover photo
+      (dimensions 16:9) to make your event stand out in the live events portal.
+      Be sure to{' '}
+      <strong>
+        hit the green "save" button on the lefthand side when you are done!
+      </strong>
+    </p>
+
+    <img
+      style={{
+        maxHeight: 400,
+        verticalAlign: 'middle',
+        margin: 10,
+        padding: 10,
+      }}
+      src={'static/img/sacfair_screencaps/4.png'}
+      alt={'Save SAC Fair Changes'}
+      title={'Save SAC Fair Changes'}
+    />
+
+    <Subheading>Zoom Best Practices</Subheading>
+    <p>
+      To get all possible features, please use an account using the school's
+      licensed Zoom accounts. You should have received an email from your home
+      school about obtaining login credentials for this. Schedule a new meeting,
+      using the <strong>given date and time</strong> as outlined in the e-mail
+      given to you by SAC with these settings:
+    </p>
+    <UnorderedList>
+      <li>A required meeting password</li>
+      <li>Under Advanced Options:</li>
+      <UnorderedList>
+        <li>
+          Waiting room – <strong>disabled</strong>
+        </li>
+        <li>
+          Mute participants upon entry – <strong>enabled</strong>
+        </li>
+        <li>
+          Only authenticated users can join – <strong>enabled</strong>
+        </li>
+      </UnorderedList>
+    </UnorderedList>
+
+    <p>When hosting your event:</p>
+    <UnorderedList>
+      <li>
+        Make all present mebers of the session <strong>co-hosts</strong> to help
+        control the session.
+      </li>
+      <li>
+        Have the <strong>chat window open</strong> to answer any questions. For
+        busier sessions, we recommend designating one member to be monitoring
+        the chat window
+      </li>
+      <li>
+        For busier sessions where multiple club members are present, we
+        recommend taking advantage of the <strong>breakout room feature</strong>
+        , which can be prompted by hosts. We would suggest{' '}
+        <strong>manually assigning</strong> people to a breakout room, to ensure
+        a member of the club is there to answer questions.
+      </li>
+    </UnorderedList>
+
+    <img
+      style={{
+        maxHeight: 400,
+        verticalAlign: 'middle',
+        margin: 10,
+        padding: 10,
+      }}
+      src={'static/img/sacfair_screencaps/5.png'}
+      alt={'Zoom Breakout Rooms'}
+      title={'Zoom Breakout Rooms'}
+    />
+
+    <Subheading>Recruitment Resources</Subheading>
+    <p>
+      Tired of creating a Google Form to track all interested members? Now,
+      students can click the "Subscribe" bell button on your club's page to
+      immediately be added to your club's own listserv without spending the time
+      to fill out a form.
+    </p>
+
+    <img
+      style={{
+        maxHeight: 400,
+        verticalAlign: 'middle',
+        margin: 10,
+        padding: 10,
+      }}
+      src={'static/img/sacfair_screencaps/6.png'}
+      alt={'Subscribe Button'}
+      title={'Subscribe Button'}
+    />
+
+    <p>
+      To access this listserv, simply navigate to "Manage Club" once again, then
+      to the "Recruitment" tab. Here, you will also find a QR code your club can
+      use for members to bookmark or subscribe to your club. You will also see a
+      table of members who have subscribed to your club. You can scroll to the
+      bottom to download an Excel file of all these members' names, emails, and
+      more self-reported information.
+    </p>
+
+    <img
+      style={{
+        maxHeight: 400,
+        verticalAlign: 'middle',
+        margin: 10,
+        padding: 10,
+      }}
+      src={'static/img/sacfair_screencaps/7.png'}
+      alt={'Listserv'}
+      title={'Listserv'}
+    />
+
+    <Subheading>Club FAQ</Subheading>
+    <p>
+      To facilitate asynchronous interactions with prospective members, we also
+      have an FAQ section on each club's page. When students post a question,
+      all officers listed under the club will receive an email notification that
+      a question for your club has been asked.
+    </p>
+
+    <img
+      style={{
+        maxHeight: 400,
+        verticalAlign: 'middle',
+        margin: 10,
+        padding: 10,
+      }}
+      src={'static/img/sacfair_screencaps/8.png'}
+      alt={'Club FAQ'}
+      title={'Club FAQ'}
+    />
+
+    <p>
+      To answer the questions, navigate once again to the "Manage Club", and
+      then to the "Questions" tab, where you can see all questions asked and
+      answer or delete them.
+    </p>
+
+    <img
+      style={{
+        maxHeight: 400,
+        verticalAlign: 'middle',
+        margin: 10,
+        padding: 10,
+      }}
+      src={'static/img/sacfair_screencaps/9.png'}
+      alt={'Answering questions for Clubs'}
+      title={'Answering questions for Clubs'}
+    />
+
+    <Subheading>Managing Members</Subheading>
+    <p>
+      As you can tell, there is a lot of responsibility for club owners already.
+      To help lessen the load, you can invite other officers of your club to
+      join your club's profile. Click the "Manage Club" button once again, and
+      then navigate to the "Membership" tab. By entering email addresses
+      (separated by commas or newlines) you can send invites to all the officers
+      of your club by clicking the "Officer" status under Permissions. You can
+      do the same with non-officer members, but leaving their status as
+      "Member".{' '}
+      <strong>
+        Only Officers and Owners of a club can use the Manage Club button.
+      </strong>
+    </p>
+
+    <img
+      style={{
+        maxHeight: 400,
+        verticalAlign: 'middle',
+        margin: 10,
+        padding: 10,
+      }}
+      src={'static/img/sacfair_screencaps/10.png'}
+      alt={'Managing club members'}
+      title={'Managing club members'}
+    />
+
+    <Subheading>Settings</Subheading>
+    <p>
+      We encourage clubs to keep their club pages as up-to-date as possible with
+      descriptions, members, social media links, events, and more! Check out{' '}
+      <a href="/rank">ranking algorithm here </a>our to discover ways to boost
+      your club and events to the top of Penn Clubs, simply by providing more
+      information on your club for prospective members.
     </p>
   </Container>
 )
