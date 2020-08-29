ceb5ec533e5c46b0ee0fa7794bf1d74829bbf33e
Eric Wang <ezwang1@gmail.com>
2020-08-29 18:20:42 -0400

    add redis lib, try hostname

--- backend/pennclubs/settings/production.py
+++ backend/pennclubs/settings/production.py
@@ -51,7 +51,7 @@ CHANNEL_LAYERS = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://localhost:6379/1",
+        "LOCATION": "redis://redis:6379/1",
         "OPTIONS": {"CLIENT_CLASS": "django_redis.client.DefaultClient"},
         "KEY_PREFIX": "django",
     }
