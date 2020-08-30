a593baf91d7cbd20805c73beebe98c8e66620c17
Campbell Phalen <cphalen@seas.upenn.edu>
2020-08-30 03:38:04 -0400

    Lint times 4 I guess we lint migration files now

--- backend/clubs/migrations/0035_clubvisit.py
+++ backend/clubs/migrations/0035_clubvisit.py
@@ -9,18 +9,31 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('clubs', '0034_auto_20200822_0943'),
+        ("clubs", "0034_auto_20200822_0943"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='ClubVisit',
+            name="ClubVisit",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('updated_at', models.DateTimeField(auto_now=True)),
-                ('club', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='clubs.club')),
-                ('person', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+                    ),
+                ),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("updated_at", models.DateTimeField(auto_now=True)),
+                (
+                    "club",
+                    models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to="clubs.club"),
+                ),
+                (
+                    "person",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL
+                    ),
+                ),
             ],
         ),
     ]
