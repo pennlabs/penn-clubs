f4484deb0f3bd3470c3f737009cf34d579a6fa87
Eric Wang <ezwang1@gmail.com>
2020-08-31 12:54:28 -0400

    more efficient fair rendering

--- backend/clubs/views.py
+++ backend/clubs/views.py
@@ -745,6 +745,48 @@ class EventViewSet(viewsets.ModelViewSet):
         return upload_endpoint_helper(request, Event, "image", pk=event.pk)
 
     @action(detail=False, methods=["get"])
+    def fair(self, request, *args, **kwargs):
+        """
+        Get the minimal information required for a fair directory listing.
+        """
+        # cache the response for this endpoint with short timeout
+        key = f"events:fair:directory:{request.user.is_authenticated}"
+        cached = cache.get(key)
+        if cached:
+            return Response(cached)
+
+        events = Event.objects.filter(type=Event.FAIR, club__badges__purpose="fair").values_list(
+            "start_time", "end_time", "club__name", "club__code", "club__badges__label"
+        )
+        output = {}
+        for event in events:
+            # group by start date
+            ts = int(event[0].replace(second=0, microsecond=0).timestamp())
+            if ts not in output:
+                output[ts] = {
+                    "start_time": event[0],
+                    "end_time": event[1],
+                    "events": {},
+                }
+
+            # group by category
+            category = event[4]
+            if category not in output[ts]["events"]:
+                output[ts]["events"][category] = {
+                    "category": category,
+                    "events": [],
+                }
+
+            output[ts]["events"][category]["events"].append({"name": event[2], "code": event[3]})
+        for item in output.values():
+            item["events"] = list(sorted(item["events"].values(), key=lambda cat: cat["category"]))
+
+        output = list(sorted(output.values(), key=lambda cat: cat["start_time"]))
+        cache.set(key, output, 60 * 5)
+
+        return Response(output)
+
+    @action(detail=False, methods=["get"])
     def owned(self, request, *args, **kwargs):
         """
         Return all events that the user has officer permissions over.
