6a0ff0ba61d03151793af4bb6b6052d709e8ecdb
Eric Wang <ezwang1@gmail.com>
2020-08-31 14:43:40 -0400

    add placeholder for upcoming events

--- frontend/pages/events.tsx
+++ frontend/pages/events.tsx
@@ -233,6 +233,16 @@ function EventPage({
                 <EventCard key={e.id} event={e} isHappening={false} />
               ))}
             </CardList>
+            {!upcomingEvents.length && (
+              <div className="notification is-info is-clearfix">
+                <img
+                  className="is-pulled-left mr-5 mb-3"
+                  style={{ width: 100 }}
+                  src="/static/img/events_calendar.png"
+                />
+                <div>There are no current upcoming events.</div>
+              </div>
+            )}
           </WideContainer>
         </SearchbarRightContainer>
       </div>
