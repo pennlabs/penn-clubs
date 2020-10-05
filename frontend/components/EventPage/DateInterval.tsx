82f538a73a3872913f52d0d02f95721e6771a135
Mohammed Abaker <mohamme@nichols-ve703-03319.apn.wlan.private.upenn.edu>
2020-10-05 11:07:46 -0400

    display today/tomorrow upcoming events

--- frontend/components/EventPage/DateInterval.tsx
+++ frontend/components/EventPage/DateInterval.tsx
@@ -2,16 +2,22 @@
 import moment from 'moment'
 import { ReactElement } from 'react'
 import styled from 'styled-components'
+import TimeAgo from 'react-timeago';
 
 import { LIGHT_GRAY } from '../../constants'
 
+
 const dateIntervalString = (startDate: Date, endDate: Date): string => {
   const [start, end] = [startDate, endDate].map((d) => moment(d))
+    
   if (![start, end].every((d) => d.isValid())) {
     return 'Invalid date range'
   }
-  const [startDateString, endDateString] = [start, end].map((d) =>
-    d.format('dddd, MMM D'),
+  const [startDateString, endDateString] = [start, end].map((d) => {
+    const dateDifference = start.startOf("day").diff(moment().startOf("day"), "days");
+    if (dateDifference == 0) return "Today";
+    if (dateDifference == 1) return "Tomorrow";
+    return d.format('dddd, MMM D')}
   )
   if (startDateString !== endDateString) {
     return `${startDateString} - ${endDateString}`
