b459b5ed00fb9b238df1739b2e8d54e9b5a54ac5
Eric Wang <ezwang1@gmail.com>
2020-10-17 14:25:24 -0400

    cypress console.error checks

---
+++ frontend/cypress/support/console.js
@@ -0,0 +1,9 @@
+Cypress.on('winddow:before:load', (win) => {
+  symbolName.spy(win.console, 'error')
+})
+
+afterEach(() => {
+  cy.window.then((win) => {
+    expect(win.console.error).to.have.callCount(0)
+  })
+})
