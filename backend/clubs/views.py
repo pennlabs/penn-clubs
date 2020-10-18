41cfd073789bcf8c8e232fc6406eec8e617daf21
Rohan Gupta <rohangupta883@gmail.com>
2020-10-18 05:56:15 +0530

    Style and dependencies

--- backend/clubs/views.py
+++ backend/clubs/views.py
@@ -25,9 +25,9 @@ from django.template.loader import render_to_string
 from django.utils import timezone
 from django.utils.text import slugify
 from django_redis import get_redis_connection
-from ics import Calendar as cal
-from ics import Event as ev
-from ics import parse
+from ics import Calendar as ICSCal
+from ics import Event as ICSEvent
+from ics import parse as ICSParse
 from rest_framework import filters, generics, mixins, parsers, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.exceptions import PermissionDenied
@@ -1534,14 +1534,14 @@ class FavoriteCalendarAPIView(APIView):
     """
 
     def get(self, request, *args, **kwargs):
-        calendar = cal()
-        calendar.extra.append(parse.ContentLine(name="X-WR-CALNAME", value="Penn Clubs Events"))
+        calendar = ICSCal()
+        calendar.extra.append(ICSParse.ContentLine(name="X-WR-CALNAME", value="Penn Clubs Events"))
         favs = Favorite.objects.filter(person__profile__uuid_secret=kwargs["user_secretuuid"])
         clubs = {fav.club for fav in favs}  # set contains distinct elements
         for club in clubs:
             all_events = club.events.all()
             for event in all_events:
-                e = ev()
+                e = ICSEvent()
                 e.name = event.club.name + ": " + event.name
                 e.begin = event.start_time
                 e.end = event.end_time
