055f8906f52292cb31b6bb19ee3a2570a1877bae
Eric Wang <ezwang1@gmail.com>
2020-10-18 23:22:05 -0400

    use common apostrophe

--- frontend/utils/branding.tsx
+++ frontend/utils/branding.tsx
@@ -58,7 +58,7 @@ const sites = {
     SITE_LOGO: '/static/img/penn_shield.png',
     SITE_FAVICON: '/static/penn_favicon.ico',
     SITE_TAGLINE:
-      'Find the support resources you need on and around Penn’s campus!',
+      "Find the support resources you need on and around Penn's campus!",
 
     APPROVAL_AUTHORITY: 'Office of Student Affairs',
     APPROVAL_AUTHORITY_URL: 'https://osa.vpul.upenn.edu/',
