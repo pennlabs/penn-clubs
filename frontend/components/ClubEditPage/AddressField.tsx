047305e3c84ab7c58f6f674ad81762594a1991cc
Eric Wang <ezwang1@gmail.com>
2020-10-24 11:55:25 -0400

    fix google maps typescript error

--- frontend/components/ClubEditPage/AddressField.tsx
+++ frontend/components/ClubEditPage/AddressField.tsx
@@ -38,7 +38,7 @@ const AddressTypeaheadField = ({
     setValue,
   } = usePlacesAutocomplete({
     requestOptions: {
-      location: { lat: () => 39.952104, lng: () => -75.193739 },
+      location: { lat: () => 39.952104, lng: () => -75.193739 } as any,
       radius: 4 * 1000,
       // Coordinates are for the university of Pennsylvania. This prioritizes the suggestions which
       // are within a 4Km radius.
