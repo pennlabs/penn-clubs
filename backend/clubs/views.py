61a3b28c5ccd59c76cf2a3ebd8516d9d20955080
Rohan Gupta <rohangupta883@gmail.com>
2020-11-07 10:28:01 +0530

    Added backend endpoint to retrieve a user's UUID

--- backend/clubs/views.py
+++ backend/clubs/views.py
@@ -109,6 +109,7 @@ from clubs.serializers import (
     UserSerializer,
     UserSubscribeSerializer,
     UserSubscribeWriteSerializer,
+    UserUUIDSerializer,
     YearSerializer,
 )
 from clubs.utils import html_to_text
@@ -2118,6 +2119,21 @@ class UserUpdateAPIView(generics.RetrieveUpdateAPIView):
         prefetch_related_objects(
             [user], "profile__school", "profile__major",
         )
+        return user
+
+
+class UserUUIDAPIView(generics.RetrieveAPIView):
+    """
+    get: Retrieve the uuid for the given user.
+    """
+
+    queryset = get_user_model().objects.all()
+    serializer_class = UserUUIDSerializer
+    permission_classes = [IsAuthenticated]
+    http_method_names = ["get"]
+
+    def get_object(self):
+        user = self.request.user
         return user
 
 
