14493d77cf5d89590d05870172911cfef30c6d56
esinx <me@esinx.net>
2020-11-08 02:01:18 +0900

    How 👏 am 👏 I 👏 keep 👏 forgetting 👏 to 👏 merge

--- frontend/pages/events.tsx
+++ frontend/pages/events.tsx
@@ -373,7 +373,6 @@ function EventPage({
               background: SNOW,
             }}
           >
-<<<<<<< HEAD
             {/* {!!liveEvents.length && (
               <>
                 <Title
@@ -391,8 +390,6 @@ function EventPage({
                 <br />
               </>
             )} */}
-=======
->>>>>>> Better trigger and placement for popups
             {isLoading && <ListLoadIndicator />}
             <Calendar
               localizer={localizer}
