3877eeab20a034e68713a94c68903c379af703a0
esinx <me@esinx.net>
2020-11-08 01:41:42 +0900

    bulma is awesomeee

--- frontend/components/EventPage/SyncModal.tsx
+++ frontend/components/EventPage/SyncModal.tsx
@@ -1,12 +1,7 @@
 import React, { ReactElement } from 'react'
 import styled from 'styled-components'
 
-import {
-  CLUBS_BLUE,
-  CLUBS_GREY,
-  CLUBS_NAVY,
-  WHITE,
-} from '../../constants/colors'
+import { CLUBS_BLUE, CLUBS_GREY, CLUBS_NAVY } from '../../constants/colors'
 
 const ModalContainer = styled.div`
   text-align: left;
@@ -26,44 +21,30 @@ const Subtitle = styled.div`
   font-weight: bold;
 `
 
-const URLCell = styled.input`
-  flex: 1;
-  font-size: 15px;
-  outline: none;
-  border: solid 1px ${CLUBS_BLUE};
-  border-top-left-radius: 5px;
-  border-bottom-left-radius: 5px;
-  padding: 10px;
-  margin-left: 10px;
-`
-
-const URLCopyButton = styled.div`
-  font-size: 15px;
-  line-height: 15px;
-  background: ${CLUBS_BLUE};
-  color: ${WHITE};
-  align-self: stretch;
-  display: grid;
-  place-items: center;
-  padding: 10px;
-  border-top-right-radius: 5px;
-  border-bottom-right-radius: 5px;
-`
-
-const SyncModal = (props: {}): ReactElement => {
+const SyncModal = (props: { calendarURL: string }): ReactElement => {
   return (
     <ModalContainer>
       <Title>Sync To Calendar</Title>
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          marginTop: '10px',
-        }}
-      >
-        <b>URL: </b>
-        <URLCell readOnly value={'https://pennclubs.com/abc'} />
-        <URLCopyButton>Copy</URLCopyButton>
+      <div style={{ display: 'block', marginTop: '15px' }}>
+        <div className="field has-addons is-expanded">
+          <div className="field-label is-normal" style={{ flexGrow: 0 }}>
+            <label className="label">URL:</label>
+          </div>
+          <div className="control is-expanded">
+            <input
+              className="input"
+              type="text"
+              readOnly
+              value={props.calendarURL}
+              style={{ border: `solid ${CLUBS_BLUE} 1px` }}
+            />
+          </div>
+          <div className="control">
+            <a className="button is-info" style={{ background: CLUBS_BLUE }}>
+              Copy
+            </a>
+          </div>
+        </div>
       </div>
       <Subtitle>Import to Google Calendar</Subtitle>
       <p>Copy and paste the URL above to import to Google Calendar.</p>
