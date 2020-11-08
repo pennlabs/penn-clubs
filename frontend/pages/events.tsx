385e65f2dc47ab63c1b20d15c19de6dac9757b44
esinx <me@esinx.net>
2020-11-09 02:18:29 +0900

    ugh I wondered why modals didn't work

--- frontend/pages/events.tsx
+++ frontend/pages/events.tsx
@@ -398,7 +398,7 @@ function EventPage({
                 toolbar: CalendarHeader,
               }}
               onSelectEvent={(event: { resource: ClubEvent }) => {
-                setPreviewEvent(event)
+                setPreviewEvent(event.resource)
               }}
               events={[...liveEvents, ...upcomingEvents].map((e) => ({
                 title: e.name,
@@ -424,7 +424,7 @@ function EventPage({
       </div>
       {previewEvent && (
         <Modal show={true} closeModal={hideModal} width="45%">
-          <EventModal event={previewEvent} isHappening={true} />
+          <EventModal event={previewEvent} showDetailsButton={true} />
         </Modal>
       )}
     </>
