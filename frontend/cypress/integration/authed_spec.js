3191306ed2249e4fc66bd2dbfdcf9c7baea502a9
esinx <me@esinx.net>
2020-11-26 05:49:29 +0900

    update test

--- frontend/cypress/integration/authed_spec.js
+++ frontend/cypress/integration/authed_spec.js
@@ -64,8 +64,6 @@ describe('Authenticated user tests', () => {
   it('Visits the events page', () => {
     cy.visit('/events')
     cy.contains(new Date().getFullYear())
-    cy.get('#event-view-list').click()
-    cy.contains('Live Events')
   })
 
   it('Visits the fair page', () => {
