0e4ddd3b712bdb044fe723495e6290207785a41d
Rohan Gupta <rohangupta883@gmail.com>
2020-12-19 05:11:33 +0530

    added a help option to management command

--- backend/clubs/management/commands/import_calendar_events.py
+++ backend/clubs/management/commands/import_calendar_events.py
@@ -4,6 +4,7 @@ from clubs.models import Club
 
 
 class Command(BaseCommand):
+    help = "Imports ICS Calendar events for each club at a set frequency"
     def handle(self, *args, **kwargs):
         for club in Club.objects.filter(ics_import_url__isnull=False).exclude(ics_import_url=""):
             club.add_ics_events()
