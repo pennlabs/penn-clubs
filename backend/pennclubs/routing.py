a5d38aa5e13bbc53a61ae06cb761b2b8fe3fd308
Eric Wang <ezwang1@gmail.com>
2021-01-03 15:34:57 -0500

    check removing allowed hosts origin validator from websockets

--- backend/pennclubs/routing.py
+++ backend/pennclubs/routing.py
@@ -1,14 +1,9 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 
 import clubs.routing
 
 
 application = ProtocolTypeRouter(
-    {
-        "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(clubs.routing.websocket_urlpatterns))
-        )
-    }
+    {"websocket": AuthMiddlewareStack(URLRouter(clubs.routing.websocket_urlpatterns))}
 )
