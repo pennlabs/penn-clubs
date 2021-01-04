b642d9cfee41c916324bce3dc5fa2e2baaecde36
Eric Wang <ezwang1@gmail.com>
2021-01-03 19:09:26 -0500

    Revert "check removing allowed hosts origin validator from websockets"
    This reverts commit a5d38aa5e13bbc53a61ae06cb761b2b8fe3fd308.

--- backend/pennclubs/routing.py
+++ backend/pennclubs/routing.py
@@ -1,9 +1,14 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 
 import clubs.routing
 
 
 application = ProtocolTypeRouter(
-    {"websocket": AuthMiddlewareStack(URLRouter(clubs.routing.websocket_urlpatterns))}
+    {
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(clubs.routing.websocket_urlpatterns))
+        )
+    }
 )
