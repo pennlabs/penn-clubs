79b2d970079a669773f5ff7b6333a9ff84b27dd3
Eric Wang <ezwang1@gmail.com>
2021-01-18 16:28:56 -0500

    fix issues

--- frontend/constants/routes.ts
+++ frontend/constants/routes.ts
@@ -42,5 +42,5 @@ export const REPORT_EDIT_ROUTE = (slug?: number): string =>
 export const PROFILE_ROUTE = (slug?: string): string =>
   slug ? `/user/${slug}` : '/user/[user]'
 
-export const CLUBS_HOME = 'http://pennclubs.com/'
-export const HUB_HOME = 'http://hub.provost.upenn.edu/'
+export const CLUBS_HOME = 'https://pennclubs.com/'
+export const HUB_HOME = 'https://hub.provost.upenn.edu/'
