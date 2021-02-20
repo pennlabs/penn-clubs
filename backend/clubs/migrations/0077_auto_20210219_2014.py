07492e5e7e7a78478e8767d7ce0763eae932c1b3
cphalen <cphalen@seas.upenn.edu>
2021-02-20 01:54:11 -0500

    Lint

--- backend/clubs/migrations/0077_auto_20210219_2014.py
+++ backend/clubs/migrations/0077_auto_20210219_2014.py
@@ -9,38 +9,51 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('clubs', '0076_auto_20210214_1305'),
+        ("clubs", "0076_auto_20210214_1305"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='club',
-            name='archived',
+            model_name="club",
+            name="archived",
             field=models.BooleanField(default=False),
         ),
         migrations.AddField(
-            model_name='club',
-            name='archived_by',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='archived_clubs', to=settings.AUTH_USER_MODEL),
+            model_name="club",
+            name="archived_by",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                related_name="archived_clubs",
+                to=settings.AUTH_USER_MODEL,
+            ),
         ),
         migrations.AddField(
-            model_name='club',
-            name='archived_on',
+            model_name="club",
+            name="archived_on",
             field=models.DateTimeField(blank=True, null=True),
         ),
         migrations.AddField(
-            model_name='historicalclub',
-            name='archived',
+            model_name="historicalclub",
+            name="archived",
             field=models.BooleanField(default=False),
         ),
         migrations.AddField(
-            model_name='historicalclub',
-            name='archived_by',
-            field=models.ForeignKey(blank=True, db_constraint=False, null=True, on_delete=django.db.models.deletion.DO_NOTHING, related_name='+', to=settings.AUTH_USER_MODEL),
+            model_name="historicalclub",
+            name="archived_by",
+            field=models.ForeignKey(
+                blank=True,
+                db_constraint=False,
+                null=True,
+                on_delete=django.db.models.deletion.DO_NOTHING,
+                related_name="+",
+                to=settings.AUTH_USER_MODEL,
+            ),
         ),
         migrations.AddField(
-            model_name='historicalclub',
-            name='archived_on',
+            model_name="historicalclub",
+            name="archived_on",
             field=models.DateTimeField(blank=True, null=True),
         ),
     ]
