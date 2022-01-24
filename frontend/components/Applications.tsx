eed198bda547afa27ad81731f33ad4ecb1ef264f
alnasir7 <65636570+alnasir7@users.noreply.github.com>
2022-01-24 16:58:48 -0500

    improved styling for apply page (#502)
    * improved styling for apply page
    
    * shuffle apps

--- frontend/components/Applications.tsx
+++ frontend/components/Applications.tsx
@@ -5,21 +5,65 @@ import styled from 'styled-components'
 import { Application } from 'types'
 import { doBulkLookup } from 'utils'
 
-import { Card, Text } from '~/components/common'
+import { Text } from '~/components/common'
 import { ClubName } from '~/components/EventPage/common'
 import DateInterval from '~/components/EventPage/DateInterval'
-import { mediaMaxWidth, mediaMinWidth, PHONE, WHITE } from '~/constants'
+import {
+  ALLBIRDS_GRAY,
+  ANIMATION_DURATION,
+  BORDER_RADIUS,
+  CLUBS_GREY_LIGHT,
+  HOVER_GRAY,
+  mediaMaxWidth,
+  SM,
+  WHITE,
+} from '~/constants'
 
-const ApplicationCardContainer = styled.div`
-  ${mediaMinWidth(PHONE)} {
-    max-width: 18em;
-    margin: 1rem;
+const CardWrapper = styled.div`
+  ${mediaMaxWidth(SM)} {
+    padding-top: 0;
+    padding-bottom: 1rem;
   }
-  ${mediaMaxWidth(PHONE)} {
-    margin: 1rem 0;
+`
+
+const Description = styled.p`
+  margin-top: 0.2rem;
+  color: ${CLUBS_GREY_LIGHT};
+  border-top: 1.5px solid rgba(0, 0, 0, 0.05);
+  width: 100%;
+`
+
+const MainInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+type CardProps = {
+  readonly hovering?: boolean
+  className?: string
+}
+
+const Card = styled.div<CardProps>`
+  padding: 10px;
+  box-shadow: 0 0 0 transparent;
+  transition: all ${ANIMATION_DURATION}ms ease;
+  border-radius: ${BORDER_RADIUS};
+  box-shadow: 0 0 0 ${WHITE};
+  background-color: ${({ hovering }) => (hovering ? HOVER_GRAY : WHITE)};
+  border: 1px solid ${ALLBIRDS_GRAY};
+  justify-content: space-between;
+  height: auto;
+  cursor: pointer;
+
+  &:hover,
+  &:active,
+  &:focus {
+    box-shadow: 0 1px 6px rgba(0, 0, 0, 0.2);
   }
-  float: left;
-  cursor: pointer; !important
+
+  ${mediaMaxWidth(SM)} {
+    width: calc(100%);
+    padding: 8px;
+  }
 `
 
 const Image = styled.img`
@@ -29,35 +73,73 @@ const Image = styled.img`
   overflow: hidden;
 `
 
+const AppsContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  min-height: 60vh;
+`
+
+function shuffle(array) {
+  let currentIndex = array.length
+  let randomIndex
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex--
+    ;[array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ]
+  }
+
+  return array
+}
+
 function ApplicationsPage({ whartonapplications }): ReactElement {
   if ('detail' in whartonapplications) {
     return <Text>{whartonapplications.detail}</Text>
   }
 
+  shuffle(whartonapplications)
+
   return (
-    <>
-      {whartonapplications != null && whartonapplications.length > 0 ? (
-        whartonapplications.map((application) => (
-          <Link href={application.external_url}>
-            <ApplicationCardContainer>
-              <Card bordered hoverable background={WHITE}>
-                {application.club_image_url != null &&
-                  application.club_image_url !== '' && (
-                    <Image src={application.club_image_url} />
-                  )}
-                <DateInterval
-                  start={application.application_start_time}
-                  end={application.application_end_time}
-                />
-                <ClubName>{application.name}</ClubName>
-              </Card>
-            </ApplicationCardContainer>
-          </Link>
-        ))
-      ) : (
-        <Text>No applications are currently available.</Text>
-      )}
-    </>
+    <AppsContainer>
+      <div className="columns is-multiline is-desktop is-tablet">
+        {whartonapplications != null && whartonapplications.length > 0 ? (
+          whartonapplications.map((application) => (
+            <CardWrapper className={'column is-half-desktop'}>
+              <Link href={application.external_url}>
+                <a target="_blank">
+                  <Card className="card">
+                    <MainInfo>
+                      <div>
+                        <ClubName>{application.name}</ClubName>
+                        <DateInterval
+                          start={application.application_start_time}
+                          end={application.application_end_time}
+                        />
+                      </div>
+                      <div>
+                        {application.club_image_url != null &&
+                          application.club_image_url !== '' && (
+                            <Image src={application.club_image_url} />
+                          )}
+                      </div>
+                    </MainInfo>
+                    {application.description &&
+                      application.description.length && (
+                        <Description>{application.description}</Description>
+                      )}
+                  </Card>
+                </a>
+              </Link>
+            </CardWrapper>
+          ))
+        ) : (
+          <Text>No applications are currently available.</Text>
+        )}
+      </div>
+    </AppsContainer>
   )
 }
 
