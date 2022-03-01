480df197ed9ee0edbcd49de4536af8550adb10dc
alnasir7 <alnasirvx@gmail.com>
2022-03-01 14:43:26 -0500

    created page for tickets view

--- frontend/components/ClubEditPage/TicketsViewCard.tsx
+++ frontend/components/ClubEditPage/TicketsViewCard.tsx
@@ -1,49 +1,22 @@
-import { Field } from 'formik'
-import moment from 'moment'
-import React, { ReactElement, useState, useEffect, useMemo } from 'react'
-import { doApiRequest, getApiUrl } from '~/utils'
+
+import React, { ReactElement } from 'react'
+import { doApiRequest } from '~/utils'
 import Table from '../common/Table'
-
-import styled from 'styled-components'
-
-import { LIGHT_GRAY } from '../../constants'
-import { Club, ClubEvent, ClubEventType } from '../../types'
-import { stripTags } from '../../utils'
-import {
-  FAIR_NAME,
-  OBJECT_EVENT_TYPES,
-  OBJECT_NAME_SINGULAR,
-} from '../../utils/branding'
-import { Device, Icon, Line, Modal, Text } from '../common'
-import EventModal from '../EventPage/EventModal'
-import {
-  DateTimeField,
-  FileField,
-  RichTextField,
-  SelectField,
-  TextField,
-} from '../FormComponents'
-import { ModelForm } from '../ModelForm'
 import BaseCard from './BaseCard'
-import TicketsModal from './TicketsModal'
 
 
 export default function TicketsViewCard( {club }): ReactElement {
 
-  console.log(club.events)
 
-  const [selectedEvent, setSelectedEvent] = useState(null)
-  
-
-  useEffect(() => {
-    doApiRequest(`/events/${club.code}/applications/?format=json`, {
+  const GetTicketsHolders = (id) => {
+    doApiRequest(`/events/${id}/tickets?format=json`, {
       method: 'GET',
     })
       .then((resp) => resp.json())
       .then((res) => {
        console.log(res)
       })
-  }, [])
+  } 
 
 
   const eventsTableFields = [
@@ -55,11 +28,9 @@ export default function TicketsViewCard( {club }): ReactElement {
       render: (id) => (
         <button
           className="button is-primary"
-          onClick={(e) => {
-            console.log(id)
-            setSelectedEvent(
-              id
-            )
+          onClick={() => {
+            console.log()
+            GetTicketsHolders(id);
           }}
         >
           View
@@ -68,15 +39,6 @@ export default function TicketsViewCard( {club }): ReactElement {
     },
     
   ]
-
-  const columns = useMemo(
-    () =>
-      club.events.map(({ label, name }) => ({
-        Header: label ?? name,
-        accessor: name,
-      })),
-    [eventsTableFields],
-  )
 
   return (
     <BaseCard title="Tickets">
