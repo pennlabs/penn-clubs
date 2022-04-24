4f2682d229312f615fe2827c0509a4739998c885
printer83mph <printer.83mph@gmail.com>
2022-04-24 13:28:01 -0400

    Basic ticket cart start

--- frontend/components/TicketsPage/CartTickets.tsx
+++ frontend/components/TicketsPage/CartTickets.tsx
@@ -1,10 +1,66 @@
-import { useMemo } from 'react'
+import { ReactElement, useMemo } from 'react'
+import styled from 'styled-components'
 
-import { CountedEventTicket, EventTicket } from '~/types'
+import { CARD_HEADING, CLUBS_GREY, H1_TEXT } from '~/constants'
+import { EventTicket } from '~/types'
+
+import { Card } from '../common'
+
+export interface CountedEventTicket extends EventTicket {
+  count: number
+}
 
 export interface CartTicketsProps {
   tickets: EventTicket[]
 }
+
+const TicketCard = styled(Card)`
+  display: flex;
+  flex-direction: row;
+`
+
+const Body = styled.div`
+  margin-left: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`
+
+const XButton = styled.button`
+  width: 14px;
+  height: 14px;
+`
+
+const EventTitle = styled.strong`
+  font-size: 1.3rem;
+  line-height: 1.2;
+  color: ${H1_TEXT};
+  margin-bottom: 0.5rem;
+  font-weight: ${CARD_HEADING};
+`
+
+const ClubName = styled.div`
+  color: ${CLUBS_GREY};
+`
+
+interface TicketImageProps {
+  url: string | null
+}
+
+const THUMBNAIL_SIZE = '40px'
+
+const Thumbnail = styled.div<TicketImageProps>`
+  width: ${THUMBNAIL_SIZE};
+  height: ${THUMBNAIL_SIZE};
+  margin: 8px;
+
+  background-image: url(${({ url }) => url || '/static/img/tickets.png'});
+  background-size: contain;
+  border-radius: 3px;
+`
+
+// TODO: use ticket type as well
+const ticketKey = (t1: EventTicket) => t1.event.id
 
 /**
  * Combines an array of tickets into a list of unique ticket types with counts
@@ -13,22 +69,36 @@ export interface CartTicketsProps {
  */
 const combineTickets = (tickets: EventTicket[]): CountedEventTicket[] => {
   const tix = [...tickets]
-  const out: CountedEventTicket[] = []
+  const countedTickets: { [key: string]: CountedEventTicket } = {}
 
   while (tix.length > 0) {
-    const currentTicket = tix.pop()
-    // TODO
+    const currentTicket = tix.pop() as EventTicket
+    const key = ticketKey(currentTicket)
+    if (countedTickets[key] === undefined) {
+      countedTickets[key] = { ...currentTicket, count: 1 }
+    } else {
+      countedTickets[key].count += 1
+    }
   }
 
-  return out
+  return Object.values(countedTickets)
 }
 
-const CartTickets = ({ tickets }: CartTicketsProps) => {
+const CartTickets = ({ tickets }: CartTicketsProps): ReactElement => {
   const condensedTickets = useMemo(() => combineTickets(tickets), [tickets])
+  console.log(condensedTickets)
   return (
     <ul>
-      {tickets.map((ticket) => {
-        return <li>{ticket.toString()}</li>
+      {condensedTickets.map(({ event, id }) => {
+        return (
+          <TicketCard key={id}>
+            <Thumbnail url={event.image_url} />
+            <Body>
+              <EventTitle>{event.name}</EventTitle>
+              <ClubName>{event.club_name}</ClubName>
+            </Body>
+          </TicketCard>
+        )
       })}
     </ul>
   )
