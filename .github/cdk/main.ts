4488ce05cc335d0cca83c7c44dd74fbb15389b7b
Rohan Gupta <rohangupta883@gmail.com>
2022-07-10 00:12:56 -0700

    thank god for typescript

--- .github/cdk/main.ts
+++ .github/cdk/main.ts
@@ -6,7 +6,7 @@ const app = new App();
 new LabsApplicationStack(app, {
   djangoProjectName: 'pennclubs',
   dockerImageBaseName: 'penn-clubs',
-  integrationTests: "contains(github.event.head_commit.message, '[skip int]')",
+  integrationTests: true,
   integrationProps: {
     testCommand: 'docker-compose -f docker-compose.test.yaml exec -T frontend yarn integration',
   },
