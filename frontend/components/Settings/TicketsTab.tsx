18a546ec6548f27fd6ba4066b8e6f8ae75e0ffdd
alnasir7 <alnasirvx@gmail.com>
2022-09-25 13:32:30 -0400

    ticket transfer modal

--- frontend/components/Settings/TicketsTab.tsx
+++ frontend/components/Settings/TicketsTab.tsx
@@ -1,32 +1,99 @@
 import Link from 'next/link'
 import { ReactElement, useEffect, useState } from 'react'
-import { TypeOptions } from 'react-toastify'
 import styled from 'styled-components'
 
-import { mediaMaxWidth, mediaMinWidth, SM } from '../../constants/measurements'
-import { Club, MembershipRank, UserInfo } from '../../types'
-import { doApiRequest, formatResponse } from '../../utils'
 import {
-  OBJECT_NAME_PLURAL,
-  OBJECT_NAME_SINGULAR,
-  OBJECT_NAME_TITLE_SINGULAR,
-  SHOW_LEAVE_CONFIRMATION,
-} from '../../utils/branding'
-import { ModalContent } from '../ClubPage/Actions'
-import { Center, EmptyState, Icon, Loading, Modal, Text } from '../common'
-import ClubTabCards from './ClubTabCards'
-import ClubTabTable from './ClubTabTable'
+  mediaMaxWidth,
+  SM,
+  ANIMATION_DURATION,
+  BORDER_RADIUS,
+  CARD_HEADING,
+} from '../../constants/measurements'
+import { UserInfo } from '../../types'
+import { doApiRequest } from '../../utils'
+import {
+  Center,
+  EmptyState,
+  Icon,
+  Loading,
+  Text,
+  Title,
+  Modal,
+} from '../common'
+import {
+  ALLBIRDS_GRAY,
+  CLUBS_GREY_LIGHT,
+  H1_TEXT,
+  HOVER_GRAY,
+  WHITE,
+} from '~/constants'
 
-const ClubTable = styled(ClubTabTable)`
-  ${mediaMaxWidth(SM)} {
-    display: none !important;
+import TicketTransferModal from './TicketTransferModal'
+
+const CardHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 0.5rem;
+`
+
+const CardTitle = styled.strong`
+  line-height: 1.2;
+  color: ${H1_TEXT};
+  font-weight: ${CARD_HEADING};
+`
+
+const ActionWrapper = styled.div`
+  width: 100%;
+  margin-top: 0.5rem;
+  padding-top: 0.5rem;
+  border-top: 1.5px solid rgba(0, 0, 0, 0.05);
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+
+  & > :not(:first-child) {
+    margin-left: 5px;
   }
 `
 
-const ClubCards = styled(ClubTabCards)`
-  ${mediaMinWidth(SM)} {
-    display: none !important;
+type CardProps = {
+  readonly hovering?: boolean
+  className?: string
+}
+
+const Card = styled.div<CardProps>`
+  padding: 10px;
+  margin-top: 1rem;
+  box-shadow: 0 0 0 transparent;
+  transition: all ${ANIMATION_DURATION}ms ease;
+  border-radius: ${BORDER_RADIUS};
+  box-shadow: 0 0 0 ${WHITE};
+  background-color: ${({ hovering }) => (hovering ? HOVER_GRAY : WHITE)};
+  border: 1px solid ${ALLBIRDS_GRAY};
+  justify-content: space-between;
+  height: auto;
+
+  &:hover,
+  &:active,
+  &:focus {
+    box-shadow: 0 1px 6px rgba(0, 0, 0, 0.2);
   }
+
+  ${mediaMaxWidth(SM)} {
+    width: calc(100%);
+    padding: 8px;
+  }
+`
+const Description = styled.p`
+  margin-bottom: 0.5rem;
+  color: ${CLUBS_GREY_LIGHT};
+  width: 100%;
+`
+const TitleWrapper = styled.div`
+  margin-top: 1.5rem;
+  color: ${CLUBS_GREY_LIGHT};
+  width: 100%;
 `
 
 type TicketsTabProps = {
@@ -36,14 +103,17 @@ type TicketsTabProps = {
 
 // remove later, for testing
 const ticks = [
-  { event: 'champions league', class: 'regular' },
-  { event: 'champions league', class: 'regular' },
-  { event: 'champions league', class: 'regular' },
-  { event: 'champions league', class: 'regular' },
+  { event: 'Champions league', class: 'General Admission', club: 'Uefa' },
+  { event: 'Champions league', class: 'General Admission', club: 'Uefa' },
+  { event: 'Champions league', class: 'General Admission', club: 'Uefa' },
+  { event: 'Champions league', class: 'General Admission', club: 'Uefa' },
 ]
 
 const TicketsTab = ({ className, userInfo }: TicketsTabProps): ReactElement => {
   const [tickets, setTickets] = useState<any>(null)
+  const [show, setShow] = useState<boolean>(false)
+  const showModal = () => setShow(true)
+  const hideModal = () => setShow(false)
 
   const getTickets = () => {
     return doApiRequest('/tickets?format=json')
@@ -61,7 +131,64 @@ const TicketsTab = ({ className, userInfo }: TicketsTabProps): ReactElement => {
   }
 
   return tickets.length ? (
-    <>{'hi'}</>
+    <div>
+      {show && (
+        <Modal
+          width="50vw"
+          show={show}
+          closeModal={hideModal}
+          marginBottom={false}
+        >
+          <TicketTransferModal event={null} />
+        </Modal>
+      )}
+      <TitleWrapper>
+        <Title>Browse Your Tickets</Title>
+      </TitleWrapper>
+      {tickets.map((ticket) => (
+        <Card className="card">
+          <div style={{ display: 'flex' }}>
+            <div>
+              <img
+                style={{ marginRight: '1rem', height: '60px' }}
+                src="/static/img/events_calendar.png"
+              />
+            </div>
+            <div>
+              <div>
+                <CardHeader>
+                  <CardTitle className="is-size-5">{ticket.event}</CardTitle>
+                </CardHeader>
+              </div>
+              <Description>{ticket.class}</Description>
+            </div>
+          </div>
+          <ActionWrapper>
+            <div style={{ flex: 1 }}></div>
+            <div style={{ flex: 1 }}>
+              <div
+                className="is-pulled-right"
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                  width: '100%',
+                }}
+              >
+                <div>
+                  <Link href="/events">
+                    <a>View QR Code</a>
+                  </Link>
+                </div>
+                <div style={{ cursor: 'pointer' }} onClick = {showModal}>
+                  Transfer Ownership <Icon name="send" />
+                </div>
+              </div>
+            </div>
+          </ActionWrapper>
+        </Card>
+      ))}
+    </div>
   ) : (
     <>
       <EmptyState name="empty_cart" />
