78dd1cd2dfc965d47aa41008a273d3c576ed07f2
Mohamed Abaker <mabaker@vagelos-ve703-08269.apn.wlan.private.upenn.edu>
2022-11-18 12:50:17 -0500

    rebase with ticketing

--- backend/clubs/serializers.py
+++ backend/clubs/serializers.py
@@ -344,11 +344,7 @@ class ClubEventSerializer(serializers.ModelSerializer):
     creator = serializers.HiddenField(default=serializers.CurrentUserDefault())
 
     def get_ticketed(self, obj) -> bool:
-<<<<<<< HEAD
         return obj.tickets.count() > 0
-=======
-        return Event.tickets.exists()
->>>>>>> 4abb6bbb (rebase master)
 
     def get_event_url(self, obj):
         # if no url, return that
