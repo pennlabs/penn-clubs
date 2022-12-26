7f5cf4ccf01cbed3447ddeba29a896a38d70e2c3
Avi Upadhyayula <69180850+aviupadhyayula@users.noreply.github.com>
2022-12-26 01:47:23 -0800

    Randomly order active Wharton apps (should be fixed from #547) (#556)

---
+++ backend/clubs/migrations/0091_cryptoextension.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.contrib.postgres.operations import CryptoExtension
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ("clubs", "0090_adminnote"),
+    ]
+
+    operations = [CryptoExtension()]
