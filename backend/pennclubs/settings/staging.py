1f5a28bbe6e9540a73f60ea8121c89884861f567
Alexander Kyimpopkin <39439486+alxkp@users.noreply.github.com>
2023-02-03 18:31:54 -0500

    created django alternate database

--- backend/pennclubs/settings/staging.py
+++ backend/pennclubs/settings/staging.py
@@ -1,1 +1,9 @@
 from pennclubs.settings.base import *  # noqa: F401, F403
+
+if "STAGING_DATABASE_URL" in os.environ and "DATABASE_NAME" in os.environ:
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("STAGING_DATABASE_URL")+"/"+os.environ.get("DATABASE_NAME"))
+    }
+else:
+    raise Exception("ERROR: Using staging environment without setting environments")
+    DATABASES = {}
