877551a5ac5f8d13a34ee47103af214ecb06ecad
Eunsoo Shin (esinx) <me@esinx.net>
2023-10-14 17:13:03 -0400

    temp:(banner)

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -6,6 +6,7 @@ import 'react-toastify/dist/ReactToastify.min.css'
 import { AppProps } from 'next/app'
 import { ReactElement } from 'react'
 
+import { Announcement } from '~/components/Announcement'
 import RouteProgressBar from '~/components/RouteProgressBar'
 
 const App = ({ Component, pageProps }: AppProps): ReactElement => {
@@ -13,6 +14,18 @@ const App = ({ Component, pageProps }: AppProps): ReactElement => {
     <>
       <Component {...pageProps} />
       <RouteProgressBar />
+      <div
+        style={{
+          zIndex: 10000,
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          right: 0,
+          padding: 20,
+        }}
+      >
+        <Announcement type="issue" title="Weekend Maintenance Alert" />
+      </div>
     </>
   )
 }
