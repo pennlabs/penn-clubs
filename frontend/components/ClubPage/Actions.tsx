b5422f2ed4e8434fb5012b93145c9fa23aafa7c2
Julian Weng <julian.weng.us@gmail.com>
2024-01-29 22:52:17 -0500

    Remove member check from request membership and apply buttons to display proper status of request.

--- frontend/components/ClubPage/Actions.tsx
+++ frontend/components/ClubPage/Actions.tsx
@@ -284,7 +284,6 @@ const Actions = ({
       <div className={className} style={style}>
         <Wrapper>
           {SHOW_MEMBERSHIP_REQUEST &&
-            !inClub &&
             club.members.length > 0 &&
             isMembershipOpen &&
             club.accepting_members && (
@@ -293,20 +292,17 @@ const Actions = ({
                 updateRequests={updateRequests}
               />
             )}
-          {SHOW_APPLICATIONS &&
-            !inClub &&
-            !isMembershipOpen &&
-            club.accepting_members && (
-              <Link
-                href={CLUB_APPLY_ROUTE()}
-                as={CLUB_APPLY_ROUTE(code)}
-                passHref
-              >
-                <ActionButton className="button is-success">
-                  <Icon name="edit" /> Apply
-                </ActionButton>
-              </Link>
-            )}
+          {SHOW_APPLICATIONS && !isMembershipOpen && club.accepting_members && (
+            <Link
+              href={CLUB_APPLY_ROUTE()}
+              as={CLUB_APPLY_ROUTE(code)}
+              passHref
+            >
+              <ActionButton className="button is-success">
+                <Icon name="edit" /> Apply
+              </ActionButton>
+            </Link>
+          )}
           {canEdit && (
             <Link href={CLUB_EDIT_ROUTE()} as={CLUB_EDIT_ROUTE(code)} passHref>
               <ActionButton className="button is-success">
