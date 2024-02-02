cb4104e7dbb902af320ab3ceef4e9ba396326e63
Joy Liu <34288846+joyliu-q@users.noreply.github.com>
2024-02-02 16:51:28 -0500

    :arrow_up: Scale Up (2024 Fall Edition)

--- k8s/main.ts
+++ k8s/main.ts
@@ -31,7 +31,7 @@ export class MyChart extends PennLabsChart {
     new DjangoApplication(this, 'django-wsgi', {
       deployment: {
         image: backendImage,
-        replicas: 4,
+        replicas: 9,
         secret: clubsSecret,
         env: [
           { name: 'REDIS_HOST', value: 'penn-clubs-redis' },
@@ -60,7 +60,7 @@ export class MyChart extends PennLabsChart {
     new ReactApplication(this, 'react', {
       deployment: {
         image: frontendImage,
-        replicas: 3,
+        replicas: 5,
       },
       domain: { host: clubsDomain, paths: ['/'] },
       port: 80,
