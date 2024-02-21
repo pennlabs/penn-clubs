36bb1f8ca22cf26c43f58d1e3af733c7e3091cb1
aviupadhyayula <aupadhy@gmail.com>
2024-02-20 23:11:38 -0500

    Whitelist localhost from CSRF

--- backend/pennclubs/settings/development.py
+++ backend/pennclubs/settings/development.py
@@ -4,6 +4,9 @@ from pennclubs.settings.base import *  # noqa: F401, F403
 from pennclubs.settings.base import INSTALLED_APPS, MIDDLEWARE
 
 
+# Enable debug mode
+DEBUG = True
+
 # Development extensions
 INSTALLED_APPS += ["django_extensions", "debug_toolbar"]
 
@@ -12,6 +15,9 @@ INTERNAL_IPS = ["127.0.0.1"]
 
 # Allow http callback for DLA
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+# Allow requests from frontend
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
 
 # Use console email backend during development
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
