b6c4d91fd432b78eb6e018c4778ec7fae47fc7e9
Julian Weng <julian.weng.us@gmail.com>
2024-02-23 18:28:41 -0500

    BUN!

---
+++ frontend/pages/api/[...all].ts
@@ -0,0 +1,38 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import httpProxyMiddleware from 'next-http-proxy-middleware'
+
+const dev = process.env.NODE_ENV !== 'production'
+
+export const config = {
+  api: {
+    externalResolver: dev,
+  },
+}
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (dev) {
+    await httpProxyMiddleware(req, res, {
+      ws: true,
+      followRedirects: true,
+      target: 'http://localhost:8000',
+      pathRewrite: [
+        {
+          patternStr: '^/api',
+          replaceStr: '/api',
+        },
+      ],
+    })
+    await httpProxyMiddleware(req, res, {
+      followRedirects: true,
+      target: 'http://localhost:8000',
+      pathRewrite: [
+        {
+          patternStr: '^/__debug__',
+          replaceStr: '/__debug__',
+        },
+      ],
+    })
+  } else {
+    res.status(404).send(null)
+  }
+}
