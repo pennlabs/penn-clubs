0b746b2675fd7cd9f47e0eab1550729a8c0727ed
Eunsoo Shin <me@esinx.net>
2024-04-17 04:10:45 +0000

    wip(payment)

--- frontend/components/Tickets/PaymentForm.tsx
+++ frontend/components/Tickets/PaymentForm.tsx
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Script from 'next/script'
 import React from 'react'
 
 type PaymentFormProps = {
@@ -9,35 +9,31 @@ const PaymentForm = ({ token }: PaymentFormProps) => {
   const captureContext = token
   return (
     <>
-      <Head>
-        <script
-          src="https://apitest.cybersource.com/up/v1/assets/0.15/SecureAcceptance.js"
-          async
-          onLoad={() => {
-            const acceptFn = (window as any).Accept
-            if (acceptFn) {
-              const showArgs = {
-                containers: {
-                  paymentSelection: '#buttonPaymentListContainer',
-                },
-              }
-              acceptFn(captureContext)
-                .then((accept) => accept.unifiedPayments())
-                .then((up) => up.show(showArgs))
-                .then((tt) => {
-                  // console.log('Transient Token:', tt)
-                  // authForm.submit();
-                })
-                .catch((error) => {
-                  // eslint-disable-next-line no-console
-                  console.error('Error initializing payment:', error)
-                })
+      <Script
+        src="https://apitest.cybersource.com/up/v1/assets/0.15/SecureAcceptance.js"
+        async
+        onLoad={() => {
+          const acceptFn = (window as any).Accept
+          if (acceptFn) {
+            const showArgs = {
+              containers: {
+                paymentSelection: '#buttonPaymentListContainer',
+              },
             }
-          }}
-        ></script>
-      </Head>
+            acceptFn(captureContext)
+              .then((accept) => accept.unifiedPayments(false))
+              .then((up) => up.show(showArgs))
+              .then((tt) => {
+                // authForm.submit();
+              })
+              .catch((error) => {
+                // eslint-disable-next-line no-console
+                console.error('Error initializing payment:', error)
+              })
+          }
+        }}
+      />
       <div style={{ width: '40vw' }}>
-        <p>{captureContext}</p>
         <div
           id="buttonPaymentListContainer"
           className="buttonPaymentListContainer"
