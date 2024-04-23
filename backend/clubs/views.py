pennlabs/penn-clubs

commit 8847a3b04b43cb2eb5623449f503a93e36f67303

Ticketing Cart Pre-Checkout  (#670)
* Cart skeleton

* Basic UI to payment.

* More appropriately-sized shopping cart icon

* Payment integration 1st step.

* fix(move)

* fix(TicketCard): extract and abstract

* checkout flow ui

* wip(payment)

* :tada: Add ability to remove tickets from cart

* :bug: Better backend error and remove cart logic

* :tada: Modify sold_out to return event type and count

* :tada: Add cart empty view and edit mode

* :art: Add empty view

* :art: Grafik design is my passion

* :tada: Modify edit success and display toast, correct e2e behavior

* :bug: Change color to make edit mode more obvious

* :tada: Toast for sold out tickets

* Add frontend auth check to cart page and fix sold_out toast functionality (multiple toasts per ticket, ticket event name)

* Fix backend tests for group discounts, new cart API, and more (#675)

* Refactor cart summation to helper fn

* Add tests for group discounts and cart totaling

* Minor change to docs

* Only populate `sold_out_tickets` if tickets cannot be replaced

* Refactor tests to use new cart API

* Make openAPI docs happy

* Make `_calculate_cart_total` static method

* Group discount shouldn't activate below threshold

* Fix API docs & improve tests

* Add minor subtest

* Align tests with new API

* Improve invalid ticket replacement

* :tada: Default to 1 ticket when buying smh

* :tada: Add a bunch of style and features

* :art: Kinda responsive

---------

Co-authored-by: Julian Weng <julian.weng.us@gmail.com>
Co-authored-by: Eunsoo Shin <me@esinx.net>
Co-authored-by: Avi Upadhyayula <69180850+aviupadhyayula@users.noreply.github.com>
Co-authored-by: aviupadhyayula <aupadhy@gmail.com>

backend/clubs/views.py
@@ -2402,7 +2402,7 @@ class ClubEventViewSet(viewsets.ModelViewSet):
         quantities = request.data.get("quantities")
         if not quantities:
             return Response(
-                {"detail": "Quantities must be specified"},
+                {"detail": "Quantities must be specified", "success": False},
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
@@ -2413,7 +2413,8 @@ class ClubEventViewSet(viewsets.ModelViewSet):
             return Response(
                 {
                     "detail": f"Order exceeds the maximum ticket limit of "
-                    f"{event.ticket_order_limit}."
+                    f"{event.ticket_order_limit}.",
+                    "success": False,
                 },
                 status=status.HTTP_403_FORBIDDEN,
             )
@@ -2440,7 +2441,9 @@ class ClubEventViewSet(viewsets.ModelViewSet):
             cart.tickets.add(*tickets[:count])
 
         cart.save()
-        return Response({"detail": "Successfully added to cart"})
+        return Response(
+            {"detail": f"Successfully added {count} to cart", "success": True}
+        )
 
     @action(detail=True, methods=["post"])
     @transaction.atomic
@@ -2473,13 +2476,26 @@ class ClubEventViewSet(viewsets.ModelViewSet):
                            properties:
                                 detail:
                                     type: string
+                                success:
+                                    type: boolean
         ---
         """
         event = self.get_object()
         quantities = request.data.get("quantities")
         if not quantities:
             return Response(
-                {"detail": "Quantities must be specified"},
+                {
+                    "detail": "Quantities must be specified",
+                    "success": False,
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        if not all(isinstance(item, dict) for item in quantities):
+            return Response(
+                {
+                    "detail": "Quantities must be a list of dictionaries",
+                    "success": False,
+                },
                 status=status.HTTP_400_BAD_REQUEST,
             )
         cart = get_object_or_404(Cart, owner=self.request.user)
@@ -2494,7 +2510,9 @@ class ClubEventViewSet(viewsets.ModelViewSet):
             cart.tickets.remove(*tickets_to_remove[:count])
 
         cart.save()
-        return Response({"detail": "Successfully removed from cart"})
+        return Response(
+            {"detail": f"Successfully removed {count} from cart", "success": True}
+        )
 
     @action(detail=True, methods=["get"])
     def buyers(self, request, *args, **kwargs):
@@ -2621,6 +2639,7 @@ class ClubEventViewSet(viewsets.ModelViewSet):
                                             required: false
                                         group_discount:
                                             type: number
+                                            format: float
                                             required: false
                                         transferable:
                                             type: boolean
@@ -4830,6 +4849,30 @@ class TicketViewSet(viewsets.ModelViewSet):
     http_method_names = ["get", "post"]
     lookup_field = "id"
 
+    @staticmethod
+    def _calculate_cart_total(cart) -> float:
+        """
+        Calculate the total price of all tickets in a cart, applying discounts
+        where appropriate. Does not validate that the cart is valid.
+
+        :param cart: Cart object
+        :return: Total price of all tickets in the cart
+        """
+        ticket_type_counts = {
+            item["type"]: item["count"]
+            for item in cart.tickets.values("type").annotate(count=Count("type"))
+        }
+        cart_total = sum(
+            (
+                ticket.price * (1 - ticket.group_discount)
+                if ticket.group_size
+                and ticket_type_counts[ticket.type] >= ticket.group_size
+                else ticket.price
+            )
+            for ticket in cart.tickets.all()
+        )
+        return cart_total
+
     @transaction.atomic
     @update_holds
     @action(detail=False, methods=["get"])
@@ -4850,7 +4893,21 @@ class TicketViewSet(viewsets.ModelViewSet):
                                     allOf:
                                         - $ref: "#/components/schemas/Ticket"
                                 sold_out:
-                                    type: integer
+                                    type: array
+                                    items:
+                                        type: object
+                                        properties:
+                                            event:
+                                                type: object
+                                                properties:
+                                                    id:
+                                                        type: integer
+                                                    name:
+                                                        type: string
+                                            type:
+                                                type: string
+                                            count:
+                                                type: integer
         ---
         """
 
@@ -4868,27 +4925,47 @@ class TicketViewSet(viewsets.ModelViewSet):
             return Response(
                 {
                     "tickets": TicketSerializer(cart.tickets.all(), many=True).data,
-                    "sold_out": 0,
+                    "sold_out": [],
                 },
             )
 
-        sold_out_count = 0
+        sold_out_tickets, replacement_tickets = [], []
 
-        replacement_tickets = []
         tickets_in_cart = cart.tickets.values_list("id", flat=True)
+        event_dict = {
+            event["id"]: event["name"]
+            for event in Event.objects.filter(
+                id__in=tickets_to_replace.values_list("event", flat=True).distinct()
+            ).values("id", "name")
+        }
+
+        # Process each ticket type and event
         for ticket_class in tickets_to_replace.values("type", "event").annotate(
             count=Count("id")
         ):
-            # We don't need to lock since we aren't updating holder/owner
-            tickets = Ticket.objects.filter(
-                event=ticket_class["event"],
-                type=ticket_class["type"],
-                owner__isnull=True,
-                holder__isnull=True,
-            ).exclude(id__in=tickets_in_cart)[: ticket_class["count"]]
+            available_tickets = (
+                Ticket.objects.filter(
+                    event=ticket_class["event"],
+                    type=ticket_class["type"],
+                    owner__isnull=True,
+                    holder__isnull=True,
+                )
+                .exclude(id__in=tickets_in_cart)
+                .select_related("event")[: ticket_class["count"]]
+            )
 
-            sold_out_count += ticket_class["count"] - tickets.count()
-            replacement_tickets.extend(list(tickets))
+            shortage = ticket_class["count"] - available_tickets.count()
+            if shortage > 0:
+                sold_out_tickets.append(
+                    {
+                        **ticket_class,
+                        "event": {
+                            "id": ticket_class["event"],
+                            "name": event_dict[ticket_class["event"]],
+                        },
+                        "count": shortage,
+                    }
+                )
 
         cart.tickets.remove(*tickets_to_replace)
         if replacement_tickets:
@@ -4898,7 +4975,7 @@ class TicketViewSet(viewsets.ModelViewSet):
         return Response(
             {
                 "tickets": TicketSerializer(cart.tickets.all(), many=True).data,
-                "sold_out": sold_out_count,
+                "sold_out": sold_out_tickets,
             },
         )
 
@@ -4969,19 +5046,7 @@ class TicketViewSet(viewsets.ModelViewSet):
                 status=status.HTTP_403_FORBIDDEN,
             )
 
-        # Calculate cart total, applying group discounts where appropriate
-        ticket_type_counts = {
-            item["type"]: item["count"]
-            for item in cart.tickets.values("type").annotate(count=Count("type"))
-        }
-
-        cart_total = sum(
-            ticket.price * (1 - ticket.group_discount)
-            if ticket.group_size
-            and ticket_type_counts[ticket.type] >= ticket.group_size
-            else ticket.price
-            for ticket in tickets
-        )
+        cart_total = self._calculate_cart_total(cart)
 
         if not cart_total:
             return Response(

backend/tests/clubs/test_ticketing.py
@@ -651,7 +651,56 @@ class TicketTestCase(TestCase):
         self.assertEqual(len(data["tickets"]), 5, data)
         for t1, t2 in zip(data["tickets"], tickets_to_add):
             self.assertEqual(t1["id"], str(t2.id))
-        self.assertEqual(data["sold_out"], 0, data)
+        self.assertEqual(len(data["sold_out"]), 0, data)
+
+    def test_calculate_cart_total(self):
+        # Add a few tickets to cart
+        cart, _ = Cart.objects.get_or_create(owner=self.user1)
+        tickets_to_add = self.tickets1[:5]
+        for ticket in tickets_to_add:
+            cart.tickets.add(ticket)
+        cart.save()
+
+        expected_total = sum(t.price for t in tickets_to_add)
+
+        from clubs.views import TicketViewSet
+
+        actual_total = TicketViewSet._calculate_cart_total(cart)
+        self.assertEqual(actual_total, expected_total)
+
+    def test_calculate_cart_total_with_group_discount(self):
+        # Create tickets with group discount
+        tickets = [
+            Ticket.objects.create(
+                type="group",
+                event=self.event1,
+                price=10.0,
+                group_size=2,
+                group_discount=0.2,
+            )
+            for _ in range(10)
+        ]
+
+        cart, _ = Cart.objects.get_or_create(owner=self.user1)
+        from clubs.views import TicketViewSet
+
+        # Add 1 ticket, shouldn't activate group discount
+        cart.tickets.add(tickets[0])
+        cart.save()
+
+        total = TicketViewSet._calculate_cart_total(cart)
+        self.assertEqual(total, 10.0)  # 1 * price=10 = 10
+
+        # Add 4 more tickets, enough to activate group discount
+        tickets_to_add = tickets[1:5]
+        for ticket in tickets_to_add:
+            cart.tickets.add(ticket)
+        cart.save()
+
+        self.assertEqual(cart.tickets.count(), 5)
+
+        total = TicketViewSet._calculate_cart_total(cart)
+        self.assertEqual(total, 40.0)  # 5 * price=10 * (1 - group_discount=0.2) = 40
 
     def test_get_cart_replacement_required(self):
         self.client.login(username=self.user1.username, password="test")
@@ -673,7 +722,7 @@ class TicketTestCase(TestCase):
 
         # The cart still has 5 tickets: just replaced with available ones
         self.assertEqual(len(data["tickets"]), 5, data)
-        self.assertEqual(data["sold_out"], 0, data)
+        self.assertEqual(len(data["sold_out"]), 0, data)
 
         in_cart = set(map(lambda t: t["id"], data["tickets"]))
         to_add = set(map(lambda t: str(t.id), tickets_to_add))
@@ -704,13 +753,24 @@ class TicketTestCase(TestCase):
         # The cart now has 3 tickets
         self.assertEqual(len(data["tickets"]), 3, data)
 
-        # 2 tickets have been sold out
-        self.assertEqual(data["sold_out"], 2, data)
+        # Only 1 type of ticket should be sold out
+        self.assertEqual(len(data["sold_out"]), 1, data)
 
-        in_cart = set(map(lambda t: t["id"], data["tickets"]))
-        to_add = set(map(lambda t: str(t.id), tickets_to_add))
+        # 2 normal tickets should be sold out
+        expected_sold_out = {
+            "type": self.tickets1[0].type,
+            "event": {
+                "id": self.tickets1[0].event.id,
+                "name": self.tickets1[0].event.name,
+            },
+            "count": 2,
+        }
+        for key, val in expected_sold_out.items():
+            self.assertEqual(data["sold_out"][0][key], val, data)
 
         # 0 tickets are the same (we sell all but last 3)
+        in_cart = set(map(lambda t: t["id"], data["tickets"]))
+        to_add = set(map(lambda t: str(t.id), tickets_to_add))
         self.assertEqual(len(in_cart & to_add), 0, in_cart | to_add)
 
     def test_initiate_checkout(self):
@@ -908,6 +968,7 @@ class TicketTestCase(TestCase):
             held_tickets = Ticket.objects.filter(holder=self.user1)
             self.assertEqual(held_tickets.count(), 2, held_tickets)
 
+            # Complete checkout
             resp = self.client.post(
                 reverse("tickets-complete-checkout"),
                 {"transient_token": "abcdefg"},
@@ -929,9 +990,10 @@ class TicketTestCase(TestCase):
             self.assertEqual(held_tickets.count(), 0, held_tickets)
 
             # Transaction record created
-            TicketTransactionRecord.objects.filter(
+            record_exists = TicketTransactionRecord.objects.filter(
                 reconciliation_id=MockPaymentResponse().reconciliation_id
             ).exists()
+            self.assertTrue(record_exists)
 
     def test_complete_checkout_stale_cart(self):
         self.client.login(username=self.user1.username, password="test")

frontend/components/ClubEditPage/EventsCard.tsx
@@ -465,11 +465,13 @@ export default function EventsCard({ club }: EventsCardProps): ReactElement {
         tableFields={eventTableFields}
         noun="Event"
         currentTitle={(obj) => (obj != null ? obj.name : 'Deleted Event')}
-        onChange={(obj) => {
+        onEditPressed={() => {
           eventDetailsRef.current?.scrollIntoView({
             behavior: 'smooth',
             block: 'start',
           })
+        }}
+        onChange={(obj) => {
           setDeviceContents(obj)
         }}
       />

frontend/components/ClubPage/Actions.tsx
@@ -8,6 +8,8 @@ import {
 } from 'react'
 import styled from 'styled-components'
 
+import { mediaMaxWidth, mediaMinWidth, SM } from '~/constants'
+
 import { BORDER, MEDIUM_GRAY, WHITE } from '../../constants/colors'
 import { CLUB_APPLY_ROUTE, CLUB_EDIT_ROUTE } from '../../constants/routes'
 import {
@@ -383,13 +385,13 @@ export const QuestionFollowUpAction = ({
 }
 
 export const DesktopActions = styled(Actions)`
-  @media (max-width: 768px) {
+  ${mediaMaxWidth(SM)} {
     display: none !important;
   }
 `
 
 export const MobileActions = styled(Actions)`
-  @media (min-width: 769px) {
+  ${mediaMinWidth(SM)} {
     display: none !important;
   }
 `

frontend/components/ModelForm.tsx
@@ -68,6 +68,7 @@ type ModelFormProps = {
   baseUrl: string
   keyField?: string
   onUpdate?: (objects: ModelObject[]) => void
+  onEditPressed?: () => void
   onChange?: (object: ModelObject) => void
   defaultObject?: ModelObject
   fileFields?: string[]
@@ -272,6 +273,7 @@ export const ModelForm = (props: ModelFormProps): ReactElement => {
     actions,
     keyField = 'id',
     onChange: parentComponentChange,
+    onEditPressed = () => undefined,
     draggable = false,
   } = props
 
@@ -464,6 +466,7 @@ export const ModelForm = (props: ModelFormProps): ReactElement => {
       <>
         <ModelTable
           onEdit={(object): void => {
+            onEditPressed()
             changeCurrentlyEditing(object[keyField])
             onChange(object)
           }}

frontend/components/Settings/TicketsTab/index.tsx
@@ -1,15 +1,13 @@
-import moment from 'moment-timezone'
 import Link from 'next/link'
 import { ReactElement, useEffect, useState } from 'react'
 import styled from 'styled-components'
 
+import { TicketCard } from '~/components/Tickets/TicketCard'
 import {
   ALLBIRDS_GRAY,
-  CLUBS_BLUE,
   CLUBS_GREY_LIGHT,
   H1_TEXT,
   HOVER_GRAY,
-  HUB_SNOW,
   WHITE,
 } from '~/constants'
 
@@ -23,15 +21,7 @@ import {
 import { UserInfo } from '../../../types'
 import { doApiRequest } from '../../../utils'
 import QRCodeCard, { QRCodeType } from '../../ClubEditPage/QRCodeCard'
-import {
-  Center,
-  EmptyState,
-  Icon,
-  Loading,
-  Modal,
-  Text,
-  Title,
-} from '../../common'
+import { Center, EmptyState, Loading, Modal, Text, Title } from '../../common'
 import TicketTransferModal from '../TicketTransferModal'
 
 const CardHeader = styled.div`
@@ -115,188 +105,6 @@ const TitleWrapper = styled.div`
 type TicketsTabProps = {
   className?: string
   userInfo: UserInfo
-}
-
-const formatTime = (startTime: string, endTime: string) => {
-  const date = new Date(startTime)
-  // return the month and date
-  const dayDuration = new Date(endTime).getDate() - date.getDate()
-  const timezone = moment.tz.guess()
-  const startFormatted = moment(startTime)
-    .tz(timezone)
-    .format(dayDuration === 0 ? 'h:mmA' : 'MMM D, h:mmA')
-  const endFormatted = moment(endTime)
-    .tz(timezone)
-    .format(dayDuration === 0 ? 'h:mmA z' : 'MMM D, h:mmA z')
-
-  return {
-    month: date.toLocaleString('default', { month: 'short' }),
-    day: date.getDate(),
-    timeRange: `${startFormatted} — ${endFormatted}`,
-    dayDuration,
-  }
-}
-
-const TicketCard = ({
-  collapsed = 0,
-  ticket,
-  showModal,
-  props,
-  onClick,
-  viewQRCode,
-}: {
-  collapsed?: number
-  ticket: any
-  showModal: () => void
-  props?: any
-  onClick?: () => void
-  viewQRCode?: () => void
-}) => {
-  const datetimeData = formatTime(
-    ticket.event.start_time,
-    ticket.event.end_time,
-  )
-  function generateBoxShadow(collapsed) {
-    let boxShadow = ''
-    boxShadow += '0 1px 6px rgba(0, 0, 0, 0.2),\n'
-    for (let i = 1; i <= collapsed; i++) {
-      boxShadow += `${i * 10}px -${i * 10}px 0 -1px ${HUB_SNOW}, ${i * 10}px -${i * 10}px rgba(0, 0, 0, 0.1)${
-        i !== collapsed ? ',\n' : ''
-      }`
-    }
-    return boxShadow
-  }
-  return (
-    <Card
-      className="card"
-      style={{
-        ...props,
-        display: 'flex',
-        cursor: 'pointer',
-        ...(collapsed !== 0
-          ? {
-              boxShadow: generateBoxShadow(collapsed),
-            }
-          : {}),
-        margin: collapsed !== 0 ? '4rem 0' : '1rem 0',
-      }}
-      onClick={(e) => {
-        onClick?.()
-      }}
-    >
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          padding: '16px',
-          background: CLUBS_BLUE,
-          borderRadius: '8px',
-          color: WHITE,
-          width: '80px',
-        }}
-      >
-        <Title
-          style={{
-            marginBottom: 0,
-            color: WHITE,
-            display: 'flex',
-            alignItems: 'top',
-          }}
-        >
-          {datetimeData.day}
-          {datetimeData.dayDuration !== 0 && (
-            <div
-              style={{
-                fontSize: '12px',
-                position: 'relative',
-                right: 0,
-                top: 0,
-              }}
-            >
-              {datetimeData.dayDuration < 0 ? '-' : '+'}{' '}
-              {Math.abs(datetimeData.dayDuration)}
-            </div>
-          )}
-        </Title>
-        <Description
-          style={{
-            color: WHITE,
-            width: '100%',
-            textAlign: 'center',
-            marginBottom: 0,
-          }}
-        >
-          {datetimeData.month}
-        </Description>
-      </div>
-      <div style={{ width: '20px' }} />
-      <div style={{ flex: 1 }}>
-        <Description
-          style={{
-            fontWeight: 600,
-          }}
-        >
-          {ticket.event.name}
-        </Description>
-        <Description>{ticket.event.club_name}</Description>
-        <Description>
-          {ticket.type} | {datetimeData.timeRange}
-        </Description>
-      </div>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-        }}
-      >
-        <div>
-          <button
-            className="button is-primary"
-            style={{
-              backgroundColor: CLUBS_BLUE,
-              padding: '16px',
-              border: '1px solid ' + CLUBS_BLUE,
-              cursor: 'pointer',
-            }}
-            onClick={(e) => {
-              e.stopPropagation()
-              viewQRCode?.()
-            }}
-          >
-            QR Code
-            <Icon
-              name="qr-code"
-              size="2rem"
-              style={{
-                color: WHITE,
-              }}
-            />
-          </button>
-        </div>
-        <div style={{ width: '12px' }} />
-        <button
-          className="button is-primary"
-          style={{ cursor: 'pointer' }}
-          onClick={(e) => {
-            e.stopPropagation()
-            showModal?.()
-          }}
-        >
-          Transfer Ownership
-          <Icon
-            name="swap-horiz"
-            size="2rem"
-            style={{
-              color: WHITE,
-            }}
-          />
-        </button>
-      </div>
-    </Card>
-  )
 }
 
 const TicketsTab = ({ className, userInfo }: TicketsTabProps): ReactElement => {
@@ -383,7 +191,6 @@ const TicketsTab = ({ className, userInfo }: TicketsTabProps): ReactElement => {
               Uncollapse
             </a>
           )}
-
           {expandedEvents.has(group[0]) || group[1].length === 1 ? (
             group[1].map((ticket, i) => (
               <TicketCard

commit 57c9645c24eeb1edb31c520615a739bd4231eb55

Fix breaking changes introduced in #670

backend/clubs/views.py
@@ -4878,7 +4878,8 @@ class TicketViewSet(viewsets.ModelViewSet):
     @action(detail=False, methods=["get"])
     def cart(self, request, *args, **kwargs):
         """
-        Validate tickets in a cart and return them
+        Validate tickets in a cart and return them. Replace in-cart tickets that
+        have been bought/held by someone else.
         ---
         requestBody:
             content: {}
@@ -4915,7 +4916,6 @@ class TicketViewSet(viewsets.ModelViewSet):
             owner=self.request.user
         )
 
-        # Replace in-cart tickets that have been bought/held by someone else
         tickets_to_replace = cart.tickets.filter(
             Q(owner__isnull=False) | Q(holder__isnull=False)
         ).exclude(holder=self.request.user)
@@ -4929,43 +4929,37 @@ class TicketViewSet(viewsets.ModelViewSet):
                 },
             )
 
-        sold_out_tickets, replacement_tickets = [], []
+        # Attempt to replace all tickets that have gone stale
+        replacement_tickets, sold_out_tickets = [], []
 
         tickets_in_cart = cart.tickets.values_list("id", flat=True)
-        event_dict = {
-            event["id"]: event["name"]
-            for event in Event.objects.filter(
-                id__in=tickets_to_replace.values_list("event", flat=True).distinct()
-            ).values("id", "name")
-        }
+        tickets_to_replace = tickets_to_replace.select_related("event")
 
-        # Process each ticket type and event
-        for ticket_class in tickets_to_replace.values("type", "event").annotate(
-            count=Count("id")
-        ):
-            available_tickets = (
-                Ticket.objects.filter(
-                    event=ticket_class["event"],
-                    type=ticket_class["type"],
-                    owner__isnull=True,
-                    holder__isnull=True,
-                )
-                .exclude(id__in=tickets_in_cart)
-                .select_related("event")[: ticket_class["count"]]
-            )
+        for ticket_class in tickets_to_replace.values(
+            "type", "event", "event__name"
+        ).annotate(count=Count("id")):
+            # we don't need to lock, since we aren't updating holder/owner
+            available_tickets = Ticket.objects.filter(
+                event=ticket_class["event"],
+                type=ticket_class["type"],
+                owner__isnull=True,
+                holder__isnull=True,
+            ).exclude(id__in=tickets_in_cart)[: ticket_class["count"]]
 
-            shortage = ticket_class["count"] - available_tickets.count()
-            if shortage > 0:
+            num_short = ticket_class["count"] - available_tickets.count()
+            if num_short > 0:
                 sold_out_tickets.append(
                     {
-                        **ticket_class,
+                        "type": ticket_class["type"],
                         "event": {
                             "id": ticket_class["event"],
-                            "name": event_dict[ticket_class["event"]],
+                            "name": ticket_class["event__name"],
                         },
-                        "count": shortage,
+                        "count": num_short,
                     }
                 )
+
+            replacement_tickets.extend(list(available_tickets))
 
         cart.tickets.remove(*tickets_to_replace)
         if replacement_tickets: