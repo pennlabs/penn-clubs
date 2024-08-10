9832bcb51cfd4c282c206342b3154eaab7dce940
Joy Liu <34288846+joyliu-q@users.noreply.github.com>
2024-08-10 16:37:06 -0400

    Update main.ts

--- k8s/main.ts
+++ k8s/main.ts
@@ -88,14 +88,6 @@ export class MyChart extends PennLabsChart {
       secret: clubsSecret,
       cmd: ['python', 'manage.py', 'import_calendar_events'],
     });
-
-
-    new CronJob(this, 'expire-stale-membership-invites', {
-      schedule: cronTime.everyDayAt(12),
-      image: backendImage,
-      secret: fyhSecret,
-      cmd: ["python", "manage.py", "expire_membership_invites"],
-    });
   }
 }
 
