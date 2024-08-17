80a5913535a0771980169405f30b49fd8b540f43
Julian Weng <julian.weng.us@gmail.com>
2024-08-17 11:29:57 -0400

    Reflect club description reapproval changes in email and form text

--- frontend/components/ClubEditPage/ClubEditCard.tsx
+++ frontend/components/ClubEditPage/ClubEditCard.tsx
@@ -447,7 +447,6 @@ export default function ClubEditCard({
         {
           name: 'description',
           required: true,
-          help: `Changing this field will require reapproval from the ${APPROVAL_AUTHORITY}.`,
           placeholder: `Type your ${OBJECT_NAME_SINGULAR} description here!`,
           type: 'html',
           hidden: !REAPPROVAL_QUEUE_ENABLED,
