db921225e1803bfc91882a6cddc0dc46f914d182
Owen Lester <owlester@sas.upenn.edu>
2024-09-02 16:03:26 -0400

    a

--- frontend/pages/settings.tsx
+++ frontend/pages/settings.tsx
@@ -13,7 +13,6 @@ import styled from 'styled-components'
 import { ApplicationSubmission, UserInfo } from 'types'
 import { OBJECT_NAME_TITLE, SHOW_MEMBERSHIP_REQUEST } from 'utils/branding'
 
-import ApplicationsPage from '~/components/Applications'
 import TicketsTab from '~/components/Settings/TicketsTab'
 import SubmissionsPage from '~/components/Submissions'
 import { BG_GRADIENT, CLUBS_BLUE, WHITE } from '~/constants/colors'
@@ -38,15 +37,9 @@ type SettingsProps = {
   userInfo?: UserInfo
   authenticated: boolean | null
   submissions: ApplicationSubmission[]
-  whartonapplications: any
 }
 
-const Settings = ({
-  userInfo,
-  authenticated,
-  submissions,
-  whartonapplications,
-}: SettingsProps) => {
+const Settings = ({ userInfo, authenticated, submissions }: SettingsProps) => {
   /**
    * Display the message to the user in the form of a toast.
    * @param The message to show to the user.
@@ -83,13 +76,6 @@ const Settings = ({
       name: 'submissions',
       label: 'Submissions',
       content: () => <SubmissionsPage initialSubmissions={submissions} />,
-    },
-    {
-      name: 'applications',
-      label: 'Applications',
-      content: () => (
-        <ApplicationsPage whartonapplications={whartonapplications} />
-      ),
     },
     {
       name: 'Requests',
@@ -133,7 +119,7 @@ type BulkResp = {
 
 Settings.getInitialProps = async (ctx: NextPageContext) => {
   const data: BulkResp = (await doBulkLookup(
-    ['whartonapplications', ['submissions', '/submissions/?format=json']],
+    [['submissions', '/submissions/?format=json']],
     ctx,
   )) as BulkResp
   return {
