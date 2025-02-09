97aef631e9b5d3aa57a8f5e5275d7a2799219f70
Sritan Motati <sritanmotati@gmail.com>
2025-02-09 16:52:43 -0500

    prettier

--- frontend/pages/health.tsx
+++ frontend/pages/health.tsx
@@ -1,24 +1,24 @@
-import { GetServerSideProps } from 'next';
+import { GetServerSideProps } from 'next'
 
 const HealthPage = () => {
-    return <div>OK</div>;
-};
+  return <div>OK</div>
+}
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
-    const userAgent = req.headers['user-agent'] || '';
+  const userAgent = req.headers['user-agent'] || ''
 
-    if (userAgent !== 'service-status') {
-        return {
-            redirect: {
-                destination: '/',
-                permanent: false,
-            },
-        };
-    }
-
+  if (userAgent !== 'service-status') {
     return {
-        props: {},
-    };
-};
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+  }
 
-export default HealthPage;
+  return {
+    props: {},
+  }
+}
+
+export default HealthPage
