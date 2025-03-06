1d1f1dc2d96169c94ee85d188ea3a4b13d1c239e
Julian Weng <julian.weng.us@gmail.com>
2025-03-06 09:46:23 -0500

    Add frontend auth gate for all pages (#788)

--- frontend/renderPage.tsx
+++ frontend/renderPage.tsx
@@ -27,11 +27,14 @@ import { Badge, Club, School, StudentType, Tag, UserInfo, Year } from './types'
 import {
   cache,
   doApiRequest,
+  getCurrentRelativePath,
   isClubFieldShown,
+  isDevelopment,
+  LOGIN_URL,
   OptionsContext,
   PermissionsContext,
 } from './utils'
-import { SITE_ID } from './utils/branding'
+import { LOGIN_REQUIRED_ALL, SITE_ID } from './utils/branding'
 import { logException } from './utils/sentry'
 
 export const ToastStyle = styled.div`
@@ -197,6 +200,9 @@ function renderPage<T>(Page: PageComponent<T>): React.ComponentType & {
         const { props, state, closeModal } = this
         const { modal } = state
         const { authenticated, userInfo } = props
+        if (LOGIN_REQUIRED_ALL && !isDevelopment() && !authenticated) {
+          window.location.href = `${LOGIN_URL}?next=${getCurrentRelativePath()}`
+        }
         return (
           <>
             <OptionsContext.Provider value={this.props.options}>
