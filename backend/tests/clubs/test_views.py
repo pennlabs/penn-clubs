70c7e576de38d1684f86d48f9f8b6334983972e7
AJ Boyle <ajboyle806@gmail.com>
2025-04-19 03:01:00 -0400

    test case

--- backend/tests/clubs/test_views.py
+++ backend/tests/clubs/test_views.py
@@ -1664,11 +1664,11 @@ class ClubTestCase(TestCase):
         self.assertEqual(
             data["new-club"]["description"]["diff"],
             """We are <ins style="text-decoration: none; background-color: #dafdd5;"""
-            """ opacity: 1;">not </ins>open<ins style="text-decoration: none; backg"""
+            """opacity: 1;">not </ins>open<ins style="text-decoration: none; backg"""
             """round-color: #dafdd5; opacity: 1;">,</ins> <del style="text-decorati"""
-            """on: none; background-color: #ffbdbd; opacity: 0.3;">source,</del><in"""
+            """on: none; background-color: #ffbdbd; opacity: 0.8;">source,</del><in"""
             """s style="text-decoration: none; background-color: #dafdd5; opacity: """
-            """1;">do not</ins> expect us.""",
+            """1;">do not</ins> expect us."""
         )
 
         # attempt to get diff of approved club
